2959145f1773432a09f8d8ea7d506342d90e5e75
Adam Pingel <pingel@gmail.com>
2011-03-20 12:01:06 -0700

    all but BayesianNetwork parse as scala

--- ai/src/main/scala/org/pingel/bayes/PartiallyDirectedGraph.scala
+++ ai/src/main/scala/org/pingel/bayes/PartiallyDirectedGraph.scala
@@ -1,180 +1,164 @@
 
 package org.pingel.bayes
 
-class PartiallyDirectedGraph
-{
-	private static final Boolean TRUE = new Boolean(true);
-	private static final Boolean FALSE = new Boolean(false);
-	
-	private List<RandomVariable> variables;
+object Constants {
+  // I have no idea what the point of these are
+  val TRUE = new Boolean(true)
+  val FALSE = new Boolean(false)
+}
 
-	private Map<RandomVariable, Integer> variable2index = new HashMap<RandomVariable, Integer>();
-	
-	private boolean[][] connect;
-	private boolean[][] mark;
-	private boolean[][] arrow;
-	
-	public PartiallyDirectedGraph(List<RandomVariable> vars)
-	{
-		this.variables = vars;
-		connect = new boolean[vars.size()][vars.size()];
-		mark = new boolean[vars.size()][vars.size()];
-		arrow = new boolean[vars.size()][vars.size()];
-		for(int i=0; i < vars.size(); i++) {
-			variable2index.put(vars.get(i), new Integer(i));
-			for(int j=0; j < vars.size(); j++) {
-				connect[i][j] = false;
-				mark[i][j] = false;
-				arrow[i][j] = false;
-			}
-		}
-	}
+class PartiallyDirectedGraph(variables: List[RandomVariable]) {
 
-	private int indexOf(RandomVariable v)
-	{
-		return variable2index.get(v).intValue();
-	}
+
+  var variable2index = Map[RandomVariable, Integer]()
 	
-    public void connect(RandomVariable v1, RandomVariable v2)
-    {
-    	int i1 = indexOf(v1);
-    	int i2 = indexOf(v2);
-    	connect[i1][i2] = true;
-    	connect[i2][i1] = true;
+  var connect = new Matrix[Boolean](vars.size, vars.size)
+  var mark = new Matrix[Boolean](vars.size, vars.size)
+  var arrow = new Matrix[Boolean](vars.size, vars.size)
+  
+  for( i <- 0 to variables.size-1 ) {
+    variable2index.put(variables(i), new Integer(i));
+    for( j <- 0 to variables.size-1 ) {
+      connect(i, j) = false
+      mark(i, j) = false
+      arrow(i, j) = false
     }
+  }
 
-    public boolean areAdjacent(RandomVariable v1, RandomVariable v2)
-    {
-    	int i1 = indexOf(v1);
-    	int i2 = indexOf(v2);
-    	return connect[i1][i2];
-    }
+  def indexOf(v: RandomVariable): Integer = variable2index(v).intValue
+	
+  def connect(v1: RandomVariable, v2: RandomVariable): Unit = {
+    val i1 = indexOf(v1)
+    val i2 = indexOf(v2)
+    connect(i1, i2) = true
+    connect(i2, i1) = true
+  }
+  
+  def areAdjacent(v1: RandomVariable, v2: RandomVariable): Boolean = {
+    connect(indexOf(v1), indexOf(v2))
+  }
 
-    public boolean undirectedAdjacent(RandomVariable v1, RandomVariable v2)
-    {
-    	int i1 = indexOf(v1);
-    	int i2 = indexOf(v2);
+  def undirectedAdjacent(v1: RandomVariable, v2: RandomVariable): Boolean = {
+    val i1 = indexOf(v1)
+    val i2 = indexOf(v2)
+    connect(i1, i2) && ! arrow(i1, i2) && ! arrow(i2, i1)
+  }
 
-    	return connect[i1][i2] && ! arrow[i1][i2] && ! arrow[i2][i1];
+  def mark(v1: RandomVariable, v2: RandomVariable): Unit = {
+    val i1 = indexOf(v1)
+    val i2 = indexOf(v2)
+    mark(i1, i2) = true
+    mark(i2, i1) = true
+  }
+    
+  def orient(v1: RandomVariable, v2: RandomVariable): Unit = {
+    // Note: we assume they are already adjacent without checking
+    val i1 = indexOf(v1)
+    val i2 = indexOf(v2)
+    arrow(i1, i2) = true
+  }
+
+  // TODO: scala version should probably use Option[Boolean] instead of allowing null
+  def links(v: RandomVariable, arrowIn: Boolean, marked: Boolean, arrowOut: Boolean): List[RandomVariable] = {
+
+    var result = new List[RandomVariable]()
     	
-    }
+    val i = indexOf(v)
+    	
+    for(j <- 0 to size-1) {
 
-    public void mark(RandomVariable v1, RandomVariable v2)
-    {
-    	int i1 = indexOf(v1);
-    	int i2 = indexOf(v2);
-    	mark[i1][i2] = true;
-    	mark[i2][i1] = true;
+      val u = variables(j)
+      var pass = connect(i, j)
+      
+      if( pass && (arrowIn != null) ) {
+    	  pass = ( arrowIn.booleanValue() == arrow(j, i) )
+      }
+      
+      if( pass && (marked != null) ) {
+    	pass = ( marked.booleanValue() == mark(i, j) )
+      }
+      
+      if( pass && (arrowOut != null) ) {
+    	  pass = ( arrowOut.booleanValue() == arrow(i, j) )
+      }
+      
+      if( pass ) {
+    	result.add(u)
+      }
     }
     
-    public void orient(RandomVariable v1, RandomVariable v2) {
+    result
+  }
 
-    	// Note: we assume they are already adjacent without checking
+  def markedPathExists(from: RandomVariable, target: RandomVariable): Boolean = {
+    // this will not terminate if there are cycles
 
-    	int i1 = indexOf(v1);
-    	int i2 = indexOf(v2);
+    var frontier = List[RandomVariable]()
+    frontier.add(from)
+    	
+    while( frontier.size > 0 ) {
+      val head = frontier.firstElement()
+      frontier.removeElementAt(0)
+      if( head.equals(target) ) {
+    	return true
+      }
+      var follow = links(head, null, Constants.TRUE, Constants.TRUE)
+      frontier.addAll(follow)
+    }
+    false
+  }
+    
+  override def toString(): String = {
         
-        arrow[i1][i2] = true;
-    }
-
-
-
-    public Vector<RandomVariable> links(RandomVariable v, Boolean arrowIn, Boolean marked, Boolean arrowOut)
-    {
-    	Vector<RandomVariable> result = new Vector<RandomVariable>();
-    	
-    	int i = indexOf(v);
-    	
-    	for(int j=0; j < variables.size(); j++) {
-
-    		RandomVariable u = variables.get(j);
-
-    		boolean pass = connect[i][j];
-
-    		if( pass && (arrowIn != null) )
-    		{
-    			pass = ( arrowIn.booleanValue() == arrow[j][i] );
-    		}
-
-    		if( pass && (marked != null) ) {
-    			pass = ( marked.booleanValue() == mark[i][j] );
-    		}
-
-    		if( pass && (arrowOut != null) ) {
-    			pass = ( arrowOut.booleanValue() == arrow[i][j] );
-    		}
-    		
-    		if( pass ) {
-    			result.add(u);
-    		}
-    	}
-
-    	return result;
-    }
-
-    public boolean markedPathExists(RandomVariable from, RandomVariable target)
-    {
-    	// this will not terminate if there are cycles
-
-    	Vector<RandomVariable> frontier = new Vector<RandomVariable>();
-    	frontier.add(from);
-    	
-    	while( frontier.size() > 0 ) {
-    		RandomVariable head = frontier.firstElement();
-    		frontier.removeElementAt(0);
-    		if( head.equals(target) ) {
-    			return true;
-    		}
-    		Vector<RandomVariable> follow = links(head, null, TRUE, TRUE);
-    		frontier.addAll(follow);
-    	}
-    	return false;
+    var result = ""
+    
+    for( rv <- variables ) {
+      result += "var " + rv + " has index " + variable2index(rv)
+      result += "\n"
     }
     
-    public String toString()
-    {
-        
-//      private boolean[][] connect;
-//      private boolean[][] mark;
-//      private boolean[][] arrow;
-
-        String result = "";
-
-        for( RandomVariable rv : variables ) {
-            result += "var " + rv + " has index " + variable2index.get(rv).toString();
-            result += "\n";
-        }
-        
-        result += "connect\n\n";
-        
-        for(int i=0; i < variables.size(); i++) {
-            for(int j=0; j < variables.size(); j++) {
-                result += connect[i][j] ? "x" : " ";
-            }
-            result += "\n";
-        }
-        result += "\n\n";
-        
-        result += "mark\n\n";
-        
-        for(int i=0; i < variables.size(); i++) {
-            for(int j=0; j < variables.size(); j++) {
-                result += mark[i][j] ? "x" : " ";
-            }
-            result += "\n";
-        }
-        result += "\n\n";
-        
-        result += "arrow\n\n";
-        
-        for(int i=0; i < variables.size(); i++) {
-            for(int j=0; j < variables.size(); j++) {
-                result += arrow[i][j] ? "x" : " ";
-            }
-            result += "\n";
-        }
-        result += "\n\n";
-
-        return result;
+    result += "connect\n\n"
+    for( i <- 0 to variables.size-1 ) {
+      for( j <- 0 to variables.size-1 ) {
+	if( connect(i, j) ) {
+	  result += "x"
+	}
+	else {
+	  result += " "
+	}
+      }
+      result += "\n"
     }
+    result += "\n\n"
+    
+    result += "mark\n\n"
+    for( i <- 0 to variables.size-1 ) {
+      for( j <- 0 to variables.size-1 ) {
+	if( mark(i, j) ) {
+	  result += "x"
+	}
+	else {
+	  result += " "
+	}
+      }
+      result += "\n"
+    }
+    result += "\n\n"
+    
+    result += "arrow\n\n"
+    for( i <- 0 to variables.size-1 ) {
+      for( j <- 0 to variables.size-1 ) {
+	if( arrow(i, j) ) {
+	  result += "x"
+	}
+	else {
+	  result += " "
+	}
+      }
+      result += "\n"
+    }
+    result += "\n\n"
+    
+    result
+  }
 }
