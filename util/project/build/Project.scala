f2014a269e1f3177123a8014786171314f05ecbb
Adam Pingel <pingel@gmail.com>
2011-05-31 23:35:15 -0700

    adding jar publishing info to sbt project files

--- util/project/build/Project.scala
+++ util/project/build/Project.scala
@@ -11,4 +11,13 @@ with Eclipsify
   val slf4jsimple = "org.slf4j" % "slf4j-simple" % "1.6.1"
   val slf4japi = "org.slf4j" % "slf4j-api" % "1.6.1"
   val glassFishRepo = "GlassFish Maven Repository" at "http://download.java.net/maven/glassfish"
+
+  override def managedStyle = ManagedStyle.Maven
+
+  val keyFile: java.io.File = Path.userHome / ".ec2" / "id_rsa-gsg-keypair" asFile
+
+  lazy val publishTo = Resolver.ssh("pingel.org-repo",
+				    "www.pingel.org",
+				    "/var/www/pingel.org/maven2") as("root", keyFile) withPermissions("0644")
+
 }
