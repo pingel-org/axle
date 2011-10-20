14917415aedaa4a3662694fa2c27a2264ee022c4
Adam Pingel <pingel@gmail.com>
2011-10-19 22:45:54 -0700

    adding other di cake example

--- di/src/main/java/org/pingel/di/Example.scala
+++ di/src/main/java/org/pingel/di/Example.scala
@@ -1,20 +1,44 @@
 
-object Example {
+ 
+// service implementations  
+
+trait OnOffDeviceComponentImpl extends OnOffDeviceComponent {  
+  class Heater extends OnOffDevice {  
+    def on = println("heater.on")  
+    def off = println("heater.off")  
+  }  
+}  
+trait SensorDeviceComponentImpl extends SensorDeviceComponent {  
+  class PotSensor extends SensorDevice {  
+    def isCoffeePresent = true  
+  }  
+}  
+
+// service declaring two dependencies that it wants injected  
+
+trait WarmerComponentImpl {  
+  this: SensorDeviceComponent with OnOffDeviceComponent =>  
+  class Warmer {  
+    def trigger = {  
+      if (sensor.isCoffeePresent) onOff.on  
+      else onOff.off  
+    }  
+  }  
+}  
   
+// instantiate the services in a module  
+
+object ComponentRegistry extends  
+  OnOffDeviceComponentImpl with  
+  SensorDeviceComponentImpl with  
+  WarmerComponentImpl
+{  
+  val onOff = new Heater  
+  val sensor = new PotSensor  
+  val warmer = new Warmer  
+}  
+  
+object Example {
+  val warmer = ComponentRegistry.warmer  
+  warmer.trigger
 }
-
-// a dummy service that is not persisting anything  
-// solely prints out info  
-
-class UserRepository {
-
-  def authenticate(user: User): User = {   
-    println("authenticating user: " + user)  
-    user  
-  }
-
-  def create(user: User) = println("creating user: " + user)
-
-  def delete(user: User) = println("deleting user: " + user)
-
-} 
