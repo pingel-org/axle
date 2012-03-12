918c6adc8144f08724eb235a7db7750950e94796
Adam Pingel <pingel@gmail.com>
2012-03-11 22:20:11 -0700

    progress on DirectedGraph.draw

--- axle/src/main/scala/org/pingel/axle/graph/DirectedGraph.scala
+++ axle/src/main/scala/org/pingel/axle/graph/DirectedGraph.scala
@@ -221,31 +221,89 @@ package org.pingel.axle.graph {
 
     import javax.swing.JFrame
     import java.awt.Dimension
-    import edu.uci.ics.jung.algorithms.layout.CircleLayout
+    import java.awt.BasicStroke
+    import java.awt.Color
+    import java.awt.Paint
+    import java.awt.Stroke
+    import java.awt.event.MouseEvent
+
+    // import edu.uci.ics.jung.algorithms.layout.CircleLayout
+    import edu.uci.ics.jung.algorithms.layout.FRLayout
     import edu.uci.ics.jung.algorithms.layout.Layout
     import edu.uci.ics.jung.graph.Graph
+    import edu.uci.ics.jung.graph.SparseGraph
     import edu.uci.ics.jung.graph.SparseMultigraph
-    import edu.uci.ics.jung.visualization.BasicVisualizationServer
+    // import edu.uci.ics.jung.visualization.BasicVisualizationServer
+    import edu.uci.ics.jung.visualization.VisualizationViewer
+    // import edu.uci.ics.jung.visualization.control.CrossoverScalingControl
+    // import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse
+    import edu.uci.ics.jung.visualization.control.PluggableGraphMouse
+    import edu.uci.ics.jung.visualization.control.PickingGraphMousePlugin
+    import edu.uci.ics.jung.visualization.control.ModalGraphMouse
+    // import edu.uci.ics.jung.visualization.control.ScalingGraphMousePlugin
+    import edu.uci.ics.jung.visualization.control.TranslatingGraphMousePlugin
+    import edu.uci.ics.jung.visualization.decorators.ToStringLabeller
+    import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position
+
+    import org.apache.commons.collections15.Transformer
+
+    val width = 700
+    val height = 700
+    val border = 50
 
     def jframe(): JFrame = {
 
-      // see http://www.grotto-networking.com/JUNG/
+      // see
+      // http://www.grotto-networking.com/JUNG/
       // http://www.grotto-networking.com/JUNG/JUNG2-Tutorial.pdf
 
-      val layout = new CircleLayout(jungGraph) // FRLayout
-      layout.setSize(new Dimension(300, 300))
-      val vv = new BasicVisualizationServer[ug.type#V, ug.type#E](layout)
-      vv.setPreferredSize(new Dimension(350, 350))
+      val layout = new FRLayout(jungGraph)
+      layout.setSize(new Dimension(width, height))
+      // val vv = new BasicVisualizationServer[ug.type#V, ug.type#E](layout) // non-interactive
+      val vv = new VisualizationViewer[ug.type#V, ug.type#E](layout) // interactive
+      vv.setPreferredSize(new Dimension(width + border, height + border))
 
-      val jf = new JFrame("Simple Graph View")
-      jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-      jf.getContentPane().add(vv)
-      jf.pack()
-      jf
+      val vertexPaint = new Transformer[ug.type#V, Paint]() {
+        def transform(i: ug.type#V): Paint = Color.GREEN
+      }
 
+      val dash = List(10.0f).toArray
+
+      val edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f)
+
+      val edgeStrokeTransformer = new Transformer[ug.type#E, Stroke]() {
+        def transform(edge: ug.type#E) = edgeStroke
+      }
+
+      val vertexLabelTransformer = new Transformer[ug.type#V, String]() {
+        def transform(vertex: ug.type#V) = "bar"
+      }
+      
+      val edgeLabelTransformer = new Transformer[ug.type#E, String]() {
+        def transform(edge: ug.type#E) = "foo"
+      }
+
+      vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint)
+      vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer)
+      vv.getRenderContext().setVertexLabelTransformer(vertexLabelTransformer) // new ToStringLabeller())
+      vv.getRenderContext().setEdgeLabelTransformer(edgeLabelTransformer)
+      vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR)
+
+      //      val gm = new DefaultModalGraphMouse()
+      //      gm.setMode(ModalGraphMouse.Mode.TRANSFORMING)
+      val gm = new PluggableGraphMouse()
+      gm.add(new TranslatingGraphMousePlugin(MouseEvent.BUTTON1))
+      gm.add(new PickingGraphMousePlugin())
+      // gm.add(new ScalingGraphMousePlugin(new CrossoverScalingControl(), 0, 1.1f, 0.9f))
+      vv.setGraphMouse(gm)
+
+      val frame = new JFrame("graph name")
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+      frame.getContentPane().add(vv)
+      frame.pack()
+      frame
     }
 
   }
-  
-  
+
 }
