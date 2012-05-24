f2b1ee779983d6cf22d37a82ea7362799ec6a5ea
Adam Pingel <pingel@gmail.com>
2012-05-24 02:16:41 -0700

    rm tttplayer

--- core/src/main/scala/axle/game/ttt/TicTacToePlayer.scala
+++
@@ -1,9 +0,0 @@
-
-package axle.game.ttt
-
-import axle.game._
-
-case class TicTacToePlayer(game: TicTacToe, playerId: String, description: String)
-  extends Player(game, playerId, description) {
-
-}
