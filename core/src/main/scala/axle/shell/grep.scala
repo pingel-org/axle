1ad238d87f05db18df1a6cd3a37121de8ca96388
Adam Pingel <pingel@gmail.com>
2012-06-13 11:44:41 -0700

    adding a few common unix commands to shell

---
+++ core/src/main/scala/axle/shell/grep.scala
@@ -0,0 +1,11 @@
+package axle.shell
+
+import java.io._
+import java.util.Scanner
+import axle.ShellCommand
+
+case class grep(pattern: String) extends ShellCommand {
+  def exec() = {
+    1
+  }
+}
