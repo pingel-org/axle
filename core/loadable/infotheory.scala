3ba726f3cb02815496595348db9094d394b9e17d
Adam Pingel <pingel@gmail.com>
2012-06-23 18:06:00 -0700

    simple demo of information theory

---
+++ core/loadable/infotheory.scala
@@ -0,0 +1,11 @@
+
+import axle.InformationTheory._
+
+val biasedCoin = coin(0.9)
+println((0 until 100).map(i => biasedCoin.choose()).mkString(" "))
+
+val fairCoin = coin()
+println((0 until 100).map(i => fairCoin.choose()).mkString(" "))
+
+println("entropy of 90/10 coin: " + biasedCoin.entropy())
+println("entropy of 50/50 coin: " + fairCoin.entropy())
