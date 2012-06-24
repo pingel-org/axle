b53df657b9953f7c07f2492771ec5e4cd208662e
Adam Pingel <pingel@gmail.com>
2012-06-23 19:07:12 -0700

    examples of axle.Enrichments

---
+++ core/loadable/enriched.scala
@@ -0,0 +1,33 @@
+
+import axle.Enrichments._
+
+true and false
+
+true ∧ false
+
+true and true
+
+true ∧ true
+    
+true or false
+    
+true ∨ false
+    
+true implies false
+    
+true implies true
+
+Set(1, 2, 3).∃( _ % 2 == 0 )
+    
+List(1, 2, 3).∀( _ % 2 == 0 )
+
+(1 to 10).Π(_ * 3)
+
+(1 to 10).Σ(_ * 2)
+
+List(1, 2, 3).doubles
+    
+Set(1, 2, 3).triples
+
+(List(1, 2, 3) ⨯ List(4, 5, 6)).toList
+
