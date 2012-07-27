6af0e068eb8c0be305eb33c3f2e049b9854543c2
Adam Pingel <pingel@gmail.com>
2012-07-27 01:18:48 -0700

    short-circuiting entropy multiplication

--- core/src/main/scala/axle/InformationTheory.scala
+++ core/src/main/scala/axle/InformationTheory.scala
@@ -12,11 +12,16 @@ object InformationTheory {
 
   def log2(x: Double) = log(x) / log(2)
 
-  // TODO: filter out P(X eq x) == 0
-  def entropy[A](X: RandomVariable[A]) = rv2it(X).Σ(x => -P(X eq x)() * log2(P(X eq x)())) *: bit
+  def entropy[A](X: RandomVariable[A]) = rv2it(X).Σ(x => {
+    val px = P(X eq x)()
+    px match {
+      case 0.0 => 0.0
+      case _ => -px * log2(px)
+    }
+  }) *: bit
 
   // def H_:(): Information#UOM = entropy()
-  
+
   def huffmanCode[A, S](alphabet: Set[S]): Map[A, Seq[S]] = {
     //   // TODO
     //   // http://en.wikipedia.org/wiki/Huffman_coding
