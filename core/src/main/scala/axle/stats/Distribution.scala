7361d5c367c178772349cd1a6acf08535f94664a
Adam Pingel <pingel@gmail.com>
2012-09-02 19:48:28 -0700

    notes for standard deviation

--- core/src/main/scala/axle/stats/Distribution.scala
+++ core/src/main/scala/axle/stats/Distribution.scala
@@ -1,7 +1,22 @@
 package axle.stats
 
 trait Distribution[A] {
+
   def probabilityOf(a: A): Double
+
+  val square: (Double => Double) = (x: Double) => x * x
+
+  /**
+   * http://en.wikipedia.org/wiki/Standard_deviation
+   */
+  
+  def σ(): Double = {
+    val p: (Double => Double) = (x: Double) => 0.0 // TODO
+    val Xs: List[Double] = Nil // TODO
+    val μ = Xs.map(xi => p(xi) * xi).sum
+    math.sqrt(Xs.map(xi => p(xi) * square(xi - μ)).sum)
+  }
+  
 }
 
 trait Distribution0[A] extends Distribution[A] {
