dc5fba68e6702e37096cd4ca3d3796e4c2820032
Adam Pingel <pingel@gmail.com>
2012-09-03 19:27:37 -0700

    map(f).flatMap(id) == flatMap(f)

--- core/src/main/scala/axle/MapReduce.scala
+++ core/src/main/scala/axle/MapReduce.scala
@@ -14,8 +14,7 @@ object ScalaMapReduce extends MapReduce {
 
   def mapReduce[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): immutable.Map[K, V] =
     data
-      .map(mapper(_))
-      .flatMap(x => x)
+      .flatMap(mapper(_))
       .toList // TODO inefficient
       .groupBy(_._1)
       .map(kv => (kv._1, kv._2.map(_._2).reduce(reducer)))
