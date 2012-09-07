8b85644ae9b2ef3ff3e486a143cdb00513bfd7e9
Adam Pingel <pingel@gmail.com>
2012-09-06 22:52:42 -0700

    simplifying Permuter

--- core/src/main/scala/axle/Permuter.scala
+++ core/src/main/scala/axle/Permuter.scala
@@ -27,45 +27,27 @@ case class Permuter[E](objects: List[E], n: Int) extends Iterable[List[E]] {
   def iterator() = new PermutionIterator[E](this)
 
   class PermutionIterator[InE](permuter: Permuter[InE]) extends Iterator[List[InE]] {
-    val remainders = mutable.ArrayBuffer[mutable.Set[InE]]()
+    val remainders = mutable.ArrayBuffer[immutable.Set[InE]]()
     val iterators = mutable.ArrayBuffer[Iterator[InE]]()
     var tuple = mutable.ArrayBuffer[InE]()
 
     if (permuter.getN > 0) {
-      val firstRemainder = mutable.Set[InE]()
-      firstRemainder ++= permuter.getObjects
-      remainders.append(firstRemainder)
-      iterators.append(firstRemainder.iterator)
-      tuple.append(iterators(0).next())
+      val firstRemainder = permuter.getObjects.toSet
+      remainders += firstRemainder
+      iterators += firstRemainder.iterator
+      tuple += iterators(0).next()
     }
 
     for (i <- 1 until permuter.getN) {
-      remainders.append(null)
-      iterators.append(null)
+      remainders += null
+      iterators += null
       setRemainder(i)
-      tuple.append(iterators(i).next())
+      tuple += iterators(i).next()
     }
 
-    //        private void setTupleElement(int i)
-    //        {
-    // TODO an interesting case of software design here.  Saying "private tuple" isn't enough.
-    // what I really want is to limit write access to tuple to this method.  In order to do that
-    // I'd have to come up with an inner class (or some such mechanism).  I think there should
-    // be better language support for this kind of thing.
-    // Actually, I don't think an inner class would be enough... because the "private" data
-    // member tuple would still be accessible here.
-    // Does AOP solve this?  From what I understand, AOP would allow me to enforce the
-    // "remainder setting always follows tuple element setting".  But I don't know about 
-    // the visibility of such a statement.
-    //        }
-
     def setRemainder(i: Int) = {
-      //println("setRemainder: i = " + i)
       if (i > 0) {
-        val r = mutable.Set[InE]()
-        r ++= remainders(i - 1)
-        r.remove(tuple(i - 1))
-        remainders(i) = r
+        remainders(i) = remainders(i - 1) - tuple(i - 1)
       }
       iterators(i) = remainders(i).iterator
     }
@@ -75,7 +57,6 @@ case class Permuter[E](objects: List[E], n: Int) extends Iterable[List[E]] {
     def hasNext() = tuple != null
 
     def incrementLastAvailable(i: Int): Boolean = {
-      //println("incrementLastAvailable: i = " + i)
       if (i == -1) {
         return true
       } else if (iterators(i).hasNext) {
@@ -90,7 +71,6 @@ case class Permuter[E](objects: List[E], n: Int) extends Iterable[List[E]] {
     }
 
     def next() = {
-      // println("next: remainders = " + remainders + ", tuple = " + tuple)
       if (tuple == null) {
         throw new NoSuchElementException()
       }
