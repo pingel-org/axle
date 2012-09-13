2c7132325514b0efbbb7f7c6c3e273908f865ece
Adam Pingel <pingel@gmail.com>
2012-09-12 17:50:38 -0700

    experimenting with moving towards specialized DSL for grammar

--- core/src/main/scala/axle/ast/language/Python.scala
+++ core/src/main/scala/axle/ast/language/Python.scala
@@ -5,80 +5,82 @@ import util.matching.Regex
 
 object Python {
 
-  val expressions =
-    new Rule("Module", Sub("node")) ::
-      new Rule("Stmt", Spread()) ::
-      new Rule("EmptyNode", Nop()) ::
-      new Rule("Keyword", SqT(Attr("name"), Sq(Lit("="), Sub("expr")))) ::
-      new Rule("Name", Attr("name")) ::
-      new Rule("Const", Repr("value")) ::
-      new Rule("Dict", Sq(Emb("{", JItems("items", " : ", ", "), "}"))) ::
-      new Rule("List", Sq(Emb("[", J("nodes", Sq(Lit(","), Sp())), "]"))) ::
-      new Rule("Tuple", Sq(Emb("(", J("nodes", Sq(Lit(","), Sp())), ")"))) ::
-      new Rule("ListComp", Sq(Emb("[", Sq(Sub("expr"), Sp(), Kw("for"), Sp(), J("quals", Nop())), "]"))) ::
-      new Rule("ListCompFor", Sq(Sub("assign"), Sp(), Kw("in"), Sp(), Sub("list"), Affix("ifs", " ", None))) ::
-      new Rule("ListCompIf", Sq(Kw("if"), Sp(), Sub("test"))) ::
-      new Rule("Subscript", Sq(Sub("expr"), Emb("[", J("subs", Sq(Lit(","), Sp())), "]"))) ::
-      new Rule("Slice", Sq(Sub("expr"), Emb("[", Sq(Sub("lower"), Lit(":"), Sub("upper")), "]"))) ::
-      new Rule("Ellipsis", Kw("...")) ::
-      new Rule("Sliceobj", Nop()) ::
-      new Rule("Lambda", Sq(Kw("lambda"), Sp(),
-        J("argnames", Sq(Lit(","), Sp())), Lit(":"), Sp(), Sub("code"))) ::
-      new Rule("GenExpr", Sq(Sub("code"))) ::
-      new Rule("GenExprFor", Sq(Kw("for"), Sp(), Sub("assign"), Sp(), Kw("in"), Sp(),
-        Sub("iter"), Sp(), Sub("ifs"))) ::
-      new Rule("GenExprIf", Sq(Kw("if"), Sp(), Sub("test"))) ::
-      new Rule("GenExprInner", Sq(Sub("expr"), J("quals", Nop()))) ::
-      new Rule("Getattr", Sq(Sub("expr"), Op("." /* TODO tight=True */ ), Attr("attrname"))) ::
-      new Rule("Backquote", Sq(Emb("`", Sub("expr"), "`"))) ::
-      new Rule("CallFunc", Sq(Sub("node"), Emb("(", J("args", Sq(Lit(","), Sp())), ")"))) ::
-      new Rule("Compare", Sq(Sub("node"), Sp(), J("args", Sp()))) ::
-      new Rule("Add", Sq(Sub("left"), Sp(), Op("+"), Sp(), Sub("right"))) ::
-      new Rule("AssAttr", Sq(Sub("expr"), Op("."), Attr("attrname"))) ::
-      new Rule("Assign", Sq(J("nodes", Sq(Lit(","), Sp())), Sp(), Op("="), Sp(), Sub("expr"))) ::
-      new Rule("Invert", Sq(Op("~"), Sp(), Sub("expr"))) ::
-      new Rule("UnaryAdd", Sq(Op("+"), Sp(), Sub("expr"))) ::
-      new Rule("UnarySub", Sq(Op("-"), Sp(), Sub("expr"))) ::
-      new Rule("Not", Sq(Op("not"), Sp(), Sub("expr"))) ::
-      new Rule("And", J("nodes", Sq(Sp(), Op("and"), Sp()))) ::
-      new Rule("Or", J("nodes", Sq(Sp(), Op("or"), Sp()))) ::
-      new Rule("Div", Sq(Sub("left"), Sp(), Op("/"), Sp(), Sub("right"))) ::
-      new Rule("FloorDiv", Sq(Sub("left"), Sp(), Op("//"), Sp(), Sub("right"))) ::
-      new Rule("LeftShift", Sq(Sub("left"), Sp(), Op("<<"), Sp(), Sub("right"))) ::
-      new Rule("Mod", Sq(Sub("left"), Sp(), Op("%"), Sp(), Sub("right"))) ::
-      new Rule("Mul", Sq(Sub("left"), Sp(), Op("*"), Sp(), Sub("right"))) ::
-      new Rule("Power", Sq(Sub("left"), Sp(), Op("**"), Sp(), Sub("right"))) ::
-      new Rule("RightShift", Sq(Sub("left"), Sp(), Op(">>"), Sp(), Sub("right"))) ::
-      new Rule("Sub", Sq(Sub("left"), Sp(), Op("-"), Sp(), Sub("right"))) ::
-      new Rule("Bitand", J("nodes", Sq(Sp(), Op("&"), Sp()))) ::
-      new Rule("Bitor", J("nodes", Sq(Sp(), Op("|"), Sp()))) ::
-      new Rule("Bitxor", J("nodes", Sq(Sp(), Op("^"), Sp()))) ::
-      Nil
+  case class RuleBuilder(name: String) {
+    def :=(statement: Statement): Rule = new Rule(name, statement)
+  }
 
-  val simpleStatements: List[Rule] =
-    new Rule("Expression", Nop()) ::
-      new Rule("Discard", Sub("expr")) ::
-      new Rule("Pass", Kw("pass")) ::
-      new Rule("Assert", Sq(Kw("assert"), Sp(), Sub("test"), SqT(Lit(","), Sp(), Sub("fail")))) ::
-      new Rule("AssList", Sq(Emb("[", J("nodes", Sq(Lit(","), Sp())), "]"))) ::
-      new Rule("AssName", Attr("name")) ::
-      new Rule("AssTuple", Sq(Emb("(", J("nodes", Sq(Lit(","), Sp())), ")"))) ::
-      new Rule("AugAssign", Sq(Sub("node"), Sp(), Attr("op"), Sp(), Sub("expr"))) ::
-      new Rule("Print", Sq(Kw("print"), Sp(), J("nodes", Sp()))) ::
-      new Rule("Printnl", Sq(Kw("print"), Sp(), J("nodes", Sp()))) ::
-      new Rule("Return", Sq(Kw("return"), Sp(), Sub("value"))) ::
-      new Rule("Yield", Sq(Kw("yield"), Sp(), Sub("value"))) ::
-      new Rule("Raise", Sq(Kw("raise"), SqT(Sp(), Sub("expr1")), SqT(Lit(", "), Sub("expr2")))) :: // TODO: possibly also expr3.  not sure how to trigger this
-      new Rule("Break", Kw("break")) ::
-      new Rule("Continue", Kw("continue")) ::
-      new Rule("From", Sq(Kw("from"), Sp(), Attr("modname"), Sp(), Kw("import"), Sp(),
-        ForDel("names", Sq(VarN(0), SqT(Sp(), Kw("as"), Sp(), VarN(1))), ", "))) ::
-      new Rule("Import", SqT(Kw("import"), Sp(), ForDel("names", Sq(VarN(0), SqT(Sp(), Kw("as"), Sp(), VarN(1))), ", "))) ::
-      new Rule("Global", Sq(Kw("global"), Sp(), J("names", Sq(Lit(","), Sp())))) ::
-      new Rule("Exec", Sq(Kw("exec"), Sp(), Sub("expr"), SqT(Lit(" in "), Sub("locals")), SqT(Lit(", "), Sub("globals")))) ::
-      Nil
+  implicit val enrichRuleName = (name: String) => RuleBuilder(name)
 
-  val if_rule = new Rule("If",
+  val expressions = List(
+    "Module" := Sub("node"),
+    "Stmt" := Spread(),
+    "EmptyNode" := Nop(),
+    "Keyword" := SqT(Attr("name"), Sq(Lit("="), Sub("expr"))),
+    "Name" := Attr("name"),
+    "Const" := Repr("value"),
+    "Dict" := Sq(Emb("{", JItems("items", " : ", ", "), "}")),
+    "List" := Sq(Emb("[", J("nodes", Sq(Lit(","), Sp())), "]")),
+    "Tuple" := Sq(Emb("(", J("nodes", Sq(Lit(","), Sp())), ")")),
+    "ListComp" := Sq(Emb("[", Sq(Sub("expr"), Sp(), Kw("for"), Sp(), J("quals", Nop())), "]")),
+    "ListCompFor" := Sq(Sub("assign"), Sp(), Kw("in"), Sp(), Sub("list"), Affix("ifs", " ", None)),
+    "ListCompIf" := Sq(Kw("if"), Sp(), Sub("test")),
+    "Subscript" := Sq(Sub("expr"), Emb("[", J("subs", Sq(Lit(","), Sp())), "]")),
+    "Slice" := Sq(Sub("expr"), Emb("[", Sq(Sub("lower"), Lit(":"), Sub("upper")), "]")),
+    "Ellipsis" := Kw("..."),
+    "Sliceobj" := Nop(),
+    "Lambda" := Sq(Kw("lambda"), Sp(), J("argnames", Sq(Lit(","), Sp())), Lit(":"), Sp(), Sub("code")),
+    "GenExpr" := Sq(Sub("code")),
+    "GenExprFor" := Sq(Kw("for"), Sp(), Sub("assign"), Sp(), Kw("in"), Sp(), Sub("iter"), Sp(), Sub("ifs")),
+    "GenExprIf" := Sq(Kw("if"), Sp(), Sub("test")),
+    "GenExprInner" := Sq(Sub("expr"), J("quals", Nop())),
+    "Getattr" := Sq(Sub("expr"), Op("." /* TODO tight=True */ ), Attr("attrname")),
+    "Backquote" := Sq(Emb("`", Sub("expr"), "`")),
+    "CallFunc" := Sq(Sub("node"), Emb("(", J("args", Sq(Lit(","), Sp())), ")")),
+    "Compare" := Sq(Sub("node"), Sp(), J("args", Sp())),
+    "Add" := Sq(Sub("left"), Sp(), Op("+"), Sp(), Sub("right")),
+    "AssAttr" := Sq(Sub("expr"), Op("."), Attr("attrname")),
+    "Assign" := Sq(J("nodes", Sq(Lit(","), Sp())), Sp(), Op("="), Sp(), Sub("expr")),
+    "Invert" := Sq(Op("~"), Sp(), Sub("expr")),
+    "UnaryAdd" := Sq(Op("+"), Sp(), Sub("expr")),
+    "UnarySub" := Sq(Op("-"), Sp(), Sub("expr")),
+    "Not" := Sq(Op("not"), Sp(), Sub("expr")),
+    "And" := J("nodes", Sq(Sp(), Op("and"), Sp())),
+    "Or" := J("nodes", Sq(Sp(), Op("or"), Sp())),
+    "Div" := Sq(Sub("left"), Sp(), Op("/"), Sp(), Sub("right")),
+    "FloorDiv" := Sq(Sub("left"), Sp(), Op("//"), Sp(), Sub("right")),
+    "LeftShift" := Sq(Sub("left"), Sp(), Op("<<"), Sp(), Sub("right")),
+    "Mod" := Sq(Sub("left"), Sp(), Op("%"), Sp(), Sub("right")),
+    "Mul" := Sq(Sub("left"), Sp(), Op("*"), Sp(), Sub("right")),
+    "Power" := Sq(Sub("left"), Sp(), Op("**"), Sp(), Sub("right")),
+    "RightShift" := Sq(Sub("left"), Sp(), Op(">>"), Sp(), Sub("right")),
+    "Sub" := Sq(Sub("left"), Sp(), Op("-"), Sp(), Sub("right")),
+    "Bitand" := J("nodes", Sq(Sp(), Op("&"), Sp())),
+    "Bitor" := J("nodes", Sq(Sp(), Op("|"), Sp())),
+    "Bitxor" := J("nodes", Sq(Sp(), Op("^"), Sp()))
+  )
+
+  val simpleStatements: List[Rule] = List(
+    "Expression" := Nop(),
+    "Discard" := Sub("expr"),
+    "Pass" := Kw("pass"),
+    "Assert" := Sq(Kw("assert"), Sp(), Sub("test"), SqT(Lit(","), Sp(), Sub("fail"))),
+    "AssList" := Sq(Emb("[", J("nodes", Sq(Lit(","), Sp())), "]")),
+    "AssName" := Attr("name"),
+    "AssTuple" := Sq(Emb("(", J("nodes", Sq(Lit(","), Sp())), ")")),
+    "AugAssign" := Sq(Sub("node"), Sp(), Attr("op"), Sp(), Sub("expr")),
+    "Print" := Sq(Kw("print"), Sp(), J("nodes", Sp())),
+    "Printnl" := Sq(Kw("print"), Sp(), J("nodes", Sp())),
+    "Return" := Sq(Kw("return"), Sp(), Sub("value")),
+    "Yield" := Sq(Kw("yield"), Sp(), Sub("value")),
+    "Raise" := Sq(Kw("raise"), SqT(Sp(), Sub("expr1")), SqT(Lit(", "), Sub("expr2"))), // TODO: possibly also expr3.  not sure how to trigger this
+    "Break" := Kw("break"),
+    "Continue" := Kw("continue"),
+    "From" := Sq(Kw("from"), Sp(), Attr("modname"), Sp(), Kw("import"), Sp(), ForDel("names", Sq(VarN(0), SqT(Sp(), Kw("as"), Sp(), VarN(1))), ", ")),
+    "Import" := SqT(Kw("import"), Sp(), ForDel("names", Sq(VarN(0), SqT(Sp(), Kw("as"), Sp(), VarN(1))), ", ")),
+    "Global" := Sq(Kw("global"), Sp(), J("names", Sq(Lit(","), Sp()))),
+    "Exec" := Sq(Kw("exec"), Sp(), Sub("expr"), SqT(Lit(" in "), Sub("locals")), SqT(Lit(", "), Sub("globals"))))
+
+  val if_rule = "If" :=
     Sq(For("tests",
       Sq(PosKw("if", "elif"), Sp(), VarN(0), Lit(":"), CR(),
         Indent(),
@@ -87,9 +89,9 @@ object Python {
       SqT(Kw("else"), Lit(":"), CR(),
         Indent(),
         Sub("else_"),
-        Dedent())))
+        Dedent()))
 
-  val for_rule = new Rule("For", Sq(Kw("for"), Sp(), Sub("assign"), Sp(), Kw("in"), Sp(), Sub("list"), Lit(":"), CR(),
+  val for_rule = "For" := Sq(Kw("for"), Sp(), Sub("assign"), Sp(), Kw("in"), Sp(), Sub("list"), Lit(":"), CR(),
     Indent(),
     Sub("body"),
     Dedent(),
@@ -97,18 +99,18 @@ object Python {
       Indent(),
       Sub("else_"),
       Dedent())
-  ))
+  )
 
-  val ifexp_rule = new Rule("IfExp", Sq(Kw("if"), Sub("test"), Lit(":"), CR(),
+  val ifexp_rule = "IfExp" := Sq(Kw("if"), Sub("test"), Lit(":"), CR(),
     Indent(),
     Sub("then"),
     Dedent(),
     Kw("else"), Lit(":"), CR(),
     Indent(),
     Sub("else_"),
-    Dedent()))
+    Dedent())
 
-  val while_rule = new Rule("While", Sq(Kw("while"), Sp(), Sub("test"), Lit(":"), CR(),
+  val while_rule = "While" := Sq(Kw("while"), Sp(), Sub("test"), Lit(":"), CR(),
     Indent(),
     Sub("body"),
     Dedent(),
@@ -116,14 +118,14 @@ object Python {
       Indent(),
       Sub("else_"),
       Dedent())
-  ))
+  )
 
-  val with_rule = new Rule("With", Sq(Kw("with"), Sp(), Sub("expr"), Sp(), Kw("as"), SqT(Sp(), Sub("vars")), Lit(":"), CR(),
+  val with_rule = "With" := Sq(Kw("with"), Sp(), Sub("expr"), Sp(), Kw("as"), SqT(Sp(), Sub("vars")), Lit(":"), CR(),
     Indent(),
     Sub("body"),
-    Dedent()))
+    Dedent())
 
-  val tryexcept_rule = new Rule("TryExcept", Sq(Kw("try"), Lit(":"), CR(),
+  val tryexcept_rule = "TryExcept" := Sq(Kw("try"), Lit(":"), CR(),
     Indent(),
     Sub("body"),
     Dedent(),
@@ -137,9 +139,9 @@ object Python {
       Indent(),
       Sub("else_"),
       Dedent())
-  ))
+  )
 
-  val tryfinally_rule = new Rule("TryFinally", Sq(Kw("try"), Lit(":"), CR(),
+  val tryfinally_rule = "TryFinally" := Sq(Kw("try"), Lit(":"), CR(),
     Indent(),
     Sub("body"),
     Dedent(),
@@ -147,26 +149,26 @@ object Python {
     Indent(),
     Sub("final"),
     Dedent()
-  ))
+  )
 
-  val class_rule = new Rule("Class", Sq(Kw("class"), Sp(), Attr("name"), Emb("(", J("bases", Sq(Lit(","), Sp())), ")"), Lit(":"), CR(),
+  val class_rule = "Class" := Sq(Kw("class"), Sp(), Attr("name"), Emb("(", J("bases", Sq(Lit(","), Sp())), ")"), Lit(":"), CR(),
     CRH(),
     Indent(),
     SqT(Repr("doc"), CR()),
     Sub("code"),
     Dedent(), CRH()
-  ))
+  )
 
-  val function_rule = new Rule("Function", Sq(
+  val function_rule = "Function" := Sq(
     Sub("decorators"), // CR()), J decorators with CR?
     Kw("def"), Sp(), Attr("name"), Emb("(", Arglist(), ")"), Lit(":"), CR(),
     Indent(),
     SqT(Repr("doc"), CR()),
     Sub("code"),
     Dedent(), CRH()
-  ))
+  )
 
-  val decorators_rule = new Rule("Decorators", For("nodes", Sq(Lit("@"), Var(), CR())))
+  val decorators_rule = "Decorators" := For("nodes", Sq(Lit("@"), Var(), CR()))
 
   val compoundStatements: List[Rule] =
     if_rule :: for_rule :: ifexp_rule :: while_rule :: with_rule :: tryexcept_rule ::
@@ -190,7 +192,7 @@ object Python {
   )
 
   val parse: String => Option[AstNode] =
-    
+
     (code: String) => {
 
       val ast2json = "python2json.py"
