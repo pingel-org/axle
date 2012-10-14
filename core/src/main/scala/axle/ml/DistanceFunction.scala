2ce30f6bff5e853bf22bcc42c635c57db1933b3a
Adam Pingel <pingel@gmail.com>
2012-10-13 19:07:59 -0700

    cosine distance'

--- core/src/main/scala/axle/ml/DistanceFunction.scala
+++ core/src/main/scala/axle/ml/DistanceFunction.scala
@@ -8,28 +8,24 @@ object DistanceFunction {
   import axle.matrix.JblasMatrixFactory._ // TODO: generalize
   type M[T] = JblasMatrix[T]
 
+  def norm(row: M[Double]): Double = sqrt((0 until row.columns).map(i => square(row(0, i))).sum)
+
+  def dotProduct(row1: M[Double], row2: M[Double]): Double = 1.0
+
   trait DistanceFunction extends ((M[Double], M[Double]) => Double)
 
   // TODO: apply should assert(r1.isRowVector && r2.isRowVector && r1.length == r2.length)
 
   object EuclideanDistanceFunction extends DistanceFunction {
-    def apply(r1: M[Double], r2: M[Double]): Double = {
-      val dRow = r1 - r2
-      sqrt((0 until r1.columns).map(i => square(dRow(0, i))).sum)
-    }
+    def apply(r1: M[Double], r2: M[Double]): Double = norm(r1 - r2)
   }
 
   object ManhattanDistanceFunction extends DistanceFunction {
-    def apply(r1: M[Double], r2: M[Double]): Double = {
-      val dRow = r1 - r2
-      (0 until r1.columns).map(i => abs(dRow(0, i))).sum
-    }
+    def apply(r1: M[Double], r2: M[Double]): Double = (r1 - r2).toList.map(abs(_)).sum
   }
 
   object CosineDistanceFunction extends DistanceFunction {
-    def apply(r1: M[Double], r2: M[Double]): Double = {
-      1.0 // TODO
-    }
+    def apply(r1: M[Double], r2: M[Double]): Double = 1.0 - dotProduct(r1, r2) / (norm(r1) * norm(r2))
   }
 
 }
