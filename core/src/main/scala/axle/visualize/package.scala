e39ea33a72effd95f2e82d67e7f4ce77625ddec2
Adam Pingel <pingel@gmail.com>
2012-11-12 23:11:58 -0800

    fixing 2 compile errors in axle.visualize resulting from immutable graph refactoring

--- core/src/main/scala/axle/visualize/package.scala
+++ core/src/main/scala/axle/visualize/package.scala
@@ -17,14 +17,28 @@ package object visualize {
   implicit def enComponentJungUndirectedGraph[VP, EP](jug: JungUndirectedGraph[VP, EP]): Component =
     new JungUndirectedGraphVisualization().component(jug)
 
-  implicit def enComponentNativeUndirectedGraph[VP, EP](nug: NativeUndirectedGraph[VP, EP]): Component =
-    new JungUndirectedGraph[VP, EP](nug.vps, nug.ef)
+  implicit def enComponentNativeUndirectedGraph[VP, EP](nug: NativeUndirectedGraph[VP, EP]): Component = {
+
+    // TODO: Avoid very wastefully converting to Native vertices and back
+    val wrappedEf = (vs: Seq[JungUndirectedGraphVertex[VP]]) =>
+      nug.ef(vs.map(v => new NativeUndirectedGraphVertex(v.payload)))
+        .map({ case (nv1, nv2, ep) => (new JungUndirectedGraphVertex(nv1.payload), new JungUndirectedGraphVertex(nv2.payload), ep) })
+
+    new JungUndirectedGraph[VP, EP](nug.vps, wrappedEf)
+  }
 
   implicit def enComponentJungDirectedGraph[VP, EP](jdg: JungDirectedGraph[VP, EP]): Component =
     new JungDirectedGraphVisualization().component(jdg)
 
-  implicit def enComponentNativeDirectedGraph[VP, EP](ndg: NativeDirectedGraph[VP, EP]): Component =
-    new JungDirectedGraph[VP, EP](ndg.vps, ndg.ef)
+  implicit def enComponentNativeDirectedGraph[VP, EP](ndg: NativeDirectedGraph[VP, EP]): Component = {
+
+    // TODO: Avoid very wastefully converting to Native vertices and back
+    val wrappedEf = (vs: Seq[JungDirectedGraphVertex[VP]]) =>
+      ndg.ef(vs.map(v => new NativeDirectedGraphVertex(v.payload)))
+        .map({ case (nv1, nv2, ep) => (new JungDirectedGraphVertex(nv1.payload), new JungDirectedGraphVertex(nv2.payload), ep) })
+
+    new JungDirectedGraph[VP, EP](ndg.vps, wrappedEf)
+  }
 
   implicit def enComponentKMeansClassifier[T](
     classifier: KMeans.KMeansClassifier[T]): Component =
