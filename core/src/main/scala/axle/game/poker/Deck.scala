8cbc464d61363379b484b08eb0ac63d5dd0e9700
Adam Pingel <pingel@gmail.com>
2012-12-24 04:37:05 -0600

    poker hand ordering

--- core/src/main/scala/axle/game/poker/Deck.scala
+++ core/src/main/scala/axle/game/poker/Deck.scala
@@ -1,6 +1,8 @@
 package axle.game.poker
 
 import util.Random.shuffle
+import math.Ordering
+import math.Ordering.Implicits._
 
 sealed trait Suit
 object Spades extends Suit { override def toString() = "♠" }
@@ -9,8 +11,14 @@ object Clubs extends Suit { override def toString() = "♣" }
 object Hearts extends Suit { override def toString() = "♡" }
 
 sealed trait Rank {
+
   def asInt(): Int
 }
+
+class RankOrdering extends Ordering[Rank] {
+  def compare(a: Rank, b: Rank) = a.asInt.compare(b.asInt)
+}
+
 object R2 extends Rank {
   def asInt() = 2
   override def toString() = "2"
@@ -65,7 +73,59 @@ object Ace extends Rank {
 }
 
 case class Card(rank: Rank, suit: Suit) {
+
   override def toString() = rank.toString + suit.toString
+
+}
+
+class CardOrdering extends Ordering[Card] {
+
+  def compare(a: Card, b: Card) = {
+    Implicits.rankOrdering.compare(a.rank, b.rank)
+  }
+}
+
+case class Hand(cards: IndexedSeq[Card]) {
+
+  implicit val co = Implicits.cardOrdering
+
+  lazy val isFlush = cards.tail.forall(_.suit == cards.head.suit)
+  lazy val groups = cards.groupBy(_.rank).map({ case (k, v) => (k, v.size) }).values.toList.sorted.reverse
+  lazy val sortedHand = cards.sorted.reverse
+  lazy val isStraight = sortedHand.zipWithIndex.tail.forall({ case (c, i) => (sortedHand.head.rank.asInt - i) == c.rank.asInt })
+
+  def orderingStats() = (isFlush, isStraight, groups, sortedHand)
+
+  override def toString() = sortedHand.map(_.toString).mkString(" ")
+  
+}
+
+class HandOrdering extends Ordering[Hand] {
+
+  // royal flush
+  // straight flush
+  // four of a kind
+  // full house
+  // flush
+  // straight
+  // three of a kind
+  // two pair
+  // one pair
+
+  implicit val co = Implicits.cardOrdering
+
+  def compare(a: Hand, b: Hand) = {
+    val aos = a.orderingStats
+    val bos = b.orderingStats
+
+    if (aos == bos)
+      0
+    else if (aos < bos)
+      -1
+    else
+      1
+  }
+
 }
 
 case class Deck(cards: IndexedSeq[Card] = shuffle(Deck.cards)) {
@@ -76,7 +136,6 @@ case class Deck(cards: IndexedSeq[Card] = shuffle(Deck.cards)) {
 object Deck {
 
   val ranks = Vector(R2, R3, R4, R5, R6, R7, R8, R9, R10, Jack, Queen, King, Ace)
-
   val suits = Vector(Spades, Diamonds, Clubs, Hearts)
 
   val cards = for {
@@ -87,3 +146,12 @@ object Deck {
   }
 
 }
+
+object Implicits {
+
+  implicit val rankOrdering = new RankOrdering()
+  implicit val cardOrdering = new CardOrdering()
+  implicit val handOrdering = new HandOrdering()
+
+}
+
