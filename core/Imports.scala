25b47b372c27701c56f6be28cc9656162a8428fb
Adam Pingel <pingel@gmail.com>
2012-12-25 03:44:47 -0600

    rm old file

--- core/Imports.scala
+++
@@ -1,7 +0,0 @@
-
-//import org.pingel.axle.Axle._
-//import org.pingel.axle.Axle.VariousFunctions._
-//import org.pingel.axle.Axle.Functor._
-//import org.pingel.axle.Axle.Functor2._
-
-//import org.pingel.axle.quanta._
