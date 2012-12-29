9122c711e845d97ade2ca526f28322e29ed9b63c
Adam Pingel <pingel@gmail.com>
2012-12-29 04:49:16 -0800

    unused imports

--- core/src/main/scala/axle/game/poker/PokerPlayer.scala
+++ core/src/main/scala/axle/game/poker/PokerPlayer.scala
@@ -1,9 +1,6 @@
 package axle.game.poker
 
 import axle.game._
-import Stream.{cons, empty}
-import collection._
-import util.Random.nextInt
 
 abstract class PokerPlayer(id: String, _description: String)(implicit game: Poker)
 extends Player[Poker](id, _description) {
