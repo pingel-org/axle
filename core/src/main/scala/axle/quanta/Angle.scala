7df8a8f668af7f367b87ff4b5f45f08910e6383e
Adam Pingel <pingel@gmail.com>
2013-01-06 14:00:05 -0800

    Angle quantum

---
+++ core/src/main/scala/axle/quanta/Angle.scala
@@ -0,0 +1,58 @@
+package axle.quanta
+
+import java.math.BigDecimal
+import axle.graph._
+import math.Pi
+
+class Angle extends Quantum {
+
+  class AngleQuantity(
+    magnitude: BigDecimal = oneBD,
+    _unit: Option[Q] = None,
+    _name: Option[String] = None,
+    _symbol: Option[String] = None,
+    _link: Option[String] = None) extends Quantity(magnitude, _unit, _name, _symbol, _link)
+
+  type Q = AngleQuantity
+
+  def newUnitOfMeasurement(
+    name: Option[String] = None,
+    symbol: Option[String] = None,
+    link: Option[String] = None): AngleQuantity =
+    new AngleQuantity(oneBD, None, name, symbol, link)
+
+  def newQuantity(magnitude: BigDecimal, unit: AngleQuantity): AngleQuantity =
+    new AngleQuantity(magnitude, Some(unit), None, None, None)
+
+  def conversionGraph() = _conversionGraph
+
+  val wikipediaUrl = "http://en.wikipedia.org/wiki/Orders_of_magnitude_(length)"
+  // "http://en.wikipedia.org/wiki/Distance"
+
+  lazy val _conversionGraph = conversions(
+    List(
+      unit("degree", "°", Some("http://en.wikipedia.org/wiki/Degree_(angle)")),
+      unit("radian", "rad", Some("http://en.wikipedia.org/wiki/Radian")),
+      unit("circleDegrees", "circle", Some("http://en.wikipedia.org/wiki/Circle")),
+      unit("circleRadians", "circle", Some("http://en.wikipedia.org/wiki/Circle"))
+    ),
+    (vs: Seq[Vertex[AngleQuantity]]) => vs match {
+      case degree :: radian :: circleDegrees :: circleRadians :: Nil => withInverses(List(
+        (degree, circleDegrees, 360.0), // TODO: precision
+        (radian, circleRadians, 2.0 * Pi),
+        (circleDegrees, circleRadians, 1.0)
+      ))
+      case _ => Nil
+    }
+  )
+
+  lazy val radian = byName("radian")
+  lazy val rad = radian
+  lazy val degree = byName("degree")
+  lazy val ° = degree
+  lazy val circleDegrees = byName("circleDegrees")
+  lazy val circleRadians = byName("circleRadians")
+  
+}
+
+object Angle extends Angle()
--- core/src/main/scala/axle/quanta/Physics.scala
+++ core/src/main/scala/axle/quanta/Physics.scala
@@ -1,13 +1,25 @@
 package axle.quanta
 
+/**
+ * The "Physics" objects models the graph of units
+ * 
+ * See http://en.wikipedia.org/wiki/SI_derived_unit
+ * 
+ */
+
 object Physics {
 
   // The graph of quanta
 
+  // Basic:
+  //
   // mass
   // distance
   // information
   // time
+
+  // Derived:
+  //
   // area = distance * distance
   // volume = distance * distance * distance
   // speed = distance / time
