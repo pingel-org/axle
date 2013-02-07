36b6a72a3000b944148f8ed27a8d28b114f06688
Adam Pingel <pingel@gmail.com>
2013-02-07 14:48:05 -0500

    passes more tests

--- core/src/main/scala/axle/CrossProduct.scala
+++ core/src/main/scala/axle/CrossProduct.scala
@@ -7,23 +7,26 @@ class CrossProduct[E](collections: IndexedSeq[IndexedSeq[E]]) extends Iterable[L
 
   def iterator() = result.toIterator
 
-  lazy val result: Stream[List[E]] = tail(None, 0)
+  lazy val result: Stream[List[E]] = tail(collections.map(c => -1), 0)
 
   def current(indices: IndexedSeq[Int]) = collections.zip(indices).map({ case (c, i) => c(i) }).toList
 
-  def tail(indices0opt: Option[IndexedSeq[Int]], i0: Int): Stream[List[E]] =
-    indices0opt.map( indices0 =>
-      if (i0 == collections.size) {
-        empty
-      } else if (indices0(i0) + 1 < collections(i0).size ) {
-        val indices1 = indices0.updated(i0, indices0(i0) + 1)
-        cons(current(indices1), tail(Some(indices1), 0))
-      } else {
-        tail(Some(indices0.updated(i0, 0)), i0 + 1)
+  def tail(indices0: IndexedSeq[Int], i0: Int): Stream[List[E]] = 
+    if (i0 == collections.size) empty
+    else {
+      if( indices0(i0) == -1 ) {
+	if( collections(i0).size > 0 ) {
+          val indices1 = indices0.updated(i0, 0)
+          if(i0 < collections.size - 1) tail(indices1, i0 + 1)
+          else cons(current(indices1), tail(indices1, 0))
+        }
+        else empty
       }
-    ).getOrElse({
-      val indices1 = collections.map(c => 0)
-      cons(current(indices1), tail(Some(indices1), 0))
-    })
+      else if (indices0(i0) + 1 < collections(i0).size) {
+        val indices1 = indices0.updated(i0, indices0(i0) + 1)
+        cons(current(indices1), tail(indices1, 0))
+      }
+      else tail(indices0.updated(i0, 0), i0+1)
+    }
 
 }
