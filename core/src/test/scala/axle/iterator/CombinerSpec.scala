7df45f2646471a069cfa88b125f9c321cebbff10
Adam Pingel <pingel@gmail.com>
2013-02-07 17:57:53 -0500

    s/perm/comb/

--- core/src/test/scala/axle/iterator/CombinerSpec.scala
+++ core/src/test/scala/axle/iterator/CombinerSpec.scala
@@ -1,11 +1,13 @@
 package axle.iterator
 
+import axle._
+
 import org.specs2.mutable._
 
 class CombinerSpec extends Specification {
 
   "Combine (a, b) 2" in {
-    val cAB2 = Permutations(Vector("a", "b"), 2).toList
+    val cAB2 = new Combinations(Vector("a", "b"), 2).toList
     cAB2 must have size (1)
     cAB2 must contain(List("a", "b"))
   }
