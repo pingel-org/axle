8253615db9ffc3da14443964fe165e905a722e69
Adam Pingel <pingel@gmail.com>
2013-02-18 21:18:14 -0800

    small simplifications to enrichedmetricspace

--- core/src/main/scala/axle/algebra/EnrichedMetricSpace.scala
+++ core/src/main/scala/axle/algebra/EnrichedMetricSpace.scala
@@ -1,6 +1,7 @@
 package axle.algebra
 
 import spire.math._
+import spire.implicits._
 import spire.algebra.MetricSpace
 import axle._
 import collection._
@@ -8,19 +9,19 @@ import collection._
 class EnrichedMetricSpace[T: Manifest](space: MetricSpace[T, Real]) {
 
   import axle.matrix.JblasMatrixModule._
+  import space._
 
   def distanceMatrix(vectors: IndexedSeq[T]): Matrix[Double] = {
     val n = vectors.size
-    matrix(n, n, (r: Int, c: Int) => space.distance(vectors(r), vectors(c)).toDouble)
+    matrix(n, n, (r: Int, c: Int) => distance(vectors(r), vectors(c)).toDouble)
   }
 
-  def nMostSimilar(query: T, vectors: Iterator[T], c: Int): List[(Int, Double)] =
+  def nMostSimilar(query: T, vectors: Iterator[T], c: Int): List[(Int, Real)] =
     vectors.zipWithIndex
-      .map({ case (v, i) => (i, space.distance(query, v)) })
+      .map({ case (v, i) => (i, distance(query, v)) })
       .toList
       .sortBy(_._2)
       .take(c)
-      .map(d => (d._1, d._2.toDouble))
 
   //  def nMostSimilarReport(query: String, n: Int) = nMostSimilar(query, n)
   //    .map(is => (is._2, corpus(is._1))).map(sd => "%.4f %s".format(sd._1, sd._2)).mkString("\n")
@@ -35,10 +36,7 @@ class EnrichedMetricSpace[T: Manifest](space: MetricSpace[T, Real]) {
    */
 
   def triangleInequalityHolds(data: GenTraversable[T]): Boolean =
-    data.triples.∀({
-      case (a, b, c) =>
-        space.distance(a, b) + space.distance(b, c) >= space.distance(a, c)
-    })
+    data.triples.forall({ case (a, b, c) => distance(a, b) + distance(b, c) >= distance(a, c) })
 
 }
 
