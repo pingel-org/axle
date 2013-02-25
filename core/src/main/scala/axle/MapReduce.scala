5ce12d0a9d8c0918f5cf008fb24f80bacb755c3c
Adam Pingel <pingel@gmail.com>
2013-02-24 19:42:18 -0800

    count

--- core/src/main/scala/axle/MapReduce.scala
+++ core/src/main/scala/axle/MapReduce.scala
@@ -6,6 +6,12 @@ trait MapReduce {
 
   def mapReduce[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): immutable.Map[K, V]
 
+  def count[D, K](dit: Iterator[D], tokenizer: D => Seq[K]): Map[K, Int] =
+    mapReduce(
+      dit,
+      mapper = (d: D) => tokenizer(d).map((_, 1)),
+      reducer = (x: Int, y: Int) => x + y)
+  
 }
 
 object ScalaMapReduce extends MapReduce {
