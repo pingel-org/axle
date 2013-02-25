4f44842a7496f061b5091103efeb2541a1fe6dd2
Adam Pingel <pingel@gmail.com>
2013-02-24 19:45:20 -0800

    classes to support document clustering

---
+++ core/src/main/scala/axle/lx/Corpus.scala
@@ -0,0 +1,35 @@
+package axle.lx
+
+import axle.ScalaMapReduce._
+
+trait Corpus[D <: Document] {
+
+  def documents(): IndexedSeq[D]
+
+  val wordCutoff = 20
+  val maxBigrams = 200
+
+  val topWordCounts =
+    count(documents.iterator, (d: D) => d.tokens)
+      .toList.sortBy(_._2).reverse
+      .filter(_._2 > wordCutoff)
+  // .take(maxWords)
+
+  val topWords = topWordCounts.map(_._1)
+
+  override def toString(): String = {
+    "Corpus of " + documents.length + " documents.\n" +
+      "There are " + topWords.length + " unique words used more than " + wordCutoff + " time(s).\n" +
+      "Top 10 words: " + topWords.take(10).mkString(", ") + "\n" +
+      "Top 10 bigrams: " + topBigrams.take(10).mkString(", ")
+  }
+
+  lazy val topBigramCounts =
+    count(documents.iterator, (d: D) => d.tokens.zip(d.tokens.tail))
+      .toList.sortBy(_._2).reverse
+      .filter(_._2 > 1)
+      .take(maxBigrams)
+
+  lazy val topBigrams = topBigramCounts.map(_._1)
+
+}
---
+++ core/src/main/scala/axle/lx/Document.scala
@@ -0,0 +1,25 @@
+package axle.lx
+
+import axle.ScalaMapReduce._
+
+class Document(text: String) {
+
+  lazy val tokens = text
+    .replaceAll("""([\?!()\";\|\[\]\.\,'])""", " ")
+    .trim
+    .split("\\s+")
+    .toIndexedSeq
+
+  lazy val bigrams = tokens.zip(tokens.tail)
+
+  lazy val wordCounts = count(List(this).iterator, (d: Document) => d.tokens)
+  lazy val bigramCounts = count(List(this).iterator, (d: Document) => d.bigrams)
+
+  lazy val averageWordLength = tokens.map(_.length).sum / tokens.length.toDouble
+
+  def features[D <: Document](corpus: Corpus[D]): IndexedSeq[Double] =
+    corpus.topWords.map(wordCounts.get(_).getOrElse(0).toDouble).toIndexedSeq ++ Vector(averageWordLength)
+
+  def numFullFeatures(corpus: Corpus[Document]) = corpus.topWords.length + 1
+
+}
