a75677ee13c9eccf8eaaf2b2890b3ef6ca95e97f
Adam Pingel <pingel@gmail.com>
2013-02-28 22:49:05 -0800

    adding english stopwords

---
+++ core/src/main/scala/axle/lx/language/English.scala
@@ -0,0 +1,15 @@
+package axle.lx.language
+
+object English {
+
+  // From Lucene's list of stopwords:
+  
+  val stopWords = Set(
+    "a", "an", "and", "are", "as", "at", "be", "but", "by",
+    "for", "if", "in", "into", "is", "it",
+    "no", "not", "of", "on", "or", "such",
+    "that", "the", "their", "then", "there", "these",
+    "they", "this", "to", "was", "will", "with"
+  )
+
+}
