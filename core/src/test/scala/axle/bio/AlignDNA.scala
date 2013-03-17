3f9536b39ae79b0488d525693378bad401c6344a
Adam Pingel <pingel@gmail.com>
2013-03-17 13:52:57 -0700

    test axle.bio

---
+++ core/src/test/scala/axle/bio/AlignDNA.scala
@@ -0,0 +1,26 @@
+package axle.bio
+
+import org.specs2.mutable._
+
+class AlignDNA extends Specification {
+
+  "Needleman-Wunsch" should {
+    "work" in {
+
+      import NeedlemanWunsch._
+      
+      optimalAlignment("ATGCGGCC", "ATCGCCGG") must be equalTo ("ATGCGGCC--", "AT-C-GCCGG")
+    }
+  }
+
+  "Smith-Waterman" should {
+    "work" in {
+
+      import SmithWaterman._
+      
+      optimalAlignment("ACACACTA", "AGCACACA") must be equalTo ("A-CACACTA", "AGCACAC-A")
+    }
+  }
+  
+  
+}
