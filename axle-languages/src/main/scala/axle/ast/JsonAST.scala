1982e3a6dd33a22bc0088c40011f6827c134af13
Adam Pingel <pingel@gmail.com>
2013-04-06 12:52:42 -0700

    fixing warnings from erasure() method

--- axle-languages/src/main/scala/axle/ast/JsonAST.scala
+++ axle-languages/src/main/scala/axle/ast/JsonAST.scala
@@ -17,9 +17,9 @@ object Jackson {
   }
 
   private[this] def typeFromManifest(m: Manifest[_]): Type = {
-    if (m.typeArguments.isEmpty) m.erasure
+    if (m.typeArguments.isEmpty) m.runtimeClass
     else new ParameterizedType {
-      def getRawType = m.erasure
+      def getRawType = m.runtimeClass
 
       def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
 
