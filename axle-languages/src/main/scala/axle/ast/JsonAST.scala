313c2458fee5d1d6f17965e24ac0bf2a5d71e3dd
Adam Pingel <pingel@gmail.com>
2013-04-06 12:50:16 -0700

    fixing lineno calculation

--- axle-languages/src/main/scala/axle/ast/JsonAST.scala
+++ axle-languages/src/main/scala/axle/ast/JsonAST.scala
@@ -40,60 +40,49 @@ object JsonAST {
 
   def fromJson(json: String, parentLineNo: Int = 1): AstNode = obj2ast(parseJsonMap(json), parentLineNo)
 
-  def obj2ast(obj: Any, parentLineNo: Int): AstNode = {
+  def mapToRuleNode(m: Map[String, Any], parentLineNo: Int) = {
 
-//    println("Case for object: " + obj)
-//    if (obj != null) {
-//      println(".getClass.getName = " + obj.getClass.getName)
-//    }
+    val lineNo =
+      m.get("_lineno").flatMap(Option(_)).asInstanceOf[Option[Int]]
+        .getOrElse(parentLineNo)
 
-    obj match {
+    val metaMap = m.keySet.filter(field => (field != "type") && (field != "_lineno")).toList
+      .map(field => field -> obj2ast(m(field), lineNo))
+      .toMap
 
-      case x: Map[_, _] => {
-        val m = x.asInstanceOf[Map[String, Any]]
-        val lineNo = m.get("_lineno").asInstanceOf[Option[String]].map(_.toInt).getOrElse(parentLineNo)
-        val metaMap = m.keySet.filter(field => (field != "type") && (field != "_lineno")).toList
-          .map(field => field -> obj2ast(m(field), lineNo))
-          .toMap
-        AstNodeRule(
-          m.get("type").asInstanceOf[Option[String]].getOrElse(""),
-          metaMap,
-          lineNo
-        )
-      }
+    AstNodeRule(
+      m.get("type").asInstanceOf[Option[String]].getOrElse(""),
+      metaMap,
+      lineNo
+    )
+  }
 
-      case x: collection.convert.Wrappers$JMapWrapper => {
-        val m = x.toMap.asInstanceOf[Map[String, Any]]
-        val lineNo = m.get("_lineno").asInstanceOf[Option[String]].map(_.toInt).getOrElse(parentLineNo)
-        val metaMap = m.keySet.filter(field => (field != "type") && (field != "_lineno")).toList
-          .map(field => field -> obj2ast(m(field), lineNo))
-          .toMap
-        AstNodeRule(
-          m.get("type").asInstanceOf[Option[String]].getOrElse(""),
-          metaMap,
-          lineNo
-        )
-      }
+  def obj2ast(obj: Any, parentLineNo: Int): AstNode = obj match {
 
-      case arr: Array[_] => AstNodeList(arr.map(obj2ast(_, parentLineNo)).toList, parentLineNo)
+    case x: Map[_, _] =>
+      mapToRuleNode(x.asInstanceOf[Map[String, Any]], parentLineNo)
 
-      case buff: Buffer[_] => AstNodeList(buff.map(obj2ast(_, parentLineNo)).toList, parentLineNo)
+    case x: collection.convert.Wrappers$JMapWrapper =>
+      mapToRuleNode(x.toMap.asInstanceOf[Map[String, Any]], parentLineNo)
 
-      case i: Int => AstNodeValue(Some(i.toString), parentLineNo)
+    case arr: Array[_] => AstNodeList(arr.map(obj2ast(_, parentLineNo)).toList, parentLineNo)
 
-      case d: Double => AstNodeValue(Some(d.toString), parentLineNo)
+    case buff: Buffer[_] => AstNodeList(buff.map(obj2ast(_, parentLineNo)).toList, parentLineNo)
 
-      case s: String => AstNodeValue(Some(s.toString), parentLineNo)
+    case i: Int => AstNodeValue(Some(i.toString), parentLineNo)
 
-      case b: Boolean => AstNodeValue(Some(b.toString), parentLineNo)
+    case d: Double => AstNodeValue(Some(d.toString), parentLineNo)
 
-      case None => AstNodeValue(None, parentLineNo) // verify this
+    case s: String => AstNodeValue(Some(s.toString), parentLineNo)
 
-      case null => AstNodeValue(None, parentLineNo)
+    case b: Boolean => AstNodeValue(Some(b.toString), parentLineNo)
 
-      case z @ _ => throw new Exception("unable to fromJson: " + z)
+    case None => AstNodeValue(None, parentLineNo) // verify this
 
-    }
+    case null => AstNodeValue(None, parentLineNo)
+
+    case z @ _ => throw new Exception("unable to fromJson: " + z)
+
   }
 
 }
