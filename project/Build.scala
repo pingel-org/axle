44f2da30d76d4231294e88da786f333d4436300f
Adam Pingel <pingel@gmail.com>
2013-04-07 15:12:36 -0700

    back to snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object AxleBuild extends Build {
 
     organization := "org.pingel",
 
-    version := "0.1-M9",
+    version := "0.1-SNAPSHOT", // last milestone was M9
 
     scalaVersion := "2.10.1",
 
