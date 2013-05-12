2c580dab930c32b3899cf74abc5797ee00cdc31e
Adam Pingel <pingel@gmail.com>
2013-05-11 17:03:52 -0700

    fixing compareAlike

--- axle-games/src/main/scala/axle/game/poker/PokerHandCategory.scala
+++ axle-games/src/main/scala/axle/game/poker/PokerHandCategory.scala
@@ -11,13 +11,13 @@ sealed trait PokerHandCategory {
   def name(): String
   def describe(hand: PokerHand) = name() + " " + specifics(hand)
   def specifics(hand: PokerHand): String
+
   def compareAlike(a: PokerHand, b: PokerHand): Int =
-    if (a.sortedHand < b.sortedHand)
-      -1
-    else if (a.sortedHand > b.sortedHand)
-      1
-    else
-      0
+    a.groups.map(_._2).zip(b.groups.map(_._2))
+      .map({ case (ar, br) => rankOrdering.compare(ar, br) })
+      .find(_ != 0)
+      .getOrElse(0)
+
 }
 
 object RoyalFlush extends PokerHandCategory {
@@ -48,69 +48,63 @@ object FullHouse extends PokerHandCategory {
   def specifics(hand: PokerHand) = three(hand) + " over " + two(hand)
   def three(hand: PokerHand) = hand.groups(0)._2
   def two(hand: PokerHand) = hand.groups(1)._2
-  override def compareAlike(a: PokerHand, b: PokerHand): Int = {
-    val cmp3 = rankOrdering.compare(three(a), three(b))
-    if (cmp3 == 0)
-      rankOrdering.compare(two(a), two(b))
-    else
-      cmp3
-  }
+
 }
 
 object Flush extends PokerHandCategory {
+
   def asInt() = 5
   def name() = "flush"
   def specifics(hand: PokerHand) = "in " + suit(hand)
   def suit(hand: PokerHand) = hand.sortedHand(0).suit
+
 }
 
 object Straight extends PokerHandCategory {
+
   def asInt() = 4
   def name() = "straight"
   def specifics(hand: PokerHand) = "to " + to(hand)
   def to(hand: PokerHand) = hand.sortedHand(0).rank
+
 }
 
 object ThreeOfAKind extends PokerHandCategory {
+
   def asInt() = 3
   def name() = "three of a kind"
   def specifics(hand: PokerHand) = "of " + rank(hand)
   def rank(hand: PokerHand) = hand.groups(0)._2
+
 }
 
 object TwoPair extends PokerHandCategory {
+
   def asInt() = 2
   def name() = "two pair"
   def specifics(hand: PokerHand) = high(hand) + " and " + low(hand)
   def high(hand: PokerHand) = hand.groups(0)._2
   def low(hand: PokerHand) = hand.groups(1)._2
-  def remaining(hand: PokerHand) = hand.groups(2)._2
-
-  override def compareAlike(a: PokerHand, b: PokerHand): Int = {
-    val cmpH = rankOrdering.compare(high(a), high(b))
-    if (cmpH == 0) {
-      val cmpL = rankOrdering.compare(low(a), low(b))
-      if( cmpL == 0 ) {
-        rankOrdering.compare(remaining(a), remaining(b))
-      } else cmpL
-    } else cmpH
-  }
 
 }
 
 object Pair extends PokerHandCategory {
+
   def asInt() = 1
   def name() = "pair"
   def specifics(hand: PokerHand) = "of " + rank(hand)
   def rank(hand: PokerHand) = hand.groups(0)._2
+
 }
 
 object High extends PokerHandCategory {
+
   def asInt() = 0
   def name() = "high"
   override def describe(hand: PokerHand) = rank(hand) + " " + name()
   def specifics(hand: PokerHand) = "" + rank(hand)
   def rank(hand: PokerHand) = hand.sortedHand(0).rank
+
 }
 
 class PokerHandCategoryOrdering extends Ordering[PokerHandCategory] {
