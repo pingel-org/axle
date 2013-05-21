48cad60c269bcc384fdaf8ae445c74067d15bdfd
Adam Pingel <pingel@gmail.com>
2013-05-20 19:22:26 -0700

    avoid mapreduce pattern in favor of foldLeft

--- axle-core/src/main/scala/axle/nlp/DocumentVectorSpace.scala
+++ axle-core/src/main/scala/axle/nlp/DocumentVectorSpace.scala
@@ -4,7 +4,7 @@ package axle.nlp
  * Vector Space Model
  *
  * For calculating document similarity.
- * 
+ *
  * Note that distance and similarity are inverses
  *
  * http://en.wikipedia.org/wiki/Vector_space_model
@@ -16,31 +16,39 @@ package axle.nlp
 import axle._
 import spire.math._
 import spire.algebra.MetricSpace
+import collection._
 
 trait DocumentVectorSpace {
 
   import ScalaMapReduce.mapReduce
-  
+
   type TermVector = Map[String, Int]
 
   val whitespace = """\s+""".r
 
   def stopwords(): Set[String]
 
-  def mrWordCount(is: Iterator[String]): Map[String, Int] = mapReduce(
-    is,
-    mapper = (doc: String) => whitespace.split(doc).filter(!stopwords.contains(_)).map((_, 1)),
-    reducer = (v1: Int, v2: Int) => v1 + v2
-  )
+  def mrWordCount(is: Seq[String]): Map[String, Int] =
+    is.foldLeft(immutable.Map.empty[String, Int].withDefaultValue(0))({
+      case (tally, doc) =>
+        whitespace
+          .split(doc.toLowerCase)
+          .filter(!stopwords.contains(_))
+          .foldLeft(tally)({ case (tally, word) => tally + (word -> (tally(word) + 1)) })
+    })
 
-  def mrWordExistsCount(is: Iterator[String]): Map[String, Int] = mapReduce(
-    is,
-    mapper = (doc: String) => whitespace.split(doc).toSet.filter(!stopwords.contains(_)).toList.map((_, 1)),
-    reducer = (v1: Int, v2: Int) => v1 + v2
-  )
+  def mrWordExistsCount(is: Seq[String]): Map[String, Int] =
+    is.foldLeft(immutable.Map.empty[String, Int].withDefaultValue(0))({
+      case (tally, doc) =>
+        whitespace
+          .split(doc.toLowerCase)
+          .toSet
+          .filter(!stopwords.contains(_))
+          .foldLeft(tally)({ case (tally, word) => tally + (word -> (tally(word) + 1)) })
+    })
 
-  def doc2vector(doc: String): TermVector = mrWordCount(List(doc.toLowerCase).iterator).withDefaultValue(0)
-  
+  def doc2vector(doc: String): TermVector = mrWordCount(List(doc))
+
   def space(): MetricSpace[TermVector, Double]
 
 }
