5c7034b5a270d9aaf121f1c876591f9adbb2d857
Adam Pingel <pingel@gmail.com>
2013-05-24 17:08:31 -0400

    enough of hmm to draw one

--- axle-core/src/main/scala/axle/ml/HiddenMarkovModel.scala
+++ axle-core/src/main/scala/axle/ml/HiddenMarkovModel.scala
@@ -1,19 +1,46 @@
 package axle.ml
 
+import axle.graph._
+
 /**
- * 
- * 
+ *
  * http://en.wikipedia.org/wiki/Hidden_Markov_model
- * 
+ *
  */
 
-case class MarkovModelState(observable: Boolean)
+sealed trait MarkovModelState
 
-// Graph of MarkovModelState
-// where un-observables can be fully connected
-// and there are arrows from un-observables to observables
+case object MarkovModelStartState extends MarkovModelState {
+  override def toString() = "start"
+}
 
-class HiddenMarkovModel()
-{
+case class UnobservableMarkovModelState(label: String) extends MarkovModelState {
+  override def toString() = label
+}
+
+case class ObservableMarkovModelState(label: String) extends MarkovModelState {
+  override def toString() = label
+}
+
+class HiddenMarkovModel(
+  states: IndexedSeq[UnobservableMarkovModelState],
+  observations: IndexedSeq[ObservableMarkovModelState],
+  startProbability: Map[UnobservableMarkovModelState, Double],
+  transitionProbability: Map[UnobservableMarkovModelState, Map[UnobservableMarkovModelState, Double]], // can be fully-connected
+  emissionProbability: Map[UnobservableMarkovModelState, Map[ObservableMarkovModelState, Double]] // arrows from un-observables to observables
+  ) {
+
+  val startState = MarkovModelStartState
+
+  val graph = JungDirectedGraph[MarkovModelState, Double](
+    states ++ observations ++ List(startState),
+    (vs: Seq[Vertex[MarkovModelState]]) => {
+      val state2vertex = vs.map(v => (v.payload, v)).toMap
+      val startEdges = startProbability.map({ case (dest, p) => (state2vertex(startState), state2vertex(dest), p) })
+      val stateEdges = transitionProbability.flatMap({ case (from, toMap) => toMap.map({ case (to, p) => (state2vertex(from), state2vertex(to), p) }) })
+      val emissionEdges = emissionProbability.flatMap({ case (from, toMap) => toMap.map({ case (to, p) => (state2vertex(from), state2vertex(to), p) }) })
+      startEdges.toList ++ stateEdges.toList ++ emissionEdges.toList
+    }
+  )
 
 }
---
+++ axle-core/src/test/scala/axle/ml/HiddenMarkovModelSpecification.scala
@@ -0,0 +1,27 @@
+package axle.ml
+
+import org.specs2.mutable._
+
+class HiddenMarkovModelSpecification extends Specification {
+
+  val rainy = UnobservableMarkovModelState("Rainy")
+  val sunny = UnobservableMarkovModelState("Sunny")
+  val walk = ObservableMarkovModelState("walk")
+  val shop = ObservableMarkovModelState("shop")
+  val clean = ObservableMarkovModelState("clean")
+
+  val hmm = new HiddenMarkovModel(
+    Vector(rainy, sunny),
+    Vector(walk, shop, clean),
+    Map(rainy -> 0.6, sunny -> 0.4),
+    Map(
+      rainy -> Map(rainy -> 0.7, sunny -> 0.3),
+      sunny -> Map(rainy -> 0.4, sunny -> 0.6)
+    ),
+    Map(
+      rainy -> Map(walk -> 0.1, shop -> 0.4, clean -> 0.5),
+      sunny -> Map(walk -> 0.6, shop -> 0.3, clean -> 0.1)
+    )
+  )
+
+}
