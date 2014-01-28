b12779f7d1e3bb1e7e7ed56bea5a249a382b5af9
Adam Pingel <pingel@gmail.com>
2014-01-27 22:48:40 -0600

    applicative reorg

--- axle-core/src/test/scala/axle/algebra/ApplicativeLawsSpec.scala
+++ axle-core/src/test/scala/axle/algebra/ApplicativeLawsSpec.scala
@@ -4,7 +4,7 @@ class ApplicativeLawsSpec {
 
   // pure id <*> v = v
 
-  def checkAxiom1[F[_] : Applicative, T](v: T): Boolean = {
+  def checkAxiom1[F[_]: Applicative, T](v: T): Boolean = {
     //val applicative = implicitly[Applicative[F]]
     //applicative.pure(id <*> v) === v
     true
@@ -24,5 +24,30 @@ class ApplicativeLawsSpec {
 
   def checkAxiom4[F[_]: Applicative, T](v: T): Boolean =
     true
-  
+
 }
+
+/*
+    val p2 = pure2( { (x: Int) => x + 3 } )
+
+    val somePlus3 = Some( { (x: Int) => x + 3 } )
+
+    val a1 = somePlus3 <*> Some(9)
+
+    val a2 = pure2( { (x: Int) => x + 3 } ) <*> Some(10)
+
+    val a3 = pure2( { (x: Int) => x + 3 } ) <*> Some(9)
+
+    val a4 = Some( { (s: String) => s ++ "hahah" } ) <*> None
+
+    val a5 = None <*> Some("woot")
+
+    val intAdd = { (x: Int, y: Int) => x + y }
+    val stringAppend = { (s1: String, s2: String) => s1 ++ s2 }
+
+    val p4 = pure3( intAdd )
+
+    val a6 = ( pure( intAdd ) <*> Some(3) ) <*> Some(5)
+    val a7 = ( pure( intAdd ) <*> Some(3) ) <*> None
+    val a8 = ( pure( intAdd ) <*> None    ) <*> Some(5)
+*/
