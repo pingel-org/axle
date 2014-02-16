896a93815197fc412b72373e89ad0c09bb301868
Adam Pingel <pingel@gmail.com>
2014-02-15 20:43:02 -0800

    defining MapReduce in terms of Iterable (vs Iterator) gives access to groupBy, meaning that the toList can be avoided

--- axle-core/src/main/scala/axle/MapReduce.scala
+++ axle-core/src/main/scala/axle/MapReduce.scala
@@ -2,16 +2,15 @@ package axle
 
 trait MapReduce {
 
-  def apply[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V]
+  def apply[D, K, V](data: Iterable[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V]
 
 }
 
 object ScalaMapReduce extends MapReduce {
 
-  def apply[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V] =
+  def apply[D, K, V](data: Iterable[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V] =
     data
       .flatMap(mapper)
-      .toList // TODO inefficient
       .groupBy(_._1)
       .map({ case (k, v) => (k, v.map(_._2).reduce(reducer)) })
 
