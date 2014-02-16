65b699157224db78f24eab9f103a6112e8ce92be
Adam Pingel <pingel@gmail.com>
2014-02-15 20:36:54 -0800

    clean up MapReduce

--- axle-core/src/main/scala/axle/MapReduce.scala
+++ axle-core/src/main/scala/axle/MapReduce.scala
@@ -2,19 +2,13 @@ package axle
 
 trait MapReduce {
 
-  def mapReduce[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V]
-
-  def count[D, K](dit: Iterator[D], tokenizer: D => Seq[K]): Map[K, Int] =
-    mapReduce(
-      dit,
-      mapper = (d: D) => tokenizer(d).map((_, 1)),
-      reducer = (x: Int, y: Int) => x + y)
+  def apply[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V]
 
 }
 
 object ScalaMapReduce extends MapReduce {
 
-  def mapReduce[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V] =
+  def apply[D, K, V](data: Iterator[D], mapper: D => Seq[(K, V)], reducer: (V, V) => V): Map[K, V] =
     data
       .flatMap(mapper)
       .toList // TODO inefficient
