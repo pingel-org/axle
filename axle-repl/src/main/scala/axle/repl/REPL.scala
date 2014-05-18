c644e1b5fed76b80576dcc71eaadd8d8227da0ea
Adam Pingel <pingel@gmail.com>
2014-05-18 14:09:00 -0700

    concatenate initial imports before console input

--- axle-repl/src/main/scala/axle/repl/REPL.scala
+++ axle-repl/src/main/scala/axle/repl/REPL.scala
@@ -4,7 +4,9 @@ package axle.repl
 import scala.tools.nsc.Settings
 import scala.tools.nsc.interpreter.ILoop
 import scala.util.Properties.{ jdkHome, javaVersion, javaVmName }
-import spire.implicits._
+import scala.collection.JavaConverters._
+import java.io.{ BufferedReader, StringReader, PrintWriter }
+import jline.console.ConsoleReader
 
 object AxleRepl extends App {
 
@@ -13,32 +15,48 @@ object AxleRepl extends App {
   settings.usejavacp.value = true
   settings.deprecation.value = true
 
-//  val code = List(
-//    "axle._",
-//    "axle.algebra._",
-//    "axle.stats._",
-//    "axle.quanta._",
-//    "axle.graph._",
-//    "axle.matrix._",
-//    "axle.ml._",
-//    "axle.visualize._",
-//    "axle.ast._",
-//    "scala.collection._") map { imp =>
-//      s"import $imp\n"
-//    } mkString
+  val code = List(
+    "axle._",
+    "axle.algebra._",
+    "axle.stats._",
+    "axle.quanta._",
+    "axle.graph._",
+    "axle.matrix._",
+    "axle.ml._",
+    "axle.visualize._",
+    "axle.ast._",
+    "scala.collection._") map { imp => s"import $imp; " } mkString
 
-  val axleILoop = new AxleILoop()
+  val output = new PrintWriter(Console.out, true)
+    
+  val initialInput = new BufferedReader(new StringReader(code.trim + "\n"))
+    
+  val input = new BufferedReader(Console.in) {
+    var initialized = false
+    
+    override def readLine() = {
+      if(initialized) {
+        super.readLine()
+      } else {
+        initialized = true
+        initialInput.readLine()
+      }
+    }
+  }
+
+  val axleILoop = new AxleILoop(Some(input), output)
   axleILoop.process(settings)
 
 }
 
-class AxleILoop extends ILoop {
+class AxleILoop(in0: Option[BufferedReader], override protected val out: PrintWriter)
+  extends ILoop(in0, out) {
 
   override def prompt =
     System.getProperty("file.encoding").toLowerCase match {
-    case "utf-8" | "utf8" => "αχλε ↦ "
-    case _ => "axle > "
-  }
+      case "utf-8" | "utf8" => "αχλε ↦ "
+      case _ => "axle > "
+    }
 
   val versionString = "0.1"
 
