b4491c20a8b44a61ddcdb3d7d3ed98ccb0a860ca
Adam Pingel <pingel@gmail.com>
2014-05-18 12:32:04 -0700

    also look for utf8 for repl prompt; adjust welcome message to be more like scala's

--- axle-repl/src/main/scala/axle/repl/REPL.scala
+++ axle-repl/src/main/scala/axle/repl/REPL.scala
@@ -3,6 +3,7 @@ package axle.repl
 
 import scala.tools.nsc.Settings
 import scala.tools.nsc.interpreter.ILoop
+import scala.util.Properties.{ jdkHome, javaVersion, javaVmName }
 import spire.implicits._
 
 object AxleRepl extends App {
@@ -12,40 +13,41 @@ object AxleRepl extends App {
   settings.usejavacp.value = true
   settings.deprecation.value = true
 
+//  val code = List(
+//    "axle._",
+//    "axle.algebra._",
+//    "axle.stats._",
+//    "axle.quanta._",
+//    "axle.graph._",
+//    "axle.matrix._",
+//    "axle.ml._",
+//    "axle.visualize._",
+//    "axle.ast._",
+//    "scala.collection._") map { imp =>
+//      s"import $imp\n"
+//    } mkString
+
   val axleILoop = new AxleILoop()
   axleILoop.process(settings)
 
-  List(
-    "axle._",
-    "axle.algebra._",
-    "axle.stats._",
-    "axle.quanta._",
-    "axle.graph._",
-    "axle.matrix._",
-    "axle.ml._",
-    "axle.visualize._",
-    "axle.ast._",
-    "scala.collection._") foreach { imp =>
-      axleILoop.interpreter.interpret(s"import $imp")
-  }
-  
 }
 
 class AxleILoop extends ILoop {
 
   override def prompt =
-    if (System.getProperty("file.encoding").toLowerCase === "utf-8")
-      "αχλε ↦ "
-    else
-      "axle > "
-  
+    System.getProperty("file.encoding").toLowerCase match {
+    case "utf-8" | "utf8" => "αχλε ↦ "
+    case _ => "axle > "
+  }
+
+  val versionString = "0.1"
+
   override def printWelcome(): Unit = {
-    echo("""
-
-Welcome to axle
-
-  axle-lang.org
-
-""")
+    echo(s"""
+      |Welcome to Axle $versionString ($javaVmName, Java $javaVersion).
+      |axle-lang.org
+      |Type in expressions to have them evaluated.
+      |Type :help for more information.""".trim.stripMargin)
+    echo("[info] started at " + new java.util.Date)
   }
 }
