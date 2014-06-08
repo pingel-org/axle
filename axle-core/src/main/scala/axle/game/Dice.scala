982e49899d8931cc318e7ae3ca5efa886322cfc8
Adam Pingel <pingel@gmail.com>
2014-06-08 01:08:57 -0700

    d6 for Rational (but commented out)

--- axle-core/src/main/scala/axle/game/Dice.scala
+++ axle-core/src/main/scala/axle/game/Dice.scala
@@ -14,6 +14,12 @@ object Dice {
       Some(1 to n),
       distribution = Some(new ConditionalProbabilityTable0((1 to n).map(i => (i, Rational(1, n))).toMap)))
 
+//  def die(n: Natural): RandomVariable[Natural, Rational] =
+//    RandomVariable0[Natural, Rational](
+//      "d" + n,
+//      Some((1 to n.toInt).map(i => Natural(i)).toIndexedSeq),
+//      distribution = Some(new ConditionalProbabilityTable0((1 to n.toInt).map(i => (Natural(i), Rational(1, n))).toMap)))
+      
   val sixth = Rational(1, 6)
 
   def utfD6: RandomVariable[Symbol, Rational] = RandomVariable0("UTF d6",
