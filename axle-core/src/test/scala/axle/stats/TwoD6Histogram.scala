0987aca061bf13198b40dc1561db0cd5f4a7f599
Adam Pingel <pingel@gmail.com>
2014-06-24 01:57:48 -0500

    prune imports to avoid implicitly[Ring[Int]]

--- axle-core/src/test/scala/axle/stats/TwoD6Histogram.scala
+++ axle-core/src/test/scala/axle/stats/TwoD6Histogram.scala
@@ -1,11 +1,10 @@
 package axle.stats
 
-import org.specs2.mutable._
-import axle._
-import axle.game.Dice._
-import spire.math._
-import spire.implicits._
-import spire.algebra._
+import org.specs2.mutable.Specification
+
+import axle.enrichGenSeq
+import axle.game.Dice.die
+import spire.implicits.IntAlgebra
 
 object TwoD6Histogram extends Specification {
 
@@ -17,13 +16,9 @@ object TwoD6Histogram extends Specification {
 
       val rolls = (0 until 1000).map(i => d6a.observe.get + d6b.observe.get)
 
-      implicitly[Ring[Int]] // TODO this is here to prevent diverging expansion. to be diagnosed
-
       val hist = rolls.orderedTally
 
       hist.size must be equalTo 11
-      
-      1 must be equalTo 1
     }
   }
 
