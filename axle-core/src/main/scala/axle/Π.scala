b1052b1f9d2c94bf3acefb0f3bb535ed5c6a954a
Adam Pingel <pingel@gmail.com>
2014-06-28 17:37:20 -0400

    pass monoid.times to aggregate without (_, _)

--- axle-core/src/main/scala/axle/Π.scala
+++ axle-core/src/main/scala/axle/Π.scala
@@ -7,7 +7,7 @@ object Π {
 
   def apply[T, N: MultiplicativeMonoid](gt: GenTraversable[T])(f: T => N): N = {
     val monoid = implicitly[MultiplicativeMonoid[N]]
-    gt.aggregate(monoid.one)({ case (x, y) => monoid.times(x, f(y)) }, monoid.times(_, _))
+    gt.aggregate(monoid.one)({ case (x, y) => monoid.times(x, f(y)) }, monoid.times)
   }
 
 }
