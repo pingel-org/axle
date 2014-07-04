bd5963267d0dfe84181ae4aeaa5a4b81638ada02
Adam Pingel <pingel@gmail.com>
2014-07-03 21:23:15 -0700

    superficial/style

--- axle-core/src/test/scala/axle/stats/TwoD6Histogram.scala
+++ axle-core/src/test/scala/axle/stats/TwoD6Histogram.scala
@@ -14,7 +14,7 @@ object TwoD6Histogram extends Specification {
 
       val d6a = die(6)
       val d6b = die(6)
-      val rolls = (0 until 1000).map(i => d6a.observe + d6b.observe)
+      val rolls = (0 until 1000) map { i => d6a.observe + d6b.observe }
 
       val hist = rolls.tally
       hist.size must be equalTo 11
@@ -22,16 +22,16 @@ object TwoD6Histogram extends Specification {
   }
 
   "distribution monad" should {
-    "combined 2 D6 correctly" in {
+    "combine 2 D6 correctly" in {
 
-      val twoD6 = for {
+      val dice = for {
         a <- die(6)
         b <- die(6)
       } yield a + b
 
-      twoD6.probabilityOf(2) must be equalTo Rational(1, 36)
-      twoD6.probabilityOf(7) must be equalTo Rational(1, 6)
-      twoD6.probabilityOf(12) must be equalTo Rational(1, 36)
+      dice.probabilityOf(2) must be equalTo Rational(1, 36)
+      dice.probabilityOf(7) must be equalTo Rational(1, 6)
+      dice.probabilityOf(12) must be equalTo Rational(1, 36)
     }
 
   }
