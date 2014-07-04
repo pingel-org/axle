49b8a7cf6d2f1506a3d370010666f330a0275c26
Adam Pingel <pingel@gmail.com>
2014-07-04 01:25:25 -0700

    Distribution0.toMap

--- axle-core/src/main/scala/axle/stats/Distribution.scala
+++ axle-core/src/main/scala/axle/stats/Distribution.scala
@@ -20,10 +20,6 @@ trait Distribution[A, N] {
   def is(v: A): CaseIs[A, N]
 
   def isnt(v: A): CaseIsnt[A, N]
-  
-//  def is(v: A): CaseIs[A, N] = CaseIs(this, v)
-//
-//  def isnt(v: A): CaseIsnt[A, N] = CaseIsnt(this, v)
 
   def observe(): A
 
@@ -38,6 +34,8 @@ trait Distribution0[A, N] extends Distribution[A, N] {
 
   def flatMap[B](f: A => Distribution0[B, N]): Distribution0[B, N]
 
+  def toMap: Map[A, N] = values map { v => (v, probabilityOf(v)) } toMap
+  
   def show(implicit order: Order[A]): String
 
   //  def σ: Real = {
