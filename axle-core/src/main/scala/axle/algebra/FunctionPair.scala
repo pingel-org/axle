ed3383a22f3c862906a98f5504c7499e81ba8e6c
Adam Pingel <pingel@gmail.com>
2014-10-05 13:58:53 -0700

    FunctionPairs can be composed

--- axle-core/src/main/scala/axle/algebra/FunctionPair.scala
+++ axle-core/src/main/scala/axle/algebra/FunctionPair.scala
@@ -1,8 +1,26 @@
 package axle.algebra
 
-// not necessarily a bijection, but could be
+/**
+ * A FunctionPair is not necessarily a bijection, but it could be.
+ *
+ */
 
 trait FunctionPair[A, B] {
+
+  f =>
+
   def apply(a: A): B
+
   def unapply(b: B): A
+
+  def compose[C](g: FunctionPair[B, C]): FunctionPair[A, C] = new FunctionPair[A, C] {
+
+    lazy val fog = f.compose(g)
+
+    def apply(a: A): C = fog(a)
+
+    lazy val ufog = (f.unapply _).compose(g.unapply _)
+
+    def unapply(c: C): A = ufog(c)
+  }
 }
