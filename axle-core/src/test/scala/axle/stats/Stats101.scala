9e5dc65490f6b00b3773a07031a6f2cc876e8a00
Adam Pingel <pingel@gmail.com>
2014-10-05 23:45:54 -0700

    additional test for list of Reals

--- axle-core/src/test/scala/axle/stats/Stats101.scala
+++ axle-core/src/test/scala/axle/stats/Stats101.scala
@@ -8,11 +8,19 @@ import spire.implicits._
 class Stats101 extends Specification {
 
   "standard deviation" should {
-    "work" in {
 
-      val dist = uniformDistribution(List(2d, 4d, 4d, 4d, 5d, 5d, 7d, 9d), "numbers")
+    "work on a list of doubles" in {
+
+      val dist = uniformDistribution(List(2d, 4d, 4d, 4d, 5d, 5d, 7d, 9d), "some doubles")
 
       standardDeviation(dist) must be equalTo 2d
+    }
+
+    "work on a list of reals" in {
+
+      val dist = uniformDistribution(List[Real](2, 4, 4, 4, 5, 5, 7, 9), "some reals")
+
+      standardDeviation(dist) must be equalTo 2
     }
   }
 
