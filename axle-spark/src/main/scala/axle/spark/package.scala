96ca4113e6e1f0e536337658561e8ec49e7709b8
Adam Pingel <pingel@gmail.com>
2014-10-25 20:57:36 -0700

    sketch of RDD as Seq

--- axle-spark/src/main/scala/axle/spark/package.scala
+++ axle-spark/src/main/scala/axle/spark/package.scala
@@ -1,16 +1,48 @@
 package axle
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.Partition
+import org.apache.spark.TaskContext
+import scala.collection.GenTraversableOnce
+import scala.reflect.ClassTag
 
 package object spark {
 
-  implicit class RDDAsSeq[T](rdd: RDD[T]) extends Seq[T] {
+  implicit class RDDAsSeq[A](rdd: RDD[A])(implicit split: Partition, context: TaskContext)
+    extends Seq[A] {
 
-    def iterator: Iterator[T] = ???
+    def iterator: Iterator[A] =
+      rdd.iterator(split, context)
 
-    def apply(idx: Int): T = ???
-    
-    def length: Int = ???
+    def apply(idx: Int): A =
+      rdd.apply(idx)
+
+    def length: Int =
+      rdd.length
+
+    def map[B: ClassTag](f: A ⇒ B): RDD[B] =
+      rdd.map(f)
+
+    def flatMap[B: ClassTag](f: (A) ⇒ TraversableOnce[B]): RDD[B] =
+      rdd.flatMap { f }
+
+    override def filter(p: (A) ⇒ Boolean): Seq[A] =
+      rdd.filter { p }
+
+    def collect[B: ClassTag](pf: PartialFunction[A, B]): Seq[B] =
+      rdd.collect { pf }
+
+    //def reduce[A1 >: A](op: (A1, A1) => A1): A1 =
+    //  rdd.reduce { op }
+
+    //    override def foldLeft[B: ClassTag](z: B)(op: (B, A) ⇒ B): B =
+    //      rdd.foldLeft(z)(op)
+
+    def aggregate[B: ClassTag](z: ⇒ B)(seqOp: (B, A) ⇒ B, combOp: (B, B) ⇒ B): B =
+      rdd.aggregate(z)(seqOp, combOp)
+
+    def groupBy[B: ClassTag](f: A => B) =
+      rdd.groupBy { f }
 
   }
 
