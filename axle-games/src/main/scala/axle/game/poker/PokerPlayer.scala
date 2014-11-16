021fcfc4c9f09ef1b0670c39dda0befc16de209a
Adam Pingel <adam@axle-lang.org>
2014-11-16 11:35:24 -0800

    Show for PokerPlayer

--- axle-games/src/main/scala/axle/game/poker/PokerPlayer.scala
+++ axle-games/src/main/scala/axle/game/poker/PokerPlayer.scala
@@ -1,15 +1,21 @@
 package axle.game.poker
 
 import axle.game._
+import axle.Show
+import axle.string
 import spire.algebra.Eq
 
 object PokerPlayer {
 
+  implicit val ppShow: Show[PokerPlayer] = new Show[PokerPlayer] {
+    def text(p: PokerPlayer): String = p.description + " (" + p.id + ")"
+  }
+
   implicit def ppEq: Eq[PokerPlayer] = new Eq[PokerPlayer] {
     def eqv(x: PokerPlayer, y: PokerPlayer): Boolean = x.equals(y)
   }
-  
+
 }
 
 abstract class PokerPlayer(id: String, description: String)(implicit game: Poker)
-extends Player[Poker](id, description)
+  extends Player[Poker](id, description)
