41fb30e94d263ea8f5c3ea0853b404981ddb8e5b
Adam Pingel <adam@axle-lang.org>
2014-12-22 11:56:22 -0600

    forall should work on empty iterable

--- axle-core/src/main/scala/axle/forall.scala
+++ axle-core/src/main/scala/axle/forall.scala
@@ -5,6 +5,6 @@ import spire.algebra.BooleanAlgebra
 object forall {
 
   def apply[T, A](as: Iterable[T])(predicate: T => A)(implicit ev: BooleanAlgebra[A]): A =
-    as.map(predicate).reduce(ev.and) // TODO short-circuit
+    as.map(predicate).aggregate(ev.one)(ev.and, ev.and) // TODO short-circuit
 
 }
