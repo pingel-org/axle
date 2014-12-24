290034214543e4d03bac1b11064b6cd79beb526a
Adam Pingel <adam@axle-lang.org>
2014-12-23 21:48:35 -0600

    generalize ClassifierPerformance

--- axle-algorithms/src/main/scala/axle/ml/ClassifierPerformance.scala
+++ axle-algorithms/src/main/scala/axle/ml/ClassifierPerformance.scala
@@ -1,22 +1,5 @@
 package axle.ml
 
-/**
- * "performance" returns four measures of classification performance
- * for the given class.
- *
- * They are:
- *
- * 1. Precision
- * 2. Recall
- * 3. Specificity
- * 4. Accuracy
- *
- * See http://en.wikipedia.org/wiki/Precision_and_recall for more information.
- *
- * http://en.wikipedia.org/wiki/F1_score
- *
- */
-
 import axle.Show
 import axle._
 import axle.algebra.Σ
@@ -34,29 +17,56 @@ import spire.math._
 import spire.implicits._
 import spire.algebra._
 
-case class ClassifierPerformance[N: Field, DATA, F[_]: Aggregatable: Functor, CLASS: Eq](
+/**
+ * ClassifierPerformance computes measures of classification performance
+ *
+ * They are:
+ *
+ *   * Precision
+ *   * Recall
+ *   * Specificity
+ *   * Accuracy
+ *   * F1
+ *
+ * The "classification task" is defined by three arguments:
+ *
+ * 1) predict   : given a datum computes a value
+ * 2) isIn      : given the value, determines whether the value is "in" the retrieved set
+ * 3) shouldBeIn: given the value, determines whether the value is *actually* "in" the retrieved set
+ *
+ * See http://en.wikipedia.org/wiki/Precision_and_recall for more information.
+ *
+ * http://en.wikipedia.org/wiki/F1_score
+ *
+ */
+
+case class ClassifierPerformance[N: Field, DATA, F[_]: Aggregatable: Functor, CLASS](
   data: F[DATA],
   predict: DATA => CLASS,
-  actual: DATA => CLASS,
-  k: CLASS) {
+  isIn: CLASS => Boolean,
+  shouldBeIn: CLASS => Boolean) {
 
   val field = implicitly[Field[N]]
 
-  val scores = data.map(d => {
-    (actual(d) === k, predict(d) === k) match {
+  val scores = data.map { d =>
+    val prediction = predict(d)
+    (shouldBeIn(prediction), isIn(prediction)) match {
       case (true, true)   => (1, 0, 0, 0) // true positive
       case (false, true)  => (0, 1, 0, 0) // false positive
       case (false, false) => (0, 0, 1, 0) // false negative
       case (true, false)  => (0, 0, 0, 1) // true negative
     }
-  })
+  }
 
   val (tpInt, fpInt, fnInt, tnInt) = Σ(scores)
   val (tp, fp, fn, tn) = (field.fromInt(tpInt), field.fromInt(fpInt), field.fromInt(fnInt), field.fromInt(tnInt))
 
   val precision: N = tp / (tp + fp)
+
   val recall: N = tp / (tp + fn)
+
   val specificity: N = tn / (tn + fp)
+
   val accuracy: N = (tp + tn) / (tp + tn + fp + fn)
 
   def f1Score: N = 2 * (precision * recall) / (precision + recall)
