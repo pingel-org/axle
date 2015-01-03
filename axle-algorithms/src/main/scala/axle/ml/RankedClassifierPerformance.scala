0b7026121db640839668f8a9906cd933bf0e2ebe
Adam Pingel <adam@axle-lang.org>
2015-01-03 11:38:09 -0800

    parameterize number type for MAP@k

--- axle-algorithms/src/main/scala/axle/ml/RankedClassifierPerformance.scala
+++ axle-algorithms/src/main/scala/axle/ml/RankedClassifierPerformance.scala
@@ -4,7 +4,14 @@ import scala.math.min
 
 import axle.algebra.mean
 import axle.algebra.Σ
-import spire.implicits.DoubleAlgebra
+import axle.algebra.Aggregatable
+import axle.algebra.Finite
+import axle.algebra.Functor
+import axle.syntax.finite._
+import axle.syntax.functor._
+import spire.algebra.Field
+import spire.implicits._
+import scala.reflect.ClassTag
 
 object RankedClassifierPerformance {
 
@@ -13,22 +20,22 @@ object RankedClassifierPerformance {
    *
    */
 
-  def averagePrecisionAtK[T](actual: Seq[T], predicted: Seq[T], k: Int): Double = {
+  def averagePrecisionAtK[T, N: ClassTag](actual: Seq[T], predicted: Seq[T], k: Int)(implicit field: Field[N]): N = {
 
-    if (actual.length == 0) {
-      0d
+    if (actual.size == 0) {
+      field.zero
     } else {
 
       val cutOff = predicted.take(k)
 
-      val score =
-        Σ(cutOff
+      val score: N =
+        Σ[N, Seq](cutOff
           .zipWithIndex
           .filter({ case (p, i) => actual.contains(p) && (!cutOff.take(i).contains(p)) })
           .zipWithIndex
-          .map({ case ((_, i), h) => (h + 1d) / (i + 1d) }))
+          .map({ case ((_, i), h) => (field.fromInt(h) + field.one) / (field.fromInt(i) + field.one) }))
 
-      score / min(actual.length, k)
+      score / min(actual.size, k)
     }
   }
 
@@ -40,7 +47,11 @@ object RankedClassifierPerformance {
    *
    */
 
-  def meanAveragePrecisionAtK[T](actual: Seq[Seq[T]], predicted: Seq[Seq[T]], k: Int = 10): Double =
-    mean(actual.zip(predicted).map({ case (a, p) => averagePrecisionAtK(a, p, k) }))
+  // TODO F[_]: Functor: Aggregatable: Zipper
+  def meanAveragePrecisionAtK[T, N: ClassTag: Field](
+    actual: Seq[Seq[T]],
+    predicted: Seq[Seq[T]],
+    k: Int = 10): N =
+    mean(actual.zip(predicted).map({ case (a: Seq[T], p: Seq[T]) => averagePrecisionAtK[T, N](a, p, k) }))
 
 }
--- axle-algorithms/src/test/scala/axle/ml/RankedClassifierPerformanceSpec.scala
+++ axle-algorithms/src/test/scala/axle/ml/RankedClassifierPerformanceSpec.scala
@@ -1,6 +1,7 @@
 package axle.ml
 
 import org.specs2.mutable._
+import spire.math.Rational
 import RankedClassifierPerformance._
 
 /**
@@ -13,24 +14,24 @@ class RankedClassifierPerformanceSpec extends Specification {
   "Average Precision" should {
     "work" in {
 
-      averagePrecisionAtK(1 until 6, List(6, 4, 7, 1, 2), 2) must be equalTo 0.25
+      averagePrecisionAtK[Int, Rational](1 until 6, List(6, 4, 7, 1, 2), 2) must be equalTo Rational(1, 4)
 
-      averagePrecisionAtK(1 until 6, List(1, 1, 1, 1, 1), 5) must be equalTo 0.2
+      averagePrecisionAtK[Int, Rational](1 until 6, List(1, 1, 1, 1, 1), 5) must be equalTo Rational(1, 5)
 
-      averagePrecisionAtK(1 until 100, (1 until 21) ++ (200 until 600), 1) must be equalTo 1.0
+      averagePrecisionAtK[Int, Rational](1 until 100, (1 until 21) ++ (200 until 600), 1) must be equalTo Rational.one
     }
   }
 
   "Mean Average Precision" should {
     "work" in {
 
-      meanAveragePrecisionAtK(List(1 until 5), List(1 until 5), 3) must be equalTo 1.0
+      meanAveragePrecisionAtK[Int, Rational](List(1 until 5), List(1 until 5), 3) must be equalTo Rational.one
 
-      meanAveragePrecisionAtK(List(List(1, 3, 4), List(1, 2, 4), List(1, 3)), List(1 until 6, 1 until 6, 1 until 6), 3) must be equalTo 0.6851851851851851
+      meanAveragePrecisionAtK[Int, Rational](List(List(1, 3, 4), List(1, 2, 4), List(1, 3)), List(1 until 6, 1 until 6, 1 until 6), 3) must be equalTo Rational(37, 54) // 0.6851851851851851
 
-      meanAveragePrecisionAtK(List(1 until 6, 1 until 6), List(List(6, 4, 7, 1, 2), List(1, 1, 1, 1, 1)), 5) must be equalTo 0.26
+      meanAveragePrecisionAtK[Int, Rational](List(1 until 6, 1 until 6), List(List(6, 4, 7, 1, 2), List(1, 1, 1, 1, 1)), 5) must be equalTo Rational(13, 50)
 
-      meanAveragePrecisionAtK(List(List(1, 3), List(1, 2, 3), List(1, 2, 3)), List(1 until 6, List(1, 1, 1), List(1, 2, 1)), 3) must be equalTo 0.611111111111111
+      meanAveragePrecisionAtK[Int, Rational](List(List(1, 3), List(1, 2, 3), List(1, 2, 3)), List(1 until 6, List(1, 1, 1), List(1, 2, 1)), 3) must be equalTo Rational(11, 18) // 0.611111111111111
     }
   }
 
