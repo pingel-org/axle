aed44de59415762bf0cb0461be4a6572ecabca4c
Adam Pingel <adam@axle-lang.org>
2015-03-07 16:16:37 -0800

    axle.quanta.conversionGraph

--- axle-core/src/main/scala/axle/quanta/Quantum.scala
+++ axle-core/src/main/scala/axle/quanta/Quantum.scala
@@ -28,39 +28,3 @@ trait Quantum4[N] {
   def links(implicit fn: Field[N]): Seq[(UnitOfMeasurement4[Q, N], UnitOfMeasurement4[Q, N], Bijection[N, N])]
 
 }
-
-  //  private[quanta] def trip2fns[N: Field: Eq](trip: (Vertex[UnitOfMeasurement[Q, N]], Vertex[UnitOfMeasurement[Q, N]], N)): Seq[(Vertex[UnitOfMeasurement[Q, N]], Vertex[UnitOfMeasurement[Q, N]], N => N)] = {
-  //    val (from, to, multiplier) = trip
-  //    Vector(
-  //      (from, to, _ * multiplier),
-  //      (to, from, _ / multiplier))
-  //  }
-  //  
-  //  private[quanta] def trips2fns[N: Field: Eq](trips: Seq[(Vertex[UnitOfMeasurement[Q, N]], Vertex[UnitOfMeasurement[Q, N]], N)]) =
-  //    trips.flatMap(trip2fns(_))
-  //
-  //  def cgnDisconnected[N: Field: Eq, DG[_, _]: DirectedGraph]: CG[DG, N] = conversions(units, (vs: Seq[Vertex[UnitOfMeasurement[Q, N]]]) => Nil)
-
-//  private def conversions[Q <: Quantum, N, DG[_, _]](
-//    vps: Seq[UnitOfMeasurement[Q, N]],
-//    ef: Seq[Vertex[UnitOfMeasurement[Q, N]]] => Seq[(Vertex[UnitOfMeasurement[Q, N]], Vertex[UnitOfMeasurement[Q, N]], N => N)])(implicit evDG: DirectedGraph[DG]): DG[UnitOfMeasurement[Q, N], N => N] =
-//    evDG.make[UnitOfMeasurement[Q, N], N => N](vps, ef)
-
-//  private def cgn[N, DG[_, _]: DirectedGraph](
-//    units: List[UnitOfMeasurement[Q, N]],
-//    links: Seq[(UnitOfMeasurement[Q, N], UnitOfMeasurement[Q, N], Bijection[N, N])]): CG[Q, DG, N] =
-//    conversions[Q, N, DG](
-//      units,
-//      (vs: Seq[Vertex[UnitOfMeasurement[Q, N]]]) => {
-//        val name2vertex = vs.map(v => (v.payload.name, v)).toMap
-//        links.flatMap({
-//          case (x, y, bijection) => {
-//            val xv = name2vertex(x.name)
-//            val yv = name2vertex(y.name)
-//            List((xv, yv, bijection.apply _), (yv, xv, bijection.unapply _))
-//          }
-//        })
-//      })
-
-//  implicit def conversionGraph[N: Field: Eq, DG[_, _]: DirectedGraph] =
-//    cgn[N, DG](units[N], links[N])
