ed347041ea777ddd3547269f138782b95eb766ef
Adam Pingel <adam@axle-lang.org>
2015-03-08 23:14:39 -0700

    adding more methods to QuantumMetadata extensions

--- axle-core/src/main/scala/axle/quanta/Time.scala
+++ axle-core/src/main/scala/axle/quanta/Time.scala
@@ -7,6 +7,19 @@ import spire.algebra.Field
 
 import spire.math.Rational
 
+//  //  lazy val globalLifeExpectancy = 67.2 *: year // Some("2010 global average life expectancy"), None, Some("http://en.wikipedia.org/wiki/Life_expectancy"))
+
+//  //  // Distant Past:
+//  //  lazy val universeAge = 13.7 *: gy // Some("universe age"), None, Some("http://en.wikipedia.org/wiki/Age_of_the_Universe"))
+//  //  lazy val earthAge = 4.54 *: gy // Some("earth age"), None, Some("http://en.wikipedia.org/wiki/Age_of_the_Earth"))
+//  //  lazy val simpleCellsAge = 3.8 *: gy // Some("simple cells evolve"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val multiCellularLifeAge = 1 *: gy // Some("multi-cellular life evolves"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val fungiAge = 560 *: my // Some("kingdom Fungi age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val classMammalAge = 215 *: my // Some("class Mammalia age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val primateAge = 60 *: my // Some("order Primate age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val australopithecusAge = 4 *: my // Some("genus Australopithecus age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+//  //  lazy val modernHumanAge = 200 *: ky // Some("anatomically modern human age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+
 case class Time() extends Quantum {
 
   def wikipediaUrl: String = "http://en.wikipedia.org/wiki/Orders_of_magnitude_(time)"
@@ -17,6 +30,37 @@ trait TimeMetadata[N] extends QuantumMetadata[Time, N] {
 
   type U = UnitOfMeasurement[Time, N]
 
+  def second: U
+  def s: U
+  def millisecond: U
+  def ms: U
+  def microsecond: U
+  def μs: U
+  def nanosecond: U
+  def ns: U
+  def picosecond: U
+  def ps: U
+  def femtosecond: U
+  def fs: U
+  def attosecond: U
+  def as: U
+  def zeptosecond: U
+  def zs: U
+  def yoctosecond: U
+  def ys: U
+  def minute: U
+  def m: U
+  def hour: U
+  def day: U
+  def year: U
+  def century: U
+  def millenium: U
+  def ky: U
+  def megayear: U
+  def my: U
+  def gigayear: U
+  def gy: U
+
 }
 
 object Time {
@@ -26,68 +70,54 @@ object Time {
     def unit(name: String, symbol: String, wiki: Option[String] = None) =
       UnitOfMeasurement[Time, N](name, symbol, wiki)
 
-    lazy val second = unit("second", "s", Some("http://en.wikipedia.org/wiki/Second"))
-    lazy val s = second
-    lazy val millisecond = unit("millisecond", "ms", Some("http://en.wikipedia.org/wiki/Millisecond"))
-    lazy val ms = millisecond
-    lazy val microsecond = unit("microsecond", "μs", Some("http://en.wikipedia.org/wiki/Microsecond"))
-    lazy val μs = microsecond
-    lazy val nanosecond = unit("nanosecond", "ns", Some("http://en.wikipedia.org/wiki/Nanosecond"))
-    lazy val ns = nanosecond
-    lazy val picosecond = unit("picosecond", "ps", Some("http://en.wikipedia.org/wiki/Picosecond"))
-    lazy val ps = picosecond
-    lazy val femtosecond = unit("femtosecond", "fs", Some("http://en.wikipedia.org/wiki/Femtosecond"))
-    lazy val fs = femtosecond
-    lazy val attosecond = unit("attosecond", "as", Some("http://en.wikipedia.org/wiki/Attosecond"))
-    lazy val as = attosecond
-    lazy val zeptosecond = unit("zeptosecond", "zs", Some("http://en.wikipedia.org/wiki/Zeptosecond"))
-    lazy val zs = zeptosecond
-    lazy val yoctosecond = unit("yoctosecond", "ys", Some("http://en.wikipedia.org/wiki/Yoctosecond"))
-    lazy val ys = yoctosecond
-    lazy val minute = unit("minute", "m", Some("http://en.wikipedia.org/wiki/Minute"))
-    lazy val m = minute
-    lazy val hour = unit("hour", "hr", Some("http://en.wikipedia.org/wiki/Hour"))
-    lazy val day = unit("day", "d", Some("http://en.wikipedia.org/wiki/Day"))
-    lazy val year = unit("year", "yr", Some("http://en.wikipedia.org/wiki/Year"))
-    lazy val century = unit("century", "century", Some("http://en.wikipedia.org/wiki/Century"))
-    lazy val millenium = unit("millenium", "ky", Some("http://en.wikipedia.org/wiki/Millenium"))
-    lazy val ky = millenium
-    lazy val megayear = unit("megayear", "my")
-    lazy val my = megayear
-    lazy val gigayear = unit("gigayear", "gy")
-    lazy val gy = gigayear
+    lazy val _second = unit("second", "s", Some("http://en.wikipedia.org/wiki/Second"))
+    lazy val _millisecond = unit("millisecond", "ms", Some("http://en.wikipedia.org/wiki/Millisecond"))
+    lazy val _microsecond = unit("microsecond", "μs", Some("http://en.wikipedia.org/wiki/Microsecond"))
+    lazy val _nanosecond = unit("nanosecond", "ns", Some("http://en.wikipedia.org/wiki/Nanosecond"))
+    lazy val _picosecond = unit("picosecond", "ps", Some("http://en.wikipedia.org/wiki/Picosecond"))
+    lazy val _femtosecond = unit("femtosecond", "fs", Some("http://en.wikipedia.org/wiki/Femtosecond"))
+    lazy val _attosecond = unit("attosecond", "as", Some("http://en.wikipedia.org/wiki/Attosecond"))
+    lazy val _zeptosecond = unit("zeptosecond", "zs", Some("http://en.wikipedia.org/wiki/Zeptosecond"))
+    lazy val _yoctosecond = unit("yoctosecond", "ys", Some("http://en.wikipedia.org/wiki/Yoctosecond"))
+    lazy val _minute = unit("minute", "m", Some("http://en.wikipedia.org/wiki/Minute"))
+    lazy val _hour = unit("hour", "hr", Some("http://en.wikipedia.org/wiki/Hour"))
+    lazy val _day = unit("day", "d", Some("http://en.wikipedia.org/wiki/Day"))
+    lazy val _year = unit("year", "yr", Some("http://en.wikipedia.org/wiki/Year"))
+    lazy val _century = unit("century", "century", Some("http://en.wikipedia.org/wiki/Century"))
+    lazy val _millenium = unit("millenium", "ky", Some("http://en.wikipedia.org/wiki/Millenium"))
+    lazy val _megayear = unit("megayear", "my")
+    lazy val _gigayear = unit("gigayear", "gy")
 
-    //  def units[N] = List[UnitOfMeasurement[Q, N]](
-    //    unit("second", "s", Some("http://en.wikipedia.org/wiki/Second")),
-    //    unit("millisecond", "ms", Some("http://en.wikipedia.org/wiki/Millisecond")),
-    //    unit("microsecond", "μs", Some("http://en.wikipedia.org/wiki/Microsecond")),
-    //    unit("nanosecond", "ns", Some("http://en.wikipedia.org/wiki/Nanosecond")),
-    //    unit("picosecond", "ps", Some("http://en.wikipedia.org/wiki/Picosecond")),
-    //    unit("femtosecond", "fs", Some("http://en.wikipedia.org/wiki/Femtosecond")),
-    //    unit("attosecond", "as", Some("http://en.wikipedia.org/wiki/Attosecond")),
-    //    unit("zeptosecond", "zs", Some("http://en.wikipedia.org/wiki/Zeptosecond")),
-    //    unit("yoctosecond", "ys", Some("http://en.wikipedia.org/wiki/Yoctosecond")),
-    //    unit("minute", "m", Some("http://en.wikipedia.org/wiki/Minute")),
-    //    unit("hour", "hr", Some("http://en.wikipedia.org/wiki/Hour")),
-    //    unit("day", "d", Some("http://en.wikipedia.org/wiki/Day")),
-    //    unit("year", "yr", Some("http://en.wikipedia.org/wiki/Year")),
-    //    unit("century", "century", Some("http://en.wikipedia.org/wiki/Century")),
-    //    unit("millenium", "ky", Some("http://en.wikipedia.org/wiki/Millenium")),
-    //    unit("megayear", "my"),
-    //    unit("gigayear", "gy"))
-
-    //  //  lazy val globalLifeExpectancy = 67.2 *: year // Some("2010 global average life expectancy"), None, Some("http://en.wikipedia.org/wiki/Life_expectancy"))
-
-    //  //  // Distant Past:
-    //  //  lazy val universeAge = 13.7 *: gy // Some("universe age"), None, Some("http://en.wikipedia.org/wiki/Age_of_the_Universe"))
-    //  //  lazy val earthAge = 4.54 *: gy // Some("earth age"), None, Some("http://en.wikipedia.org/wiki/Age_of_the_Earth"))
-    //  //  lazy val simpleCellsAge = 3.8 *: gy // Some("simple cells evolve"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val multiCellularLifeAge = 1 *: gy // Some("multi-cellular life evolves"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val fungiAge = 560 *: my // Some("kingdom Fungi age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val classMammalAge = 215 *: my // Some("class Mammalia age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val primateAge = 60 *: my // Some("order Primate age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val australopithecusAge = 4 *: my // Some("genus Australopithecus age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
-    //  //  lazy val modernHumanAge = 200 *: ky // Some("anatomically modern human age"), None, Some("http://en.wikipedia.org/wiki/Timeline_of_evolution"))
+    def second = _second
+    def s = _second
+    def millisecond = _millisecond
+    def ms = _millisecond
+    def microsecond = _microsecond
+    def μs = _microsecond
+    def nanosecond = _nanosecond
+    def ns = _nanosecond
+    def picosecond = _picosecond
+    def ps = _picosecond
+    def femtosecond = _femtosecond
+    def fs = _femtosecond
+    def attosecond = _attosecond
+    def as = _attosecond
+    def zeptosecond = _zeptosecond
+    def zs = _zeptosecond
+    def yoctosecond = _yoctosecond
+    def ys = _yoctosecond
+    def minute = _minute
+    def m = _minute
+    def hour = _hour
+    def day = _day
+    def year = _year
+    def century = _century
+    def millenium = _millenium
+    def ky = _millenium
+    def megayear = _megayear
+    def my = _megayear
+    def gigayear = _gigayear
+    def gy = _gigayear
 
     def units: List[UnitOfMeasurement[Time, N]] =
       List(second, millisecond, microsecond, nanosecond, picosecond, femtosecond, attosecond,
