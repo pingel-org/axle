9a8653f2e9e524b9c0b5554f24d52239c953fba3
Adam Pingel <adam@axle-lang.org>
2015-03-09 00:24:23 -0700

    whitespace

--- project/Build.scala
+++ project/Build.scala
@@ -51,7 +51,7 @@ import axle._
     publishTo <<= version { (v: String) =>
       val nexus = "https://oss.sonatype.org/"
       if (v.trim.endsWith("SNAPSHOT")) 
-        Some("snapshots" at nexus + "content/repositories/snapshots") 
+        Some("snapshots" at nexus + "content/repositories/snapshots")
       else
         Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
