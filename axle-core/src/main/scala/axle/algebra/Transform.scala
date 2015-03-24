94d78a5c37f0798f456306f74ca3ff8b6cbfe72d
Adam Pingel <adam@axle-lang.org>
2015-03-23 23:21:03 -0700

    fix order of arguments to group.op{,Inverse} in Transform

--- axle-core/src/main/scala/axle/algebra/Transform.scala
+++ axle-core/src/main/scala/axle/algebra/Transform.scala
@@ -6,8 +6,8 @@ import spire.implicits._
 
 case class Transform[N](t: N)(implicit group: AbGroup[N]) extends Bijection[N, N] {
 
-  def apply(n: N): N = group.op(t, n)
+  def apply(n: N): N = group.op(n, t)
 
-  def unapply(n: N): N = group.opInverse(t, n)
+  def unapply(n: N): N = group.opInverse(n, t)
 
 }
