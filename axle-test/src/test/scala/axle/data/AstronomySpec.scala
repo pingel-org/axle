323cbe8f7789001700ce8b93e09d06b1808df837
Adam Pingel <adam@axle-lang.org>
2015-04-11 16:13:01 -0500

    fix AstronomySpec

--- axle-test/src/test/scala/axle/data/AstronomySpec.scala
+++ axle-test/src/test/scala/axle/data/AstronomySpec.scala
@@ -8,6 +8,7 @@ import spire.algebra.Module
 import spire.math.Rational
 import axle.quanta.Mass
 import axle.quanta.Distance
+import axle.quanta.Time
 import axle.jung.JungDirectedGraph
 import spire.algebra.Rng
 import spire.algebra.AdditiveGroup
@@ -22,6 +23,7 @@ class AstronomySpec extends Specification {
 
       implicit val md = Mass.converterGraph[Double, JungDirectedGraph]
       implicit val dd = Distance.converterGraph[Double, JungDirectedGraph]
+      implicit val td = Time.converterGraph[Double, JungDirectedGraph]
       val astro = axle.data.Astronomy()
       val sorted = astro.bodies.sortBy(_.mass)
 
