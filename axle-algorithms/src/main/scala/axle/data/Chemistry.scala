abca793f0e650665f61df812cfccc183ef9b6b81
Adam Pingel <adam@axle-lang.org>
2015-04-12 18:51:06 -0700

    avogadro constant

--- axle-algorithms/src/main/scala/axle/data/Chemistry.scala
+++ axle-algorithms/src/main/scala/axle/data/Chemistry.scala
@@ -4,6 +4,8 @@ import axle.quanta._
 
 case class Chemistry()(implicit tc: TemperatureConverter[Double]) {
 
+  lazy val avogadroConstant = BigDecimal("6.02214129E23")
+  
   import tc._
 
   lazy val waterBoilingPoint = 100d *: celsius
