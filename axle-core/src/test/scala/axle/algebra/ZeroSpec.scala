25c726b8180904ba24fdc05818d1f5cefc0ce922
Adam Pingel <adam@axle-lang.org>
2015-04-19 22:19:59 -0700

    more tests for axle.algebra.Zero

--- axle-core/src/test/scala/axle/algebra/ZeroSpec.scala
+++ axle-core/src/test/scala/axle/algebra/ZeroSpec.scala
@@ -2,14 +2,26 @@ package axle.algebra
 
 import org.specs2.mutable.Specification
 
-import spire.implicits.DoubleAlgebra
-
 class ZeroSpec extends Specification {
 
   "Zero[Double]" should {
-    "work" in {
-
+    "be available" in {
+      import spire.implicits.DoubleAlgebra
       Zero[Double].zero must be equalTo 0d
+    }
+  }
+
+  "Zero[Int]" should {
+    "be available" in {
+      import spire.implicits.IntAlgebra
+      Zero[Int].zero must be equalTo 0
+    }
+  }
+
+  "Zero[Long]" should {
+    "be available" in {
+      import spire.implicits.LongAlgebra
+      Zero[Long].zero must be equalTo 0L
     }
   }
 
