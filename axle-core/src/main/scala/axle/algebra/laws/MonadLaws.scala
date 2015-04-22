69029c543de2a1246b7afcc7ca09754a871eb7c7
Adam Pingel <adam@axle-lang.org>
2015-04-21 23:15:38 -0700

    monad laws

---
+++ axle-core/src/main/scala/axle/algebra/laws/MonadLaws.scala
@@ -0,0 +1,61 @@
+package axle.algebra.laws
+
+import org.typelevel.discipline.Laws
+import axle.algebra.Monad
+import scala.reflect.ClassTag
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.propBoolean
+import org.typelevel.discipline.Laws
+import spire.algebra.Eq
+import spire.implicits.eqOps
+
+object MonadLaws {
+
+  def apply[M[_], A] = new MonadLaws[M, A] {}
+}
+
+trait MonadLaws[M[_], A] extends Laws {
+
+  trait MonadRuleSet extends RuleSet {
+
+    def name: String = "monad"
+
+    def bases: Seq[(String, Laws#RuleSet)] = Seq()
+
+    def parents: Seq[RuleSet] = Seq.empty
+
+  }
+
+  def leftIdentity[B](implicit monad: Monad[M], arbMa: Arbitrary[M[A]], arbA: Arbitrary[A], arbAMb: Arbitrary[A => M[B]], eqMb: Eq[M[B]]) =
+    new MonadRuleSet {
+      def props: Seq[(String, Prop)] = Seq(
+        "left identity" → forAll { (m: M[A], x: A, f: A => M[B]) =>
+          val lhs: M[B] = monad.bind(monad.unit(x))(f)
+          val rhs: M[B] = f(x)
+          lhs === rhs
+        })
+    }
+
+  def rightIdentity(implicit monad: Monad[M], arbMa: Arbitrary[M[A]], eqMa: Eq[M[A]]) =
+    new MonadRuleSet {
+      def props: Seq[(String, Prop)] = Seq(
+        "right identity" → forAll { (m: M[A]) =>
+          val lhs: M[A] = monad.bind(m)(monad.unit)
+          val rhs: M[A] = m
+          lhs === rhs
+        })
+    }
+
+  def associativity[B, C](implicit monad: Monad[M], arbMa: Arbitrary[M[A]], arbAMb: Arbitrary[A => M[B]], arbBMc: Arbitrary[B => M[C]], eqMc: Eq[M[C]]) =
+    new MonadRuleSet {
+      def props: Seq[(String, Prop)] = Seq(
+        "associativity" → forAll { (m: M[A], f: A => M[B], g: B => M[C]) =>
+          val lhs: M[C] = monad.bind(monad.bind(m)(f))(g)
+          val rhs: M[C] = monad.bind(m)((x: A) => monad.bind(f(x))(g))
+          lhs === rhs
+        })
+    }
+
+}
--- axle-core/src/test/scala/axle/algebra/MonadLawsSpec.scala
+++ axle-core/src/test/scala/axle/algebra/MonadLawsSpec.scala
@@ -1,48 +1,27 @@
 package axle.algebra
 
+import axle.algebra.laws._
 import spire.algebra._
 import spire.implicits._
+
 import org.specs2.ScalaCheck
 import org.specs2.mutable._
 import org.scalacheck._
+
 import Arbitrary._
 import Gen._
 import Prop._
 
-abstract class MonadLawsSpec[M[_]: Monad, A: Eq: Arbitrary, B: Eq: Arbitrary, C: Eq: Arbitrary](name: String)(
-  implicit eqma: Eq[M[A]],
-  arbma: Arbitrary[M[A]],
-  arbamb: Arbitrary[A => M[B]],
-  eqmb: Eq[M[B]],
-  arbbmc: Arbitrary[B => M[C]],
-  eqmc: Eq[M[C]])
+import org.typelevel.discipline.Laws
+import org.typelevel.discipline.Predicate
+import org.typelevel.discipline.specs2.mutable.Discipline
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop
+import org.scalacheck.Prop._
+
+abstract class MonadLawsSpec
   extends Specification
-  with ScalaCheck {
-
-  s"$name obey left identity" ! prop { (m: M[A], x: A, f: A => M[B]) =>
-    val monad = Monad[M]
-    val lhs: M[B] = monad.bind(monad.unit(x))(f)
-    val rhs: M[B] = f(x)
-    lhs === rhs
-  }
-
-  s"$name obey right identity" ! prop { (m: M[A]) =>
-    val monad = Monad[M]
-    val lhs: M[A] = monad.bind(m)(monad.unit)
-    val rhs: M[A] = m
-    lhs === rhs
-  }
-
-  s"$name obey associativity" ! prop { (m: M[A], f: A => M[B], g: B => M[C]) =>
-    val monad = Monad[M]
-    val lhs: M[C] = monad.bind(monad.bind(m)(f))(g)
-    val rhs: M[C] = monad.bind(m)((x: A) => monad.bind(f(x))(g))
-    lhs === rhs
-  }
-
-}
-
-object Implicits {
+  with Discipline {
 
   implicit def eqEitherIntInt[L: Eq, R: Eq]: Eq[Either[L, R]] = new Eq[Either[L, R]] {
     def eqv(x: Either[L, R], y: Either[L, R]): Boolean = (x, y) match {
@@ -52,14 +31,19 @@ object Implicits {
     }
   }
 
+  checkAll("List[Int]", MonadLaws[List, Int].leftIdentity[Int])
+  checkAll("List[String]", MonadLaws[List, String].leftIdentity[String])
+  checkAll("Option[Int]", MonadLaws[Option, Int].leftIdentity[Int])
+  checkAll("Either[({ type λ[α] = Either[Int, α] })#λ, Int]", MonadLaws[({ type λ[α] = Either[Int, α] })#λ, Int].leftIdentity[Int])
+
+  checkAll("List[Int]", MonadLaws[List, Int].rightIdentity)
+  checkAll("List[String]", MonadLaws[List, String].rightIdentity)
+  checkAll("Option[Int]", MonadLaws[Option, Int].rightIdentity)
+  checkAll("Either[({ type λ[α] = Either[Int, α] })#λ, Int]", MonadLaws[({ type λ[α] = Either[Int, α] })#λ, Int].rightIdentity)
+
+  checkAll("List[Int]", MonadLaws[List, Int].associativity[Int, Int])
+  checkAll("List[String]", MonadLaws[List, String].associativity[String, String])
+  checkAll("Option[Int]", MonadLaws[Option, Int].associativity[Int, Int])
+  checkAll("Either[({ type λ[α] = Either[Int, α] })#λ, Int]", MonadLaws[({ type λ[α] = Either[Int, α] })#λ, Int].associativity[Int, Int])
+
 }
-
-import Implicits._
-
-class ListIntMonadLawsSpec extends MonadLawsSpec[List, Int, Int, Int]("List[Int] Monad")
-
-class ListStringMonadLawsSpec extends MonadLawsSpec[List, String, String, String]("List[String] Monad")
-
-class OptionIntMonadLawsSpec extends MonadLawsSpec[Option, Int, Int, Int]("Option[Int] Monad")
-
-class EitherIntMonadLawsSpec extends MonadLawsSpec[({ type λ[α] = Either[Int, α] })#λ, Int, Int, Int]("Either[Int, Int] Monad")
