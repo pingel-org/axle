9e669412f75b9ae4e6532797779a3ad0933464ba
Adam Pingel <adam@axle-lang.org>
2015-04-27 00:07:58 -0700

    aside, atop, transpose test for axle.jblas

--- axle-jblas/src/test/scala/axle/jblas/MatrixSpecification.scala
+++ axle-jblas/src/test/scala/axle/jblas/MatrixSpecification.scala
@@ -1,62 +1,61 @@
 package axle.jblas
 
-import org.specs2.mutable._
-import axle.syntax.linearalgebra._
-import axle.syntax.endofunctor._
-import spire.implicits._
+import org.jblas.DoubleMatrix
+import org.specs2.mutable.Specification
 
 class MatrixSpecification extends Specification {
 
   "DoubleJblasMatrix" should {
     "work" in {
 
-      val z = linearAlgebraDoubleMatrix.zeros(3, 4) // .zero
-      val o = linearAlgebraDoubleMatrix.ones(2, 3)
-      val r = linearAlgebraDoubleMatrix.rand(1, 2)
+      import axle.syntax.endofunctor.endofunctorOps
+      import axle.syntax.linearalgebra.matrixOps
+      import axle.jblas.linearAlgebraDoubleMatrix.{ ones, zeros, rand }
 
-      val dm = linearAlgebraDoubleMatrix.rand(3, 3)
-      val c2 = dm.column(2)
-      val r2 = dm.row(2)
+      val z = zeros(3, 4)
+      val o = ones(2, 3)
+      val x = rand(1, 2)
+
+      val y = rand(3, 3)
+      val c = y.column(2)
+      val r = y.row(2)
 
       1 must be equalTo (1)
     }
   }
 
-  "x+x === x.map(*2)" should {
+  "x+x === x.map(_*2)" should {
     "work" in {
-      
-      implicit val ring = linearAlgebraDoubleMatrix.ring
-      
-      val x = linearAlgebraDoubleMatrix.randn(2, 2)
 
-      val xx = ring.plus(x, x)
+      import linearAlgebraDoubleMatrix.{ randn }
+      import axle.syntax.endofunctor.endofunctorOps
+      import axle.syntax.linearalgebra.matrixOps
+
+      val x = randn(2, 2)
+
+      val xx = implicitly[spire.algebra.Ring[DoubleMatrix]].plus(x, x)
+
       val mapped = x.map(_ * 2)
-      
+
       xx must be equalTo mapped
     }
   }
 
-  //  "IntJblasMatrix" should {
-  //    "work" in {
-  //
-  //      val z = zeros[Int](1, 3)
-  //      val o = ones[Int](2, 2)
-  //      val e = eye[Int](3)
-  //
-  //      1 must be equalTo (1)
-  //    }
-  //  }
+  "aside, atop, transpose" should {
+    "work" in {
 
-  //  "BooleanJblasMatrix" should {
-  //    "work" in {
-  //
-  //      val f = falses(2, 3)
-  //      val t = trues(3, 2)
-  //      val e = eye[Boolean](4)
-  //
-  //      1 must be equalTo (1)
-  //    }
-  //  }
+      import axle.jblas.linearAlgebraDoubleMatrix.rand
+      import axle.syntax.linearalgebra.matrixOps
+
+      val r = 2
+      val c = 3
+
+      val m = rand(r, c)
+      val n = rand(r, c)
+
+      (n atop m) must be equalTo ((n.t aside m.t).t)
+    }
+  }
 
 }
 
