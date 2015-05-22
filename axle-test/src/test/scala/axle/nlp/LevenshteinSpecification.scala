35fa8effaa055d0682ff2493258fea7ea2bdf2b0
Adam Pingel <adam@axle-lang.org>
2015-05-22 11:08:49 -0600

    test levenshtein satisfies cauchy-schwarz

---
+++ axle-test/src/test/scala/axle/nlp/LevenshteinSpecification.scala
@@ -0,0 +1,23 @@
+package axle.nlp
+
+import org.specs2.mutable.Specification
+import org.typelevel.discipline.specs2.mutable.Discipline
+
+import axle._
+import axle.algebra._
+import spire.implicits.IntAlgebra
+import org.jblas.DoubleMatrix
+import axle.jblas._
+import axle.algebra.laws.MetricSpaceLaws
+
+class LevenshteinSpecification
+  extends Specification
+  with Discipline {
+
+  implicit val laJblasInt = linearAlgebraDoubleMatrix[Int]
+
+  implicit val space = Levenshtein[IndexedSeq, Char, DoubleMatrix, Int]()
+
+  checkAll("Levenshtein space", MetricSpaceLaws[IndexedSeq[Char], Int].cauchySchwarz)
+
+}
