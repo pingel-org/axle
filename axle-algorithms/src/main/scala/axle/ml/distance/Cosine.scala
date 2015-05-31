9a2842e89261098434118dc5ef830ff58951a203
Adam Pingel <adam@axle-lang.org>
2015-05-30 17:52:43 -0700

    clean up cosine distance

--- axle-algorithms/src/main/scala/axle/ml/distance/Cosine.scala
+++ axle-algorithms/src/main/scala/axle/ml/distance/Cosine.scala
@@ -1,24 +1,17 @@
 package axle.ml.distance
 
 import scala.math.Pi
+import scala.math.abs
 import scala.math.acos
 
-import axle.algebra.LinearAlgebra
-import axle.algebra.Zero
-import axle.syntax.linearalgebra.matrixOps
 import spire.algebra.AdditiveAbGroup
 import spire.algebra.Field
-import spire.algebra.MetricSpace
-import spire.algebra.NormedVectorSpace
 import spire.algebra.InnerProductSpace
-import spire.algebra.MultiplicativeMonoid
+import spire.algebra.MetricSpace
 import spire.algebra.NRoot
 import spire.implicits.convertableOps
 import spire.implicits.multiplicativeGroupOps
 import spire.implicits.multiplicativeSemigroupOps
-import spire.implicits.nrootOps
-import spire.implicits._
-import math.abs
 import spire.math.ConvertableFrom
 import spire.math.ConvertableTo
 
@@ -31,8 +24,6 @@ import spire.math.ConvertableTo
  * @param n = num columns in row vectors
  *
  * http://math.stackexchange.com/questions/102924/cosine-similarity-distance-and-triangle-equation
- *
- * distance(r1, r2) = 1.0 - abs(rvDot(r1, r2) / (norm(r1) * norm(r2)))
  *
  */
 
@@ -49,9 +40,9 @@ case class Cosine[M, D: NRoot: Field: AdditiveAbGroup: ConvertableFrom: Converta
     dot(v, w) / (norm(v) * norm(w))
 
   def distance(v: M, w: M): D = {
-    // val d = Field[D].one - similarity(v, w)
-    // val d = 1d - (2d * (acos(similarity(v, w).toDouble) / Pi))
+
     val d = 1d - abs(acos(similarity(v, w).toDouble) / Pi)
+
     ConvertableTo[D].fromDouble(d)
   }
 
