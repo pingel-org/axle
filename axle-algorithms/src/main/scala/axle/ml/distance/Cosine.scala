a58f6facb84aaaddbc1523e96578e7fd718ac1a6
Adam Pingel <adam@axle-lang.org>
2015-05-30 17:48:49 -0700

    define cosine distance in terms of an inner product space

--- axle-algorithms/src/main/scala/axle/ml/distance/Cosine.scala
+++ axle-algorithms/src/main/scala/axle/ml/distance/Cosine.scala
@@ -6,12 +6,19 @@ import scala.math.acos
 import axle.algebra.LinearAlgebra
 import axle.algebra.Zero
 import axle.syntax.linearalgebra.matrixOps
+import spire.algebra.AdditiveAbGroup
 import spire.algebra.Field
 import spire.algebra.MetricSpace
+import spire.algebra.NormedVectorSpace
+import spire.algebra.InnerProductSpace
 import spire.algebra.MultiplicativeMonoid
+import spire.algebra.NRoot
 import spire.implicits.convertableOps
 import spire.implicits.multiplicativeGroupOps
 import spire.implicits.multiplicativeSemigroupOps
+import spire.implicits.nrootOps
+import spire.implicits._
+import math.abs
 import spire.math.ConvertableFrom
 import spire.math.ConvertableTo
 
@@ -23,23 +30,28 @@ import spire.math.ConvertableTo
  *
  * @param n = num columns in row vectors
  *
+ * http://math.stackexchange.com/questions/102924/cosine-similarity-distance-and-triangle-equation
+ *
  * distance(r1, r2) = 1.0 - abs(rvDot(r1, r2) / (norm(r1) * norm(r2)))
  *
  */
 
-case class Cosine[M, R: Zero: MultiplicativeMonoid, C: Zero, D: Field: ConvertableFrom: ConvertableTo](n: C)(
-  implicit la: LinearAlgebra[M, R, C, D])
+case class Cosine[M, D: NRoot: Field: AdditiveAbGroup: ConvertableFrom: ConvertableTo](
+  implicit ips: InnerProductSpace[M, D])
     extends MetricSpace[M, D] {
 
-  def dot(v: M, w: M): D =
-    v.mulPointwise(w).rowSums.scalar
+  val normed = ips.normed
+
+  val norm = normed.norm _
+  import ips.dot
 
   def similarity(v: M, w: M): D =
-    dot(v, w) / (dot(v, v) * dot(w, w))
+    dot(v, w) / (norm(v) * norm(w))
 
   def distance(v: M, w: M): D = {
-    // assert(v.isRowVector && w.isRowVector && v.length === w.length)
-    val d = 1d - (acos(similarity(v, w).toDouble) / Pi)
+    // val d = Field[D].one - similarity(v, w)
+    // val d = 1d - (2d * (acos(similarity(v, w).toDouble) / Pi))
+    val d = 1d - abs(acos(similarity(v, w).toDouble) / Pi)
     ConvertableTo[D].fromDouble(d)
   }
 
--- axle-test/src/test/scala/axle/ml/distance/CosineSpec.scala
+++ axle-test/src/test/scala/axle/ml/distance/CosineSpec.scala
@@ -1,17 +1,18 @@
 package axle.ml.distance
 
 import org.jblas.DoubleMatrix
-import org.specs2.mutable.Specification
-import org.typelevel.discipline.specs2.mutable.Discipline
-import org.typelevel.discipline.Predicate
 import org.scalacheck.Arbitrary
-import org.scalacheck.Gen
-import spire.implicits.IntAlgebra
-import spire.implicits.DoubleAlgebra
-import spire.laws.VectorSpaceLaws
-import axle.jblas._
-import axle.jblas.eqDoubleMatrix
+import org.specs2.mutable.Specification
+import org.typelevel.discipline.Predicate
+import org.typelevel.discipline.specs2.mutable.Discipline
+
 import axle.algebra.LinearAlgebra
+import axle.jblas.eqDoubleMatrix
+import axle.jblas.linearAlgebraDoubleMatrix
+import axle.jblas.moduleDoubleMatrix
+import spire.implicits.DoubleAlgebra
+import spire.implicits.IntAlgebra
+import spire.laws.VectorSpaceLaws
 
 class CosineSpec
     extends Specification
@@ -22,14 +23,16 @@ class CosineSpec
   val m = 1
   val n = 2
 
-  implicit val arbMatrix: Arbitrary[DoubleMatrix] =
-    Arbitrary(LinearAlgebra.genMatrix[DoubleMatrix, Double](m, n, -10d, 10d))
+  implicit val ips = axle.jblas.innerProductSpace(n)(IntAlgebra, laJblasDouble, moduleDoubleMatrix)
 
-  implicit val space = Cosine[DoubleMatrix, Int, Int, Double](n)
+  implicit val space = Cosine[DoubleMatrix, Double]()
 
   implicit val pred: Predicate[Double] = new Predicate[Double] {
     def apply(a: Double) = true
   }
+
+  implicit val arbMatrix: Arbitrary[DoubleMatrix] =
+    Arbitrary(LinearAlgebra.genMatrix[DoubleMatrix, Double](m, n, 0d, 10d))
 
   checkAll("Cosine space on 1x2 matrix",
     VectorSpaceLaws[DoubleMatrix, Double].metricSpace)
