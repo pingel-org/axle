fe408955c5e26f56063852c4254b294714ec3f64
Adam Pingel <adam@axle-lang.org>
2015-06-20 12:13:23 -0700

    organize imports

--- axle-visualize/src/main/scala/axle/awt/PlotComponent.scala
+++ axle-visualize/src/main/scala/axle/awt/PlotComponent.scala
@@ -5,20 +5,26 @@ import java.awt.Font
 import java.awt.Graphics
 import java.awt.Graphics2D
 
-import scala.Vector
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
-import axle.visualize._
-import DataFeedProtocol.Fetch
 import akka.pattern.ask
 import axle.actor.Defaults.askTimeout
 import axle.algebra.LengthSpace
 import axle.algebra.Tics
 import axle.algebra.Zero
-import axle.quanta.AngleConverter
+import axle.visualize.DataFeedProtocol.Fetch
+import axle.visualize.Fed
+import axle.visualize.Plot
 import axle.visualize.PlotView
-import axle.visualize.element._
+import axle.visualize.angleDouble
+import axle.visualize.element.DataLines
+import axle.visualize.element.HorizontalLine
+import axle.visualize.element.Key
+import axle.visualize.element.Text
+import axle.visualize.element.VerticalLine
+import axle.visualize.element.XTics
+import axle.visualize.element.YTics
 import javax.swing.JPanel
 import spire.algebra.Eq
 
