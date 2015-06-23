fbbc4b7988d9755e3121e5288de635f07aeef566
Adam Pingel <adam@axle-lang.org>
2015-06-22 21:15:22 -0700

    add Irises use case for KMeans

--- axle-test/src/test/scala/axle/ml/KMeansSpecification.scala
+++ axle-test/src/test/scala/axle/ml/KMeansSpecification.scala
@@ -1,30 +1,30 @@
 package axle.ml
 
-import scala.math.Pi
-import scala.math.cos
-import scala.math.sin
-import scala.math.sqrt
-import scala.util.Random.nextDouble
-import scala.util.Random.nextGaussian
-import scala.util.Random.shuffle
-
-import org.jblas.DoubleMatrix
 import org.specs2.mutable.Specification
-
-import axle.jblas.linearAlgebraDoubleMatrix
-import axle.jblas.additiveAbGroupDoubleMatrix
-import axle.jblas.rowVectorInnerProductSpace
-import axle.jblas.moduleDoubleMatrix
-import axle.ml.distance.Euclidean
-import spire.algebra.Eq
-import spire.implicits.DoubleAlgebra
-import spire.implicits.IntAlgebra
 
 class KMeansSpecification
     extends Specification {
 
   "K-Means Clustering" should {
-    "work" in {
+    "cluster random 2d points with small gaussian distribution around a center into 2 clusters" in {
+
+      import scala.math.Pi
+      import scala.math.cos
+      import scala.math.sin
+      import scala.math.sqrt
+      import scala.util.Random.nextDouble
+      import scala.util.Random.nextGaussian
+      import scala.util.Random.shuffle
+
+      import org.jblas.DoubleMatrix
+      import axle.jblas.linearAlgebraDoubleMatrix
+      import axle.jblas.additiveAbGroupDoubleMatrix
+      import axle.jblas.rowVectorInnerProductSpace
+      import axle.jblas.moduleDoubleMatrix
+      import axle.ml.distance.Euclidean
+      import spire.algebra.Eq
+      import spire.implicits.DoubleAlgebra
+      import spire.implicits.IntAlgebra
 
       case class Foo(x: Double, y: Double)
 
@@ -61,6 +61,67 @@ class KMeansSpecification
       val exemplar = km.exemplar(km(Foo(99.9, 99.9)))
 
       fooSimilarity(exemplar, Foo(100, 100)) must be lessThan 5d
+    }
+  }
+
+  "K-Means Clustering" should {
+    "cluster irises" in {
+
+      import axle.quanta.Distance
+      import axle.quanta.DistanceConverter
+      import axle.jung._
+
+      implicit val distanceConverter: DistanceConverter[Double] = {
+        import spire.implicits.DoubleAlgebra
+        import axle.algebra.modules.doubleRationalModule
+        Distance.converterGraph[Double, JungDirectedGraph]
+      }
+
+      import axle.data.Irises
+
+      val irisesData = new Irises[JungDirectedGraph]
+      import irisesData._
+
+      import axle.ml.distance.Euclidean
+      import org.jblas.DoubleMatrix
+      import axle.jblas.linearAlgebraDoubleMatrix
+
+      implicit val space: Euclidean[DoubleMatrix, Double] = {
+        import spire.implicits.IntAlgebra
+        import spire.implicits.DoubleAlgebra
+        import axle.jblas.moduleDoubleMatrix
+        implicit val inner = axle.jblas.rowVectorInnerProductSpace[Int, Int, Double](2)
+        Euclidean[DoubleMatrix, Double]
+      }
+
+      import axle.ml.KMeans
+      import axle.ml.PCAFeatureNormalizer
+      import distanceConverter.cm
+      import spire.implicits.DoubleAlgebra
+      import axle.ml.PCAFeatureNormalizer
+
+      val irisFeaturizer = (iris: Iris) => List((iris.sepalLength in cm).magnitude.toDouble, (iris.sepalWidth in cm).magnitude.toDouble)
+
+      val normalizer = (PCAFeatureNormalizer[DoubleMatrix] _).curried.apply(0.98)
+
+      val irisConstructor = (features: Seq[Double]) => Iris(1 *: cm, 1 *: cm, 1 *: cm, 1 *: cm, "")
+
+      val classifier = KMeans[Iris, List, DoubleMatrix](
+        irises,
+        N = 2,
+        irisFeaturizer,
+        normalizer,
+        irisConstructor,
+        K = 3,
+        iterations = 20)
+
+      import axle.ml.ConfusionMatrix
+      import spire.implicits.IntAlgebra
+      // import spire.implicits.StringAlgebra
+
+      // val confusion = ConfusionMatrix[Iris, Int, String, Vector, DoubleMatrix](classifier, irises.toVector, _.species, 0 to 2)
+
+      1 must be equalTo 1
     }
   }
 
