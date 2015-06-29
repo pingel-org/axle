91bfae46c2a320095c3d56b5c27d8ffaf3774e94
Adam Pingel <adam@axle-lang.org>
2015-06-28 19:22:08 -0700

    fix test

--- axle-test/src/test/scala/axle/visualize/PokerHandChartSpec.scala
+++ axle-test/src/test/scala/axle/visualize/PokerHandChartSpec.scala
@@ -2,35 +2,38 @@ package axle.visualize
 
 import org.specs2.mutable.Specification
 
-import axle.enrichGenSeq
-import axle.game.cards.Deck
-import axle.game.poker.PokerHand
-import axle.game.poker.PokerHandCategory
-import axle.smoosh
-import spire.compat.ordering
-import spire.implicits.IntAlgebra
-
 class PokerHandChartSpec extends Specification {
 
   "poker hand chart" should {
     "work" in {
 
-      val ns = 5 to 9
+      import axle.game.cards.Deck
+      import axle.game.poker.PokerHand
+      import axle.game.poker.PokerHandCategory
+      import spire.compat.ordering
 
-      def winnersFromHandSize(n: Int) =
-        (1 to 20).map(i => Deck().cards.take(n).combinations(5).map(PokerHand(_)).toList.max.category)
+      def winnerFromHandSize(handSize: Int) =
+        Deck().cards.take(handSize).combinations(5).map(PokerHand(_)).toList.max
 
-      val data: Map[Tuple2[PokerHandCategory, Int], Int] =
-        smoosh(ns.map(n => (n, winnersFromHandSize(n).tally)).toMap).withDefaultValue(0)
+      val data: IndexedSeq[(PokerHandCategory, Int)] =
+        for {
+          handSize <- 5 to 9
+          trial <- 1 to 1000
+        } yield (winnerFromHandSize(handSize).category, handSize)
 
       // TODO the inner Int should be (n: Int) => s"5 from $n"
 
-      val chart = BarChartGrouped[PokerHandCategory, Int, Int, Map[Tuple2[PokerHandCategory, Int], Int]](
-        data,
-        xAxis = 0,
-        title = Some("Poker Hand Distribution"))
+      import spire.implicits.IntAlgebra
+      import axle.visualize.BarChartGrouped
 
-      // png(chart, "pokerhands.png")
+      val chart = BarChartGrouped[PokerHandCategory, Int, Int, Map[(PokerHandCategory, Int), Int]](
+        data.tally.withDefaultValue(0),
+        yAxisLabel = Some("instances of hand category by initial hand size (1000 trial for each hand size)"),
+        title = Some("Poker Hands"))
+
+      import axle.web._
+
+      SVG[BarChartGrouped[PokerHandCategory, Int, Int, Map[(PokerHandCategory, Int), Int]]]
 
       1 must be equalTo 1
     }
