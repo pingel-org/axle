7daf9b9828c1d40475deeca24dec0f276bc5c988
Adam Pingel <adam@axle-lang.org>
2015-06-28 19:05:12 -0700

    rename type param

--- axle-algorithms/src/main/scala/axle/nlp/TermVectorizer.scala
+++ axle-algorithms/src/main/scala/axle/nlp/TermVectorizer.scala
@@ -4,35 +4,35 @@ import spire.algebra.Ring
 import spire.implicits.MapRng
 import spire.implicits.additiveSemigroupOps
 
-case class TermVectorizer[C: Ring](stopwords: Set[String])
-    extends Function1[String, Map[String, C]] {
+case class TermVectorizer[V: Ring](stopwords: Set[String])
+    extends Function1[String, Map[String, V]] {
 
-  val one = Ring[C].one
-  val zero = Ring[C].zero
+  val one = Ring[V].one
+  val zero = Ring[V].zero
 
   val whitespace = """\s+""".r
 
-  val emptyCount = Map.empty[String, C].withDefaultValue(zero)
+  val emptyCount = Map.empty[String, V].withDefaultValue(zero)
 
-  def countWordsInLine(line: String): Map[String, C] =
+  def countWordsInLine(line: String): Map[String, V] =
     whitespace.split(line.toLowerCase)
       .filterNot(stopwords.contains)
       .aggregate(emptyCount)((m, w) => m + (w -> (m(w) + one)), _ + _)
 
-  def uniqueWordsInLine(line: String): Map[String, C] =
+  def uniqueWordsInLine(line: String): Map[String, V] =
     whitespace.split(line.toLowerCase)
       .filterNot(stopwords.contains)
       .toSet
       .map((w: String) => (w, one))
       .toMap
 
-  def wordCount(is: Seq[String]): Map[String, C] =
+  def wordCount(is: Seq[String]): Map[String, V] =
     is.aggregate(emptyCount)((m, line) => m + countWordsInLine(line), _ + _)
 
-  def wordExistsCount(is: Seq[String]): Map[String, C] =
+  def wordExistsCount(is: Seq[String]): Map[String, V] =
     is.aggregate(emptyCount)((m, line) => m + uniqueWordsInLine(line), _ + _)
 
-  def apply(doc: String): Map[String, C] =
+  def apply(doc: String): Map[String, V] =
     wordCount(List(doc))
 
 }
