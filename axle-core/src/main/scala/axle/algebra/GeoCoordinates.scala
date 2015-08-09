6e35c88a0605eb73a8418693b2f5f275e13a8fb3
Adam Pingel <adam@axle-lang.org>
2015-08-09 15:11:45 -0700

    geo length space for waypoint calculation

--- axle-core/src/main/scala/axle/algebra/GeoCoordinates.scala
+++ axle-core/src/main/scala/axle/algebra/GeoCoordinates.scala
@@ -42,53 +42,70 @@ case class GeoCoordinates[N](
 object GeoCoordinates {
 
   implicit def geoCoordinatesMetricSpace[N: Field: Eq: ConvertableFrom: ConvertableTo](
-    implicit angleConverter: AngleConverter[N],
-    distanceConverter: DistanceConverter[N]): MetricSpace[GeoCoordinates[N], UnittedQuantity[Distance, N]] =
-    new MetricSpace[GeoCoordinates[N], UnittedQuantity[Distance, N]] {
+    implicit angleConverter: AngleConverter[N]): MetricSpace[GeoCoordinates[N], UnittedQuantity[Angle, N]] =
+    new MetricSpace[GeoCoordinates[N], UnittedQuantity[Angle, N]] {
 
-      import distanceConverter.km
+      import angleConverter.radian
       val ctn = ConvertableTo[N]
       val cfn = ConvertableFrom[N]
-      val earthRadius = ctn.fromDouble(6371d) *: km
       val half = ctn.fromRational(Rational(1, 2))
 
       // Haversine distance
 
-      def distance(v: GeoCoordinates[N], w: GeoCoordinates[N]): UnittedQuantity[Distance, N] = {
+      def distance(v: GeoCoordinates[N], w: GeoCoordinates[N]): UnittedQuantity[Angle, N] = {
         val dLat = w.latitude - v.latitude
         val dLon = w.longitude - v.longitude
         val a = square(sine(dLat :* half)) + square(sine(dLon :* half)) * cosine(v.latitude) * cosine(w.latitude)
-        val c = ctn.fromDouble(2 * atan2(sqrt(a), sqrt(1 - a)))
-        earthRadius :* c
+        val c = ctn.fromDouble(atan2(sqrt(a), sqrt(1 - a)))
+        c *: radian
       }
 
     }
 
   implicit def geoCoordinatesLengthSpace[N: Field: Eq: ConvertableFrom: ConvertableTo](
-    implicit angleConverter: AngleConverter[N],
-    distanceConverter: DistanceConverter[N]): LengthSpace[GeoCoordinates[N], UnittedQuantity[Distance, N]] =
-    new LengthSpace[GeoCoordinates[N], UnittedQuantity[Distance, N]] {
+    implicit angleConverter: AngleConverter[N]): LengthSpace[GeoCoordinates[N], UnittedQuantity[Angle, N]] =
+    new LengthSpace[GeoCoordinates[N], UnittedQuantity[Angle, N]] {
 
-      val metricSpace: MetricSpace[GeoCoordinates[N], UnittedQuantity[Distance, N]] =
+      val metricSpace: MetricSpace[GeoCoordinates[N], UnittedQuantity[Angle, N]] =
         geoCoordinatesMetricSpace[N]
 
-      import distanceConverter.km
+      import angleConverter.radian
       val ctn = ConvertableTo[N]
       val cfn = ConvertableFrom[N]
-      val earthRadius = ctn.fromDouble(6371d) *: km
       val half = ctn.fromRational(Rational(1, 2))
 
-      def distance(v: GeoCoordinates[N], w: GeoCoordinates[N]): UnittedQuantity[Distance, N] =
+      def distance(v: GeoCoordinates[N], w: GeoCoordinates[N]): UnittedQuantity[Angle, N] =
         metricSpace.distance(v, w)
 
-      def onPath(left: GeoCoordinates[N], right: GeoCoordinates[N], p: Double): GeoCoordinates[N] = {
-        val portion = ctn.fromDouble(p)
-        GeoCoordinates(???, ???)
+      /**
+       * Source: http://williams.best.vwh.net/avform.htm
+       *   "Intermediate points on a great circle"
+       *
+       * Note: The two points cannot be antipodal
+       *  (i.e. lat1+lat2=0 and abs(lon1-lon2)=pi) because then the route is undefined
+       *
+       */
+
+      def onPath(p1: GeoCoordinates[N], p2: GeoCoordinates[N], f: Double): GeoCoordinates[N] = {
+
+        val d = distance(p1, p2)
+
+        val A = sine(d :* ctn.fromDouble(1d - f)) / sine(d)
+        val B = sine(d :* ctn.fromDouble(f)) / sine(d)
+        val x = A * cosine(p1.latitude) * cosine(p1.longitude) + B * cosine(p2.latitude) * cosine(p2.longitude)
+        val y = A * cosine(p1.latitude) * sine(p1.longitude) + B * cosine(p2.latitude) * sine(p2.longitude)
+        val z = A * sine(p1.latitude) + B * sine(p2.latitude)
+
+        val latitude = ctn.fromDouble(atan2(z, sqrt(square(x) + square(y)))) *: radian
+        val longitude = ctn.fromDouble(atan2(y, x)) *: radian
+
+        GeoCoordinates(latitude, longitude)
       }
 
-      def portion(left: GeoCoordinates[N], v: GeoCoordinates[N], right: GeoCoordinates[N]): Double = {
-        val num = (distance(left, v) in km).magnitude
-        val denom = (distance(left, right) in km).magnitude
+      // assumes that v is on the path from p1 to p2
+      def portion(p1: GeoCoordinates[N], v: GeoCoordinates[N], p2: GeoCoordinates[N]): Double = {
+        val num = (distance(p1, v) in radian).magnitude
+        val denom = (distance(p1, p2) in radian).magnitude
         cfn.toDouble(num / denom)
       }
     }
--- axle-test/src/test/scala/axle/algebra/GeoLengthSpaceSpec.scala
+++ axle-test/src/test/scala/axle/algebra/GeoLengthSpaceSpec.scala
@@ -2,32 +2,59 @@ package axle.algebra
 
 import org.specs2.mutable.Specification
 
-import axle.algebra.GeoCoordinates.geoCoordinatesMetricSpace
+import axle.algebra.GeoCoordinates.geoCoordinatesLengthSpace
 import axle.algebra.modules.doubleDoubleModule
 import axle.algebra.modules.doubleRationalModule
+import axle.distanceOnSphere
 import axle.jung.directedGraphJung
 import axle.quanta.Angle
 import axle.quanta.Distance
 import axle.quanta.UnitOfMeasurement
+import axle.quanta.UnittedQuantity
 import edu.uci.ics.jung.graph.DirectedSparseGraph
 import spire.implicits.DoubleAlgebra
 import spire.implicits.metricSpaceOps
+import spire.implicits.moduleOps
 
 class GeoLengthSpaceSpec extends Specification {
 
-  "geo length space" should {
+  implicit val angleConverter = Angle.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Angle], Double => Double]]
+  import angleConverter.°
+  import angleConverter.radian
+
+  implicit val distanceConverter = Distance.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Distance], Double => Double]]
+  import axle.quanta.UnittedQuantity
+
+  val sf = GeoCoordinates(37.7833 *: °, 122.4167 *: °)
+  val ny = GeoCoordinates(40.7127 *: °, 74.0059 *: °)
+
+  "geo metric space" should {
     "measure distance from San Francisco to New York" in {
 
-      implicit val angleConverter = Angle.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Angle], Double => Double]]
-      import angleConverter.°
+      val degreesDistance = ((sf distance ny) in °).magnitude
 
-      implicit val distanceConverter = Distance.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Distance], Double => Double]]
       import distanceConverter.km
+      val earthRadius = 6371d *: km
+      val kmDistance = (distanceOnSphere(sf distance ny, earthRadius) in km).magnitude
 
-      val sf = GeoCoordinates(37.7833 *: °, 122.4167 *: °)
-      val ny = GeoCoordinates(40.7127 *: °, 74.0059 *: °)
+      degreesDistance must be equalTo 18.564484707158623
+      kmDistance must be equalTo 4128.553030413071
+    }
+  }
 
-      ((sf distance ny) in km).magnitude must be equalTo 4128.553030413071
+  // See http://williams.best.vwh.net/avform.htm
+  val lax = GeoCoordinates(0.592539 *: radian, 2.066470 *: radian)
+  val jfk = GeoCoordinates(0.709186 *: radian, 1.287762 *: radian)
+
+  "geo length space" should {
+    "calculate the way-point 40% from LAX to JFK correctly" in {
+
+      import axle.algebra.GeoCoordinates.geoCoordinatesLengthSpace
+
+      val waypoint = geoCoordinatesLengthSpace.onPath(lax, jfk, 0.4)
+
+      (waypoint.latitude in °).magnitude must be equalTo 38.64825118848812
+      (waypoint.longitude in °).magnitude must be equalTo 101.7338280099326
     }
   }
 
