24d66e3ab218749762a034632aa128ff6010aa80
Adam Pingel <adam@axle-lang.org>
2015-08-09 11:53:46 -0700

    fix test broken after merge

--- axle-test/src/test/scala/axle/algebra/GeoLengthSpaceSpec.scala
+++ axle-test/src/test/scala/axle/algebra/GeoLengthSpaceSpec.scala
@@ -1,7 +1,5 @@
 package axle.algebra
 
-import scala.annotation.implicitNotFound
-
 import org.specs2.mutable.Specification
 
 import axle.algebra.GeoCoordinates.geoCoordinatesLengthSpace
@@ -10,6 +8,7 @@ import axle.algebra.modules.doubleRationalModule
 import axle.jung.directedGraphJung
 import axle.quanta.Angle
 import axle.quanta.Distance
+import axle.quanta.UnitOfMeasurement
 import edu.uci.ics.jung.graph.DirectedSparseGraph
 import spire.implicits.DoubleAlgebra
 
@@ -18,17 +17,16 @@ class GeoLengthSpaceSpec extends Specification {
   "geo length space" should {
     "measure distance from San Francisco to New York" in {
 
-      implicit val angleConverter = Angle.converterGraph[Double, DirectedSparseGraph]
-      import angleConverter.degree
+      implicit val angleConverter = Angle.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Angle], Double => Double]]
       import angleConverter.°
+
+      implicit val distanceConverter = Distance.converterGraph[Double, DirectedSparseGraph[UnitOfMeasurement[Distance], Double => Double]]
+      import distanceConverter.km
 
       val sf = GeoCoordinates(37.7833 *: °, 122.4167 *: °)
       val ny = GeoCoordinates(40.7127 *: °, 74.0059 *: °)
 
       import axle.algebra.GeoCoordinates.geoCoordinatesLengthSpace
-
-      implicit val distanceConverter = Distance.converterGraph[Double, DirectedSparseGraph]
-      import distanceConverter.km
 
       val geoLengthSpace = geoCoordinatesLengthSpace[Double]
 
