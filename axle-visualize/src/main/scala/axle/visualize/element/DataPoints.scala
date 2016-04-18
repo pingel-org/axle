579cfc2c46dabe093d3de12bbc016c18e384c5ba
Adam Pingel <pingel@gmail.com>
2016-04-17 22:23:42 -0700

    simple ScatterPlot for Set[(X, Y)]

---
+++ axle-visualize/src/main/scala/axle/visualize/element/DataPoints.scala
@@ -0,0 +1,10 @@
+package axle.visualize.element
+
+import axle.visualize.Color
+import axle.visualize.ScaledArea2D
+
+case class DataPoints[X, Y](
+  scaledArea: ScaledArea2D[X, Y],
+  data: Set[(X, Y)],
+  colorStream: Stream[Color],
+  pointDiameter: Int)
