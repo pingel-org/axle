2854dfb339703281dc25646e58660e652c9b7df0
Adam Pingel <pingel@gmail.com>
2016-08-19 21:17:17 -0700

    update docs notes

--- update-docs.sh
+++ update-docs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sbt -J-Xmx6G tut
+sbt -J-Xmx6G "project axle-docs" tut
 
 mv *.svg docs/images/
 
-cp -R axle-test/target/scala-2.11/tut/* docs/
+cp -R axle-docs/target/scala-2.11/tut/* docs/
 
 sed -i -e 's/releaseVersion/0.2.3/g' `find docs -name '*.md'`
 
 sed -i -e 's/snapshotVersion/0.2.4-SNAPSHOT/g' `find docs -name '*.md'`
+
+find . -name '*.md-e' | xargs rm
