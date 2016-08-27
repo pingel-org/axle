4324eceb84895cdbec1d6b191c9d9dc2d2085399
Adam Pingel <pingel@gmail.com>
2016-08-27 15:22:31 -0700

    s3 bucket policy

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 s3cmd sync ~/s3/axle-lang.org/ s3://axle-lang.org/
-s3cmd setacl s3://axle-lang.org/ --acl-public --recursive
+# s3cmd setacl s3://axle-lang.org/ --acl-public --recursive
