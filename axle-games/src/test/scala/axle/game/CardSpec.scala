009947789670e71e2f98606ac820524e708be5f8
Adam Pingel <pingel@gmail.com>
2016-10-02 17:21:30 -0700

    card spec

--- axle-games/src/test/scala/axle/game/CardSpec.scala
+++ axle-games/src/test/scala/axle/game/CardSpec.scala
@@ -2,12 +2,18 @@ package axle.game
 
 import org.specs2.mutable._
 
+import axle.game.cards.Card
+
 class CardSpec extends Specification {
 
-  "deck" should {
-    "" in {
-
-      1 must be equalTo 1
+  "cards" should {
+    "serialize" in {
+      Card("6♡").serialize must be equalTo "6♡"
+    }
+    "be equal" in {
+      import spire.algebra.Eq
+      Eq[Card].eqv(Card("7♣"), Card("7♣")) must be equalTo true
     }
   }
+
 }
