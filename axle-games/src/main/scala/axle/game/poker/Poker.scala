a9ab34ca1ad9405745571d1f0dbd0d9378882106
Adam Pingel <pingel@gmail.com>
2016-10-03 23:06:31 -0700

    get rid of string -> PokerPlayer method

--- axle-games/src/main/scala/axle/game/poker/Poker.scala
+++ axle-games/src/main/scala/axle/game/poker/Poker.scala
@@ -3,25 +3,19 @@ package axle.game.poker
 import axle.game._
 import axle.game.cards._
 
-case class Poker(numPlayers: Int) extends Game[Poker] {
+// (1 to numPlayers).map(i => player("P" + i, "Player " + i, "human"))
+// def players: IndexedSeq[PokerPlayer] = _players
 
-  implicit val poker = this
+case class Poker(players: IndexedSeq[PokerPlayer]) extends Game[Poker] {
+
+  val numPlayers = players.length
 
   type PLAYER = PokerPlayer
   type MOVE = PokerMove
   type STATE = PokerState
   type OUTCOME = PokerOutcome
 
-  val dealer = player("D", "Dealer", "dealer")
-
-  val _players = (1 to numPlayers).map(i => player("P" + i, "Player " + i, "human"))
-
-  def player(id: String, description: String, which: String): PokerPlayer = which match {
-    case "random" => RandomPokerPlayer(id, description)
-    case "ai"     => PokerPlayerAI(id, description)
-    case "dealer" => PokerPlayerDealer(id, description)
-    case _        => PokerPlayerInteractive(id, description)
-  }
+  val dealer = PokerPlayerDealer("D", "Dealer")
 
   def startState: PokerState =
     PokerState(
@@ -32,7 +26,7 @@ case class Poker(numPlayers: Int) extends Game[Poker] {
       Map[PokerPlayer, Seq[Card]](),
       0, // pot
       0, // current bet
-      _players.toSet, // stillIn
+      players.toSet, // stillIn
       Map(), // inFors
       players.map(player => (player, 100)).toMap, // piles
       None,
@@ -60,7 +54,5 @@ case class Poker(numPlayers: Int) extends Game[Poker] {
   }
 
   def introMessage: String = "Welcome to Axle Texas Hold Em Poker"
-
-  def players: IndexedSeq[PokerPlayer] = _players
 
 }
