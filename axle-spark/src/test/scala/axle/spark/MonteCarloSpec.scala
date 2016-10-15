72e84c183c4accaf8b43862bdfd56549241404b6
Adam Pingel <pingel@gmail.com>
2016-10-15 15:58:56 -0700

    test other spark witnesses

--- axle-spark/src/test/scala/axle/spark/MonteCarloSpec.scala
+++ axle-spark/src/test/scala/axle/spark/MonteCarloSpec.scala
@@ -9,6 +9,8 @@ import axle._
 import spire.implicits._
 import axle.syntax.setfrom._
 import axle.syntax.mapfrom._
+import axle.syntax.mapreducible._
+import axle.algebra.MapReducible
 
 class MonteCarloSpec extends Specification {
 
@@ -24,11 +26,14 @@ class MonteCarloSpec extends Specification {
       val π = monteCarloPiEstimate[RDD[Long], Long, Double, RDD[Double]](trials, (n: Long) => n.toDouble)
 
       val s = setFromOps(trials).toSet
-
       val m = mapFromOps(trials.map(i => i -> i % 10)).toMap
+      val mr = mapReducibleOps[RDD[Long], Long, Long, Long, RDD[(Long, Long)]](trials).
+        mapReduce(i => i % 10 -> i, 0L, _ + _)
+      val mrCount = mr.count()
 
       spark.stop()
 
+      mrCount must be equalTo 10L
       π must be greaterThan 3.1
     }
   }
