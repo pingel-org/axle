ad7a48c2cd563e0ea9474b83335fcae4e5bfbe2b
Adam Pingel <pingel@gmail.com>
2016-10-15 15:50:01 -0700

    test toSet and toMap witnesses

--- axle-spark/src/test/scala/axle/spark/MonteCarloSpec.scala
+++ axle-spark/src/test/scala/axle/spark/MonteCarloSpec.scala
@@ -7,6 +7,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import axle._
 import spire.implicits._
+import axle.syntax.setfrom._
+import axle.syntax.mapfrom._
 
 class MonteCarloSpec extends Specification {
 
@@ -17,9 +19,13 @@ class MonteCarloSpec extends Specification {
       val n = 1000L * slices
       val conf = new SparkConf().setAppName("Pi on Axle on Spark").setMaster("local[2]")
       val spark = new SparkContext(conf)
-      val trials = spark.parallelize(1 to n.toInt, slices).map(_.toLong)
+      val trials: RDD[Long] = spark.parallelize(1 to n.toInt, slices).map(_.toLong)
 
       val π = monteCarloPiEstimate[RDD[Long], Long, Double, RDD[Double]](trials, (n: Long) => n.toDouble)
+
+      val s = setFromOps(trials).toSet
+
+      val m = mapFromOps(trials.map(i => i -> i % 10)).toMap
 
       spark.stop()
 
