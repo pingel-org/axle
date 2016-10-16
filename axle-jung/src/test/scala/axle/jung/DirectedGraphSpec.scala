f2146ee0acf9331192f3233926b6b50c6401ce84
Adam Pingel <pingel@gmail.com>
2016-10-16 13:25:49 -0700

    assertions for directed graph

--- axle-jung/src/test/scala/axle/jung/DirectedGraphSpec.scala
+++ axle-jung/src/test/scala/axle/jung/DirectedGraphSpec.scala
@@ -5,6 +5,8 @@ import org.specs2.mutable.Specification
 
 import edu.uci.ics.jung.graph.DirectedSparseGraph
 import axle.algebra.DirectedGraph
+import spire.math.Real
+import spire.implicits._
 import axle.syntax.finite._
 import axle.syntax.directedgraph._
 import axle.syntax.DirectedGraphOps
@@ -14,7 +16,7 @@ class DirectedGraphSpec extends Specification {
   "Directed Graph" should {
     "work" in {
 
-      class Edge
+      class Edge(val weight: Real)
 
       val jdg = DirectedGraph.k2[DirectedSparseGraph, String, Edge]
 
@@ -25,37 +27,20 @@ class DirectedGraphSpec extends Specification {
 
       val g = jdg.make(List(a, b, c, d),
         List(
-          (a, b, new Edge),
-          (b, c, new Edge),
-          (c, d, new Edge),
-          (d, a, new Edge),
-          (a, c, new Edge),
-          (b, d, new Edge)))
+          (a, b, new Edge(1.1)),
+          (b, c, new Edge(1.1)),
+          (c, d, new Edge(1.1)),
+          (d, a, new Edge(1.1)),
+          (a, c, new Edge(1.1)),
+          (b, d, new Edge(1.1))))
 
       g.size must be equalTo 4
-    }
-  }
-
-  "REPL Demo" should {
-    "work" in {
-
-      class Edge(weight: Double)
-
-      val jdg = DirectedGraph.k2[DirectedSparseGraph, String, Edge]
-
-      val a = "a"
-      val b = "b"
-      val c = "c"
-      val d = "d"
-
-      val g = jdg.make(List(a, b, c, d),
-        List(
-          (a, b, new Edge(0.3)),
-          (a, c, new Edge(0.2)),
-          (b, c, new Edge(0.4)),
-          (c, d, new Edge(2.4))))
-
-      g.size must be equalTo 4
+      g.neighbors(a).size must be equalTo 3
+      // g.edges.size must be equalTo 6
+      g.vertices.size must be equalTo 4
+      g.findVertex(_ == "a").get must be equalTo "a"
+      vertexFunctorDSG.map(g)(s => s + s).findVertex(_ == "aa").get must be equalTo "aa"
+      edgeFunctorDSG.map(g)(r => new Edge(r.weight + 1.1)).findEdge("a","b").weight must be equalTo Real(2.2)
     }
   }
 
