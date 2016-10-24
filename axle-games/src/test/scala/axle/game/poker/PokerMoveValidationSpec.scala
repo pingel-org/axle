a425be774376a34031baad9bd47c20965ead989f
Adam Pingel <pingel@gmail.com>
2016-10-23 20:45:50 -0700

    test poker move validation

---
+++ axle-games/src/test/scala/axle/game/poker/PokerMoveValidationSpec.scala
@@ -0,0 +1,22 @@
+package axle.game.poker
+
+import org.specs2.mutable._
+import axle.game._
+
+class PokerMoveValidationSpec extends Specification {
+
+  val p1 = Player("P1", "Player 1")
+  val p2 = Player("P2", "Player 2")
+
+  val game = Poker(Vector(
+    (p1, randomMove, println),
+    (p2, randomMove, println)),
+    println)
+
+  "move validator" must {
+    "reject River as first move" in {
+
+      Poker.evGame.isValid(game, startState(game), River()) must be equalTo Left("invalid move")
+    }
+  }
+}
