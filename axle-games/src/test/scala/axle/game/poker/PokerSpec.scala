5cd3c6b6bdae3bd3ab860fd8aade3f76fc06fc7c
Adam Pingel <pingel@gmail.com>
2016-10-23 18:03:16 -0700

    test move display

--- axle-games/src/test/scala/axle/game/poker/PokerSpec.scala
+++ axle-games/src/test/scala/axle/game/poker/PokerSpec.scala
@@ -8,13 +8,12 @@ class PokerSpec extends Specification {
   val p1 = Player("P1", "Player 1")
   val p2 = Player("P2", "Player 2")
 
-  val game = Poker(Vector(
-    (p1, interactiveMove, println),
-    (p2, interactiveMove, println)),
-    println)
-
   "start state" should {
     "display something" in {
+      val game = Poker(Vector(
+        (p1, interactiveMove, println),
+        (p2, interactiveMove, println)),
+        println)
       startState(game).displayTo(p1, game) must contain("Current bet: 0")
     }
   }
@@ -48,11 +47,19 @@ class PokerSpec extends Specification {
         dropOutput)
 
       val start = startState(game)
-      val lastState = moveStateStream(game, start).last._3
+      val history = moveStateStream(game, start).toVector
+      val lastState = history.last._3
 
       val outcome = lastState.outcome(game).get
       val newGameState = startFrom(game, lastState).get
+
       // TODO these messages should include amounts
+      history.map({
+        case (from, move, to) => {
+          val mover = evState.mover(from).get
+          evMove.displayTo(game, mover, move, p1)
+        }
+      }).mkString(", ") must contain("call")
       evOutcome.displayTo(game, outcome, p1) must contain("You beat")
       evOutcome.displayTo(game, outcome, p2) must contain("beat You")
       outcome.winner.get should be equalTo p1
@@ -60,28 +67,28 @@ class PokerSpec extends Specification {
     }
   }
 
-//  "random game" should {
-//
-//    val rGame: Poker = Poker(Vector(
-//      (p1, randomMove, dropOutput),
-//      (p2, randomMove, dropOutput)),
-//      dropOutput)
-//
-//    "produce moveStateStream" in {
-//      val stream = moveStateStream(rGame, startState(rGame))
-//      stream.take(3).length must be equalTo 3
-//    }
-//
-//    "terminate in a state with no further moves" in {
-//      val endState = play(rGame)
-//      endState.moves(rGame).length must be equalTo 0
-//    }
-//
-//    "produce game stream" in {
-//      val stream = gameStream(rGame, startState(rGame), false)
-//      stream.take(2).length must be equalTo 2
-//    }
-//
-//  }
+  //  "random game" should {
+  //
+  //    val rGame: Poker = Poker(Vector(
+  //      (p1, randomMove, dropOutput),
+  //      (p2, randomMove, dropOutput)),
+  //      dropOutput)
+  //
+  //    "produce moveStateStream" in {
+  //      val stream = moveStateStream(rGame, startState(rGame))
+  //      stream.take(3).length must be equalTo 3
+  //    }
+  //
+  //    "terminate in a state with no further moves" in {
+  //      val endState = play(rGame)
+  //      endState.moves(rGame).length must be equalTo 0
+  //    }
+  //
+  //    "produce game stream" in {
+  //      val stream = gameStream(rGame, startState(rGame), false)
+  //      stream.take(2).length must be equalTo 2
+  //    }
+  //
+  //  }
 
 }
