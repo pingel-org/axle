835b8b96c4bbf73d2386b49659719b2b09d2923b
Adam Pingel <pingel@gmail.com>
2016-10-27 08:24:37 -0700

    re-order methods in Game trait

--- axle-core/src/main/scala/axle/game/Game.scala
+++ axle-core/src/main/scala/axle/game/Game.scala
@@ -3,28 +3,33 @@ package axle.game
 
 trait Game[G, S, O, M] {
 
-  def introMessage(g: G): String
-
   def startState(g: G): S
 
   def startFrom(g: G, s: S): Option[S]
 
   def players(g: G): IndexedSeq[Player]
 
+  def mover(s: S): Option[Player]
+
+  def moves(s: S, game: G): Seq[M]
+
   def strategyFor(g: G, player: Player): (S, G) => M
 
-  /**
-   *
-   * default should be no-op:
-   *
-   *    (s: String) => {}
-   */
+  def isValid(g: G, state: S, move: M): Either[String, M]
 
-  def displayerFor(g: G, player: Player): String => Unit
+  def applyMove(state: S, game: G, move: M)(implicit evGame: Game[G, S, O, M]): S
+
+  def outcome(s: S, game: G): Option[O]
+
+  /**
+   * IO related
+   */
 
   def parseMove(g: G, input: String): Either[String, M]
 
-  def isValid(g: G, state: S, move: M): Either[String, M]
+  def displayerFor(g: G, player: Player): String => Unit
+
+  def introMessage(g: G): String
 
   def displayMoveTo(
     game: G,
@@ -39,19 +44,7 @@ trait Game[G, S, O, M] {
     observer: Player)(
       implicit evGame: Game[G, S, O, M]): String
 
-  def mover(s: S): Option[Player]
-
-  def applyMove(
-    state: S,
-    game: G,
-    move: M)(
-      implicit evGame: Game[G, S, O, M]): S
-
   def displayTo(state: S, observer: Player, game: G)(
     implicit evGame: Game[G, S, O, M]): String
-
-  def outcome(s: S, game: G): Option[O]
-
-  def moves(s: S, game: G): Seq[M]
 
 }
