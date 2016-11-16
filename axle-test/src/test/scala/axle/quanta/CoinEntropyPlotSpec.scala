5ed81a822d834d3d828f7239f02998c21db2a06b
Adam Pingel <pingel@gmail.com>
2016-11-15 21:25:46 -0800

    more implicit search for Plot construction

--- axle-test/src/test/scala/axle/quanta/CoinEntropyPlotSpec.scala
+++ axle-test/src/test/scala/axle/quanta/CoinEntropyPlotSpec.scala
@@ -8,7 +8,7 @@ import org.specs2.mutable.Specification
 
 import axle.stats.H
 import axle.stats.coin
-import axle.orderToOrdering
+// import axle.orderToOrdering
 import spire.math.Rational
 import spire.implicits._
 import cats.implicits._
@@ -24,11 +24,11 @@ class CoinEntropyPlotSpec extends Specification {
       import edu.uci.ics.jung.graph.DirectedSparseGraph
       import axle.jung.directedGraphJung
       import axle.spireToCatsEq
-      import axle.algebra.Tics
+      //import axle.algebra.Tics
       import cats.kernel.Order
-      import cats.kernel.Eq
-      import axle.algebra.Zero
-      import axle.algebra.LengthSpace
+      //import cats.kernel.Eq
+      //import axle.algebra.Zero
+      //import axle.algebra.LengthSpace
       import axle.quanta.unittedTics
 
       implicit val id =
@@ -44,16 +44,16 @@ class CoinEntropyPlotSpec extends Specification {
         new TreeMap[Rational, UnittedQuantity[Information, Double]]() ++
           (0 to 100).map(i => (Rational(i / 100d), H(coin(Rational(i, 100))))).toMap
 
-      implicit val zr = Zero[Rational]
-      implicit val tr = Tics[Rational]
-      implicit val er = Eq[Rational]
-      implicit val lsrrd = LengthSpace[Rational, Rational, Double]
-      implicit val zuqid = Zero[UnittedQuantity[Information, Double]]
-      implicit val tuqid = unittedTics[Information, Double, DG]
-      implicit val euqid = Eq[UnittedQuantity[Information, Double]]
-      implicit val lsuqiddd = LengthSpace[UnittedQuantity[Information, Double], Double, Double]
-      implicit val or: Order[Rational] = Order[Rational]
-      implicit val pdv = PlotDataView.treeMapDataView[Rational, UnittedQuantity[Information, Double]]
+      // implicit val zr = Zero[Rational]
+      // implicit val tr = Tics[Rational]
+      // implicit val er = Eq[Rational]
+      // implicit val lsrrd = LengthSpace[Rational, Rational, Double]
+      // implicit val zuqid = Zero[UnittedQuantity[Information, Double]]
+      // implicit val tuqid = unittedTics[Information, Double, DG]
+      // implicit val euqid = Eq[UnittedQuantity[Information, Double]]
+      // implicit val lsuqiddd = LengthSpace[UnittedQuantity[Information, Double], Double, Double]
+      implicit val or: Order[Rational] = cats.kernel.Order.apply[Rational]
+      // implicit val pdv = PlotDataView.treeMapDataView[Rational, UnittedQuantity[Information, Double]]
 
       val plot = Plot[Rational, UnittedQuantity[Information, Double], D](
         List(("h", hm)),
@@ -63,7 +63,7 @@ class CoinEntropyPlotSpec extends Specification {
         xAxisLabel = Some("p(x='HEAD)"),
         yAxis = Some(Rational(0)),
         yAxisLabel = Some("H"),
-        title = Some("Entropy"))(zr, tr, er, lsrrd, zuqid, tuqid, euqid, lsuqiddd, pdv)
+        title = Some("Entropy")) // (zr, tr, er, lsrrd, zuqid, tuqid, euqid, lsuqiddd, pdv)
 
       import axle.web._
       val d = SVG[Plot[Rational, UnittedQuantity[Information, Double], D]]
