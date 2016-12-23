46f0a84a9d8ffe8e616e71587ac0431bbee7e8ff
Adam Pingel <pingel@gmail.com>
2016-12-22 18:22:07 -0700

    scalatest for axle-languages

--- axle-languages/src/test/scala/axle/ast/language/PythonSpecification.scala
+++ axle-languages/src/test/scala/axle/ast/language/PythonSpecification.scala
@@ -3,26 +3,25 @@ package axle.ast.language
 
 import axle.ast._
 import axle.ast.view.ViewString
-import org.specs2.mutable._
+import org.scalatest._
 
-object PythonSpecification extends Specification {
-  "conversion from JSON to AstNode" should {
+object PythonSpecification extends FunSuite with Matchers {
 
-    "turn 3 into 3" in {
+  test("conversion from JSON to AstNode turn 3 into 3") {
 
-      //JsonAST.fromJson("""{"node": {"spread": [{"expr": {"right": {"type": "Const", "value": 2}, "type": "Add", "left": {"type": "Const", "value": 1}}, "type": "Discard"}], "type": "Stmt"}, "type": "Module"}""")
-      //JsonAST.fromJson("""{"node": {"spread": [{"test": {"type": "Name", "name": "True"}, "fail": {"type": "Name", "name": "True"}, "type": "Assert"}], "type": "Stmt"}, "type": "Module"}""")
+    //JsonAST.fromJson("""{"node": {"spread": [{"expr": {"right": {"type": "Const", "value": 2}, "type": "Add", "left": {"type": "Const", "value": 1}}, "type": "Discard"}], "type": "Stmt"}, "type": "Module"}""")
+    //JsonAST.fromJson("""{"node": {"spread": [{"test": {"type": "Name", "name": "True"}, "fail": {"type": "Name", "name": "True"}, "type": "Assert"}], "type": "Stmt"}, "type": "Module"}""")
 
-      JsonAST.fromJson("""{"node": {"spread": [{"test": {"type": "Name", "name": "True"}, "fail": null, "type": "Assert"}], "type": "Stmt"}, "type": "Module"}""")
+    JsonAST.fromJson("""{"node": {"spread": [{"test": {"type": "Name", "name": "True"}, "fail": null, "type": "Assert"}], "type": "Stmt"}, "type": "Module"}""")
 
-      // TODO add some assertions about how Python.trim should work
+    // TODO add some assertions about how Python.trim should work
 
-      JsonAST.fromJson(
-        """{"type": "Const", "value": 3}""") must be equalTo (
-          AstNodeRule("Const", Map("value" -> AstNodeValue(Some("3"), 1)), 1))
+    JsonAST.fromJson(
+      """{"type": "Const", "value": 3}""") should be(
+        AstNodeRule("Const", Map("value" -> AstNodeValue(Some("3"), 1)), 1))
 
-      JsonAST.fromJson(
-        """
+    JsonAST.fromJson(
+      """
 {
    "node": {
       "spread": [{
@@ -40,126 +39,120 @@ object PythonSpecification extends Specification {
    "_lineno": null,
    "type": "Module"
 }
-""") must be equalTo (
-          AstNodeRule("Module", Map(
-            "node" -> AstNodeRule("Stmt", Map(
-              "spread" -> AstNodeList(List(AstNodeRule("Discard", Map(
-                "expr" -> AstNodeRule("Add", Map(
-                  "right" -> AstNodeRule("Const", Map(
-                    "value" -> AstNodeValue(Some("3"), 1)), 1),
-                  "left" -> AstNodeRule("Const", Map(
-                    "value" -> AstNodeValue(Some("3"), 1)), 1)), 1)), 1)), 1)), 1)), 1))
-    }
+""") should be(
+        AstNodeRule("Module", Map(
+          "node" -> AstNodeRule("Stmt", Map(
+            "spread" -> AstNodeList(List(AstNodeRule("Discard", Map(
+              "expr" -> AstNodeRule("Add", Map(
+                "right" -> AstNodeRule("Const", Map(
+                  "value" -> AstNodeValue(Some("3"), 1)), 1),
+                "left" -> AstNodeRule("Const", Map(
+                  "value" -> AstNodeValue(Some("3"), 1)), 1)), 1)), 1)), 1)), 1)), 1))
   }
 
   val language = Python.language
 
-  "function call emit" should {
-    "work" in {
+  test("function call emit") {
 
-      val ast = AstNodeRule("CallFunc", //stmt = Sq(WrappedArray(Sub(node), Emb((,J(args,Sq(WrappedArray(Lit(,), Sp()))),)))), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
-        Map("node" -> //stmt = Sub(node), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
-          AstNodeRule("Name",
-            Map("name" -> //stmt = Attr(name), nodeOpt = Some(AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1))
-              AstNodeValue(Some("f"), 1)), 1),
-          "args" -> //stmt = J(args,Sq(WrappedArray(Lit(,), Sp()))), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
-            AstNodeList(List(
-              AstNodeRule("Name",
-                Map("name" -> //stmt = Attr(name), nodeOpt = Some(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1))
-                  AstNodeValue(Some("a"), 1)),
-                1) // AstNodeRule("Name"
-                ), // List(
-              1) // (List
-              ), // Map("node"...
-        1)
+    val ast = AstNodeRule("CallFunc", //stmt = Sq(WrappedArray(Sub(node), Emb((,J(args,Sq(WrappedArray(Lit(,), Sp()))),)))), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
+      Map("node" -> //stmt = Sub(node), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
+        AstNodeRule("Name",
+          Map("name" -> //stmt = Attr(name), nodeOpt = Some(AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1))
+            AstNodeValue(Some("f"), 1)), 1),
+        "args" -> //stmt = J(args,Sq(WrappedArray(Lit(,), Sp()))), nodeOpt = Some(AstNodeRule(CallFunc,Map(node -> AstNodeRule(Name,Map(name -> AstNodeValue(Some(f),1)),1), args -> AstNodeList(List(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1)),1)),1))
+          AstNodeList(List(
+            AstNodeRule("Name",
+              Map("name" -> //stmt = Attr(name), nodeOpt = Some(AstNodeRule(Name,Map(name -> AstNodeValue(Some(a),1)),1))
+                AstNodeValue(Some("a"), 1)),
+              1) // AstNodeRule("Name"
+              ), // List(
+            1) // (List
+            ), // Map("node"...
+      1)
 
-      val actual = ViewString.AstNode(ast, language)
-      actual must be equalTo ("f(a)")
-    }
+    val actual = ViewString.AstNode(ast, language)
+    actual should be("f(a)")
   }
 
-  "signature extraction" should {
+  test("signature extraction emits correct python") {
 
-    "emit correct python" in {
-
-      def parseTests =
-        "a" ::
-          "3" ::
-          "3.0" ::
-          "a = 'a'\n" :: // TODO spaces
-          "f(a, b)\n" ::
-          "x = f(a, b)\n" ::
-          "a = [f(x) for x in range(3, 4)]\n" ::
-          "a = [f(x) for x in range(3, 4) if False]\n" ::
-          "y = a.z\n" ::
-          "f = {}\n" ::
-          "f = dict()\n" ::
-          "y = (a, b, c)\n" ::
-          "a = x[3:4]\n" ::
-          "x = [1, 2, 3]\n" ::
-          "f = {a : b, c : d}\n" ::
-          "f = 4 + f(3)\n" ::
-          "f = 2 - 1\n" ::
-          "f = 4 * 3\n" ::
-          "f = 2 / 4\n" ::
-          "g[3] = f[3] + h[3 * 2]\n" ::
-          "global a, b, c\n" ::
-          "print fmtstr % (x, y)\n" ::
-          "assert True, True\n" ::
-          "assert True\n" ::
-          "a and b and c\n" ::
-          "3 < 4 < 5\n" ::
-          "exec code\n" ::
-          "exec code in locals(), globals()\n" ::
-          "exec code in locals()\n" ::
-          "lambda x, y: x * y\n" ::
-          "import re\n" ::
-          "import bar as b\n" ::
-          "import bar as b, baz as c\n" ::
-          "from foo import x\n" ::
-          "raise" ::
-          "raise Exception(a)\n" ::
-          """try:
+    def parseTests =
+      "a" ::
+        "3" ::
+        "3.0" ::
+        "a = 'a'\n" :: // TODO spaces
+        "f(a, b)\n" ::
+        "x = f(a, b)\n" ::
+        "a = [f(x) for x in range(3, 4)]\n" ::
+        "a = [f(x) for x in range(3, 4) if False]\n" ::
+        "y = a.z\n" ::
+        "f = {}\n" ::
+        "f = dict()\n" ::
+        "y = (a, b, c)\n" ::
+        "a = x[3:4]\n" ::
+        "x = [1, 2, 3]\n" ::
+        "f = {a : b, c : d}\n" ::
+        "f = 4 + f(3)\n" ::
+        "f = 2 - 1\n" ::
+        "f = 4 * 3\n" ::
+        "f = 2 / 4\n" ::
+        "g[3] = f[3] + h[3 * 2]\n" ::
+        "global a, b, c\n" ::
+        "print fmtstr % (x, y)\n" ::
+        "assert True, True\n" ::
+        "assert True\n" ::
+        "a and b and c\n" ::
+        "3 < 4 < 5\n" ::
+        "exec code\n" ::
+        "exec code in locals(), globals()\n" ::
+        "exec code in locals()\n" ::
+        "lambda x, y: x * y\n" ::
+        "import re\n" ::
+        "import bar as b\n" ::
+        "import bar as b, baz as c\n" ::
+        "from foo import x\n" ::
+        "raise" ::
+        "raise Exception(a)\n" ::
+        """try:
    foo()
 finally:
    close()
 """ ::
-          """while x < 5:
+        """while x < 5:
    print x
    x += 1
 else:
    y = 0
 """ ::
-          """def f(a, b, c):
+        """def f(a, b, c):
    g()
 """ ::
-          //      """def f(a, b, *c):
-          //   g()
-          //""" ::
-          //      """def f(a, b, **c):
-          //   g()
-          //""" ::
-          //      """def f(a, *b, **c):
-          //   g()
-          //""" ::
-          """@logging
+        //      """def f(a, b, *c):
+        //   g()
+        //""" ::
+        //      """def f(a, b, **c):
+        //   g()
+        //""" ::
+        //      """def f(a, *b, **c):
+        //   g()
+        //""" ::
+        """@logging
 def f(a, b, x=True, y=False):
    return a
 """ :: Nil
 
-      // TODO:
-      // function's docstring
-      // try/except (try: bar() except Exception, e: foo()
-      // try/except/finally
-      // indentation following else (see BaseHTTPServer.py.html)
-      // if's condition not showing up sometimes
+    // TODO:
+    // function's docstring
+    // try/except (try: bar() except Exception, e: foo()
+    // try/except/finally
+    // indentation following else (see BaseHTTPServer.py.html)
+    // if's condition not showing up sometimes
 
-      parseTests forall { input =>
-        val parsed = language.parseString(input)
-        val actual = parsed.map(ViewString.AstNode(_, language)).getOrElse("")
-        actual == input
-      } must be equalTo true
-    }
+    parseTests forall { input =>
+      val parsed = language.parseString(input)
+      val actual = parsed.map(ViewString.AstNode(_, language)).getOrElse("")
+      actual == input
+    } should be(true)
 
   }
 }
