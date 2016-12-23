7ab7dba04c65b2bf9f1abff752b203d21c96d310
Adam Pingel <pingel@gmail.com>
2016-12-22 17:51:07 -0700

    axle-algorithms on scalatest

--- axle-algorithms/src/test/scala/axle/lx/GoldSpecification.scala
+++ axle-algorithms/src/test/scala/axle/lx/GoldSpecification.scala
@@ -1,40 +1,37 @@
 package axle.lx
 
-import org.specs2.mutable._
+import org.scalatest._
 import Gold._
 import spire.implicits._
 
-class GoldSpecification extends Specification {
+class GoldSpecification extends FunSuite with Matchers {
 
-  "Gold Paradigm" should {
+  test("Gold Paradigm: memorizing learner memorizes") {
 
-    "memorizing learner memorizes" in {
+    val mHi = Morpheme("hi")
+    val mIm = Morpheme("I'm")
+    val mYour = Morpheme("your")
+    val mMother = Morpheme("Mother")
+    val mShut = Morpheme("shut")
+    val mUp = Morpheme("up")
 
-      val mHi = Morpheme("hi")
-      val mIm = Morpheme("I'm")
-      val mYour = Morpheme("your")
-      val mMother = Morpheme("Mother")
-      val mShut = Morpheme("shut")
-      val mUp = Morpheme("up")
+    val Σ = Vocabulary(Set(mHi, mIm, mYour, mMother, mShut, mUp))
 
-      val Σ = Vocabulary(Set(mHi, mIm, mYour, mMother, mShut, mUp))
+    val s1 = mHi :: mIm :: mYour :: mMother :: Nil
+    val s2 = mShut :: mUp :: Nil
 
-      val s1 = mHi :: mIm :: mYour :: mMother :: Nil
-      val s2 = mShut :: mUp :: Nil
+    val ℒ = Language(Set(s1, s2))
 
-      val ℒ = Language(Set(s1, s2))
+    val T = Text(s1 :: ♯ :: ♯ :: s2 :: ♯ :: s2 :: s2 :: Nil)
 
-      val T = Text(s1 :: ♯ :: ♯ :: s2 :: ♯ :: s2 :: s2 :: Nil)
+    val ɸ = MemorizingLearner()
+    val outcome = ɸ.guesses(T)
+      .find(_.ℒ === ℒ)
+      .map(finalGuess => "well done, ɸ")
+      .getOrElse("ɸ never made a correct guess")
 
-      val ɸ = MemorizingLearner()
-      val outcome = ɸ.guesses(T)
-        .find(_.ℒ === ℒ)
-        .map(finalGuess => "well done, ɸ")
-        .getOrElse("ɸ never made a correct guess")
+    // T.isFor(ℒ)
 
-      // T.isFor(ℒ)
-
-      outcome must be equalTo "ɸ never made a correct guess"
-    }
+    outcome should be("ɸ never made a correct guess")
   }
 }
