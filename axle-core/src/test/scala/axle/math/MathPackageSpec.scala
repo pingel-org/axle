2733d8041d5a3da6e88a2a8e4a6d9c397e1f0d53
Adam Pingel <pingel@gmail.com>
2017-05-21 22:26:13 -0700

    create axle.math package with mathy stuff from axle._ package object

---
+++ axle-core/src/test/scala/axle/math/MathPackageSpec.scala
@@ -0,0 +1,60 @@
+package axle.math
+
+import org.scalatest._
+
+class MathPackageSpec extends FunSuite with Matchers {
+
+  test("monte carlo pi be at least 2.9") {
+    import spire.implicits._
+    monteCarloPiEstimate(
+      (1 to 1000).toList,
+      (n: Int) => n.toDouble) should be > 2.9
+  }
+
+  test("Wallis pi > 3d") {
+    wallisΠ(100).toDouble should be > 3d
+  }
+
+  test("fibonacci iteratively arrive at fib(7) == 21") {
+    assertResult(fibonacciByFold(7))(21)
+  }
+
+  test("fibonacci recursively") {
+    assertResult(fibonacciRecursively(7))(21)
+  }
+
+  test("exponentiation by recursive squaring") {
+    import spire.implicits._
+    assertResult(exponentiateByRecursiveSquaring(2, 10))(1024)
+  }
+
+  test("ackermann(2, 2) == ") {
+    assertResult(ackermann(2, 2))(7)
+  }
+
+  test("mandelbrot at 1.8 1.7") {
+    import spire.implicits.DoubleAlgebra
+    // import cats.implicits._
+    assertResult(inMandelbrotSetAt(4d, 1.8, 1.7, 100).get)(0)
+  }
+
+  test("primes using sieveOfEratosthenes") {
+    // See https://primes.utm.edu/howmany.html
+    val primes = sieveOfEratosthenes(10000)
+    primes.filter(_ < 10).length should be(4)
+    primes.filter(_ < 100).length should be(25)
+    primes.filter(_ < 1000).length should be(168)
+    primes.filter(_ < 10000).length should be(1229)
+  }
+
+  test("primes using primeStream") {
+    // See https://primes.utm.edu/howmany.html
+    import spire.implicits.IntAlgebra
+    val primes = primeStream(10000).toList
+    primes.filter(_ < 10).length should be(4)
+    primes.filter(_ < 100).length should be(25)
+    primes.filter(_ < 1000).length should be(168)
+    primes.filter(_ < 10000).length should be(1229)
+  }
+
+}
