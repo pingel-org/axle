6fbb1db30e069f63052456648884881c959ab248
Adam Pingel <pingel@gmail.com>
2017-05-22 22:24:25 -0700

    prep 0.3.5

--- update-docs.sh
+++ update-docs.sh
@@ -5,9 +5,9 @@ sbt -J-Xmx6G "project axle-docs" makeSite
 # mkdir -p axle-docs/target/site/tutorial/images/
 mv *.svg *.png axle-docs/target/site/tutorial/images/
 
-sed -i -e 's/releaseVersion/0.3.4/g' `find axle-docs/target/site/ -name '*.md'`
+sed -i -e 's/releaseVersion/0.3.5/g' `find axle-docs/target/site/ -name '*.md'`
 
-sed -i -e 's/snapshotVersion/0.3.5-SNAPSHOT/g' `find axle-docs/target/site/ -name '*.md'`
+sed -i -e 's/snapshotVersion/0.3.6-SNAPSHOT/g' `find axle-docs/target/site/ -name '*.md'`
 
 find axle-docs -name '*.md-e' | xargs rm
 
