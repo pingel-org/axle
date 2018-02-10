a44b2b350d67592c4cab58268e29089f15ff2a1e
Adam Pingel <pingel@gmail.com>
2018-02-10 14:11:37 -0800

    ScaleExp -- works with negative exponents

--- axle-core/src/main/scala/axle/algebra/Scale.scala
+++ axle-core/src/main/scala/axle/algebra/Scale.scala
@@ -2,6 +2,7 @@ package axle.algebra
 
 import spire.algebra.Field
 import spire.algebra.Module
+import spire.math.abs
 import spire.math.ConvertableTo
 import spire.math.Rational
 import spire.implicits.multiplicativeGroupOps
@@ -17,34 +18,28 @@ case class Scale[N](factor: Rational)(implicit module: Module[N, Rational]) exte
   def unapply(n: N): N = module.timesr(n, inverseFactor)
 }
 
-case class Scale10s[N](exp: Int)(
+class ScaleExp[N](base: Int, exp: Int)(
   implicit
   fieldN:         Field[N],
   convertableToN: ConvertableTo[N]) extends Bijection[N, N] {
 
-  require(exp > 0)
+  val growFactor = fieldN.multiplicative.combineN(convertableToN.fromInt(base), abs(exp))
+  val shrinkFactor = fieldN.multiplicative.combineN(fieldN.one / convertableToN.fromInt(base), -abs(exp))
 
-  val up = fieldN.multiplicative.combineN(convertableToN.fromInt(10), exp)
+  val applyFactor = if (exp < 0) { shrinkFactor } else { growFactor }
+  val unapplyFactor = if (exp < 0) { growFactor } else { shrinkFactor }
 
-  def apply(n: N): N = n * up
+  def apply(n: N): N = n * applyFactor
 
-  val down = fieldN.multiplicative.combineN(fieldN.one / convertableToN.fromInt(10), exp)
-
-  def unapply(n: N): N = n * down
+  def unapply(n: N): N = n * unapplyFactor
 }
+
+case class Scale10s[N](exp: Int)(
+  implicit
+  fieldN:         Field[N],
+  convertableToN: ConvertableTo[N]) extends ScaleExp(10, exp)
 
 case class Scale2s[N](exp: Int)(
   implicit
   fieldN:         Field[N],
-  convertableToN: ConvertableTo[N]) extends Bijection[N, N] {
-
-  require(exp > 0)
-
-  val up = fieldN.multiplicative.combineN(convertableToN.fromInt(2), exp)
-
-  def apply(n: N): N = n * up
-
-  val down = fieldN.multiplicative.combineN(fieldN.one / convertableToN.fromInt(2), exp)
-
-  def unapply(n: N): N = n * down
-}
+  convertableToN: ConvertableTo[N]) extends ScaleExp(2, exp)
