344f3410fd89bf5d10399a9c13e12767424b2b91
Adam Pingel <pingel@gmail.com>
2019-04-16 16:39:18 -0700

    fix typo in moveFromRandomState and get tests passing in MoveFromRandomStateSpec

--- axle-core/src/test/scala/axle/game/MoveFromRandomStateSpec.scala
+++ axle-core/src/test/scala/axle/game/MoveFromRandomStateSpec.scala
@@ -22,7 +22,7 @@ class MoveFromRandomStateSpec extends FunSuite with Matchers {
   val Sb = TestGameState("b")
   val Sc = TestGameState("c")
 
-  implicit val eqS: Eq[TestGameState] = Eq[TestGameState]
+  implicit val eqS: Eq[TestGameState] = Eq.fromUniversalEquals[TestGameState]
 
   case class TestGameMove(name: String)
   val Maa = TestGameMove("aa")
@@ -38,9 +38,18 @@ class MoveFromRandomStateSpec extends FunSuite with Matchers {
   case class TestGameOutcome()
 
   val movesMap: Map[TestGameState, Map[TestGameMove, (TestGameState, Rational)]] = Map(
-    Sa -> Map((Maa, (Sa, Rational(1, 2))), (Mab, (Sb, Rational(1, 4))), (Mac, (Sc, Rational(1, 4)))),
-    Sb -> Map((Mba, (Sa, Rational(0))),    (Mbb, (Sb, Rational(1))),    (Mbc, (Sc, Rational(0)))),
-    Sc -> Map((Mca, (Sa, Rational(0))),    (Mcb, (Sb, Rational(0))),    (Mcc, (Sc, Rational(1))))
+    Sa -> Map(
+      (Maa, (Sa, Rational(1, 2))),
+      (Mab, (Sb, Rational(1, 4))),
+      (Mac, (Sc, Rational(1, 4)))),
+    Sb -> Map(
+      (Mba, (Sa, Rational(0))),
+      (Mbb, (Sb, Rational(1))),
+      (Mbc, (Sc, Rational(0)))),
+    Sc -> Map(
+      (Mca, (Sa, Rational(0))),
+      (Mcb, (Sb, Rational(0))),
+      (Mcc, (Sc, Rational(1))))
   )
 
   implicit val evGame: Game[TestGame,TestGameState,TestGameOutcome,TestGameMove,TestGameState,TestGameMove,Rational,ConditionalProbabilityTable] =
@@ -61,7 +70,6 @@ class MoveFromRandomStateSpec extends FunSuite with Matchers {
       def mover(game: TestGame, state: TestGameState): Option[Player] =
         Some(player)
 
-      // TODO rename to moverMasked?
       def moverM(game: TestGame, state: TestGameState): Option[Player] =
         Some(player)
 
@@ -77,7 +85,7 @@ class MoveFromRandomStateSpec extends FunSuite with Matchers {
       def strategyFor(game: TestGame, player: Player): (TestGame, TestGameState) => ConditionalProbabilityTable[TestGameMove, Rational] =
         (game: TestGame, state: TestGameState) => {
           val mm = movesMap(state)
-          pm.construct(Variable("S"), mm.keys, mm.mapValues(_._2))
+          pm.construct(Variable("S"), mm.keys, s => mm(s)._2)
         }
 
       def isValid(game: TestGame, state: TestGameState, move: TestGameMove): Either[String, TestGameMove] =
@@ -102,27 +110,36 @@ class MoveFromRandomStateSpec extends FunSuite with Matchers {
   val currentStateModelMap = Map(Sa -> Rational(1, 3), Sb -> Rational(2, 3))
   val currentStateModel = pm.construct(Variable("S"), currentStateModelMap.keys, currentStateModelMap)
 
-  (1 to 20) foreach { i =>
-    val (stateMoveOpt, newStateModel) = moveFromRandomState(game, currentStateModel, rng)
-    println(s"$stateMoveOpt -> $newStateModel")
+  test("moveFromRandomState on hard-coded graph from start state") {
+
+    // Randomly select Sa then Mab
+    //   Sa: (1/3, -1/4) => 1/3 + (1/3 . -1/4) = 1/4
+    //   Sb: (2/3,  1/4) => 2/3 + (1/3 . -1/4) = 3/4
+
+    // (Sa, Mac) yields
+    //   Sa: (1/3, -1/4) => 1/3 + (1/3 . -1/4) = 1/4
+    //   Sb: (2/3,  0  ) => 2/3 + (1/3 .  0  ) = 2/3
+    //   Sc: (0  ,  1/4) => 0   + (1/3 .  1/4) = 1/12
+
+    val actualResult = ((1 to 1000) map { i =>
+      moveFromRandomState(game, currentStateModel, rng)
+    }) toSet
+
+    val expectedResult = Set(
+      (Some((Sa, Maa)), currentStateModel),
+      (Some((Sa, Mab)), pm.construct(Variable("S"), List(Sa, Sb), Map(
+        Sa -> Rational(1, 4),
+        Sb -> Rational(3, 4)))),
+      (Some((Sa, Mac)), pm.construct(Variable("S"), List(Sa, Sb, Sc), Map(
+        Sa -> Rational(1, 4),
+        Sb -> Rational(2, 3),
+        Sc -> Rational(1, 12)))),
+      (Some((Sb, Mbb)), currentStateModel)
+    )
+
+    actualResult should be(expectedResult)
   }
-  // makeSomeAssertions
 
   // (Sa, Maa) or (Sb, Mbb) or (Sc, Mcc) leaves distribution untouched
-
-  // (Sa, Mac) yields
-  //   Sa: (1/3, -1/4) => 1/3 + (1/3 . -1/4) = 1/4
-  //   Sb: (2/3,  0  ) => 2/3 + (1/3 .  0  ) = 2/3
-  //   Sc: (0  ,  1/4) => 0   + (1/3 .  1/4) = 1/12
-
-  val SaMacModelMap = Map(Sa -> Rational(1, 4), Sb -> Rational(2, 3), Sc -> Rational(1, 12))
-  val SaMacStateModel = pm.construct(Variable("S"), SaMacModelMap.keys, SaMacModelMap)
-
-  // Randomly select Sa then Mab
-  //   Sa: (1/3, -1/4) => 1/3 + (1/3 . -1/4) = 1/4
-  //   Sb: (2/3,  1/4) => 2/3 + (1/3 . -1/4) = 3/4
-
-  val SaMabModelMap = Map(Sa -> Rational(1, 4), Sb -> Rational(3, 4))
-  val SaMabStateModel = pm.construct(Variable("S"), SaMabModelMap.keys, SaMabModelMap)
 
 }
