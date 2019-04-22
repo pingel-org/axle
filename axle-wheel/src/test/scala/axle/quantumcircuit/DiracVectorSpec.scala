0e3113a6ff183bb4ebee1eef8e29c9e313b6a048
Adam Pingel <pingel@gmail.com>
2019-04-21 23:01:29 -0700

    version of DiracVectorSpach to match 'unindex' method name

--- axle-wheel/src/test/scala/axle/quantumcircuit/DiracVectorSpec.scala
+++ axle-wheel/src/test/scala/axle/quantumcircuit/DiracVectorSpec.scala
@@ -8,37 +8,37 @@ class DiracVectorSpec extends FunSuite with Matchers {
 
   test("CBit to Dirac Vector Notation") {
 
-    CBit0.reverseIndex should be(|("0").>.reverseIndex)
+    CBit0.unindex should be(|("0").>.unindex)
 
-    CBit1.reverseIndex should be(|("1").>.reverseIndex)
+    CBit1.unindex should be(|("1").>.unindex)
   }
 
   test("Multiple bits are represented as tensored product vector") {
 
-    |("00").>.reverseIndex should be(Vector[Binary](1, 0) ⊗ Vector[Binary](1, 0))
-    |("00").>.reverseIndex should be(CBit0.reverseIndex ⊗ CBit0.reverseIndex)
-    |("00").>.reverseIndex should be(Vector[Binary](1, 0, 0, 0))
+    |("00").>.unindex should be(Vector[Binary](1, 0) ⊗ Vector[Binary](1, 0))
+    |("00").>.unindex should be(CBit0.unindex ⊗ CBit0.unindex)
+    |("00").>.unindex should be(Vector[Binary](1, 0, 0, 0))
 
-    |("01").>.reverseIndex should be(Vector[Binary](1, 0) ⊗ Vector[Binary](0, 1))
-    |("01").>.reverseIndex should be(CBit0.reverseIndex ⊗ CBit1.reverseIndex)
-    |("01").>.reverseIndex should be(Vector[Binary](0, 1, 0, 0))
+    |("01").>.unindex should be(Vector[Binary](1, 0) ⊗ Vector[Binary](0, 1))
+    |("01").>.unindex should be(CBit0.unindex ⊗ CBit1.unindex)
+    |("01").>.unindex should be(Vector[Binary](0, 1, 0, 0))
 
-    |("10").>.reverseIndex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](1, 0))
-    |("10").>.reverseIndex should be(CBit1.reverseIndex ⊗ CBit0.reverseIndex)
-    |("10").>.reverseIndex should be(Vector[Binary](0, 0, 1, 0))
+    |("10").>.unindex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](1, 0))
+    |("10").>.unindex should be(CBit1.unindex ⊗ CBit0.unindex)
+    |("10").>.unindex should be(Vector[Binary](0, 0, 1, 0))
 
-    |("11").>.reverseIndex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](0, 1))
-    |("11").>.reverseIndex should be(CBit1.reverseIndex ⊗ CBit1.reverseIndex)
-    |("11").>.reverseIndex should be(Vector[Binary](0, 0, 0, 1))
+    |("11").>.unindex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](0, 1))
+    |("11").>.unindex should be(CBit1.unindex ⊗ CBit1.unindex)
+    |("11").>.unindex should be(Vector[Binary](0, 0, 0, 1))
   }
 
   test("Dirac Vector of non-base-2 numbers") {
 
     import DiracVector.intStringToVectorBinary
 
-    |("4", intStringToVectorBinary).>.reverseIndex should be(|("100").>.reverseIndex)
-    |("4", intStringToVectorBinary).>.reverseIndex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](1, 0) ⊗ Vector[Binary](1, 0))
-    |("4", intStringToVectorBinary).>.reverseIndex should be(Vector[Binary](0, 0, 0, 0, 1, 0, 0, 0))
+    |("4", intStringToVectorBinary).>.unindex should be(|("100").>.unindex)
+    |("4", intStringToVectorBinary).>.unindex should be(Vector[Binary](0, 1) ⊗ Vector[Binary](1, 0) ⊗ Vector[Binary](1, 0))
+    |("4", intStringToVectorBinary).>.unindex should be(Vector[Binary](0, 0, 0, 0, 1, 0, 0, 0))
   }
 
 }
