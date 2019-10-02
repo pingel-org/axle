91c244c08ed30e7250eb6a95f951bbe635d76dd8
Adam Pingel <pingel@gmail.com>
2019-10-01 23:13:15 -0400

    fix ProbabilitySpec

--- axle-core/src/test/scala/axle/stats/ProbabilitySpec.scala
+++ axle-core/src/test/scala/axle/stats/ProbabilitySpec.scala
@@ -5,12 +5,12 @@ import org.scalatest._
 import spire.math._
 import axle.game.Dice._
 import axle.syntax.probabilitymodel._
-import axle.algebra.RegionEq
-import axle.algebra.RegionEqTuple2of2
+import axle.algebra._
 
 class ProbabilitySpec extends FunSuite with Matchers {
 
   implicit val prob = ProbabilityModel[ConditionalProbabilityTable]
+  // implicit val rat = new spire.math.RationalAlgebra()
 
   test("two independent coins") {
 
@@ -18,29 +18,22 @@ class ProbabilitySpec extends FunSuite with Matchers {
     val coin2 = coin()
 
     import cats.implicits._
-    //import cats.syntax.all._
 
     val bothCoinsModel = prob.chain(coin1)(coin2)
 
-    bothCoinsModel.P(
-      { coins: (Symbol, Symbol) => (coins._1 === 'HEAD) && (coins._2 === 'HEAD)}
-    ) should be(Rational(1, 4))
-
-    // implicit val rat = new spire.math.RationalAlgebra()
+    bothCoinsModel.P(RegionEqTuple1of2('HEAD) and RegionEqTuple2of2('HEAD)) should be(Rational(1, 4))
 
     bothCoinsModel.P(RegionEq(('HEAD, 'HEAD))) should be(Rational(1, 4))
 
-    bothCoinsModel.P(
-      { coins: (Symbol, Symbol) => coins._1 === 'HEAD }
-    ) should be(Rational(1, 2))
+    bothCoinsModel.P(RegionEqTuple1of2('HEAD)) should be(Rational(1, 2))
 
-    bothCoinsModel.P(
-      { coins: (Symbol, Symbol) => (coins._1 === 'HEAD) || (coins._2 === 'HEAD)}
-    ) should be(Rational(3, 4))
+    bothCoinsModel.P(RegionEqTuple1of2('HEAD) or RegionEqTuple2of2('HEAD)) should be(Rational(3, 4))
 
-    val coin2Conditioned = bothCoinsModel.filter(RegionEqTuple2of2('TAIL)).map(_._1)
+    val coin2Conditioned = prob.map(bothCoinsModel.filter(RegionEqTuple2of2('TAIL))) {
+      _._1
+    }
 
-    coin2Conditioned.P('HEAD) should be(Rational(1, 2))
+    coin2Conditioned.P(RegionEq('HEAD)) should be(Rational(1, 2))
   
  }
 
@@ -50,13 +43,9 @@ class ProbabilitySpec extends FunSuite with Matchers {
 
     val bothDieModel = prob.chain(die(6))(die(6))
 
-    bothDieModel.P(
-      { rolls: (Int, Int) => (rolls._1 === 1) }
-    ) should be(Rational(1, 6))
+    bothDieModel.P(RegionEqTuple1of2(1)) should be(Rational(1, 6))
 
-    bothDieModel.P(
-      { rolls: (Int, Int) => (rolls._1 !== 3) }
-    ) should be(Rational(5, 6))
+    bothDieModel.P(RegionNegate(RegionEqTuple1of2(3))) should be(Rational(5, 6))
 
     bothDieModel.P(
       { rolls: (Int, Int) => (rolls._1 === 1) && (rolls._2 === 2)}
