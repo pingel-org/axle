556081316f6bd59bb919d0e89e6142fb77c234bd
Adam Pingel <pingel@gmail.com>
2020-08-29 23:54:17 -0400

    MapCRng.plus

--- axle-core/src/main/scala/axle/algebra/Talliable.scala
+++ axle-core/src/main/scala/axle/algebra/Talliable.scala
@@ -2,8 +2,8 @@ package axle.algebra
 
 import scala.annotation.implicitNotFound
 import spire.algebra.Ring
-import spire.implicits.MapCRng
-import spire.implicits.additiveSemigroupOps
+import spire.algebra.CRing
+
 
 @implicitNotFound("Witness not found for Talliable[${F}]")
 trait Talliable[F[_]] {
@@ -16,20 +16,22 @@ object Talliable {
   implicit val tallySeq =
     new Talliable[Seq] {
 
-      def tally[A, N](xs: Seq[A])(implicit ring: Ring[N]): Map[A, N] = {
+      def tally[A, N](xs: Seq[A])(implicit ring: CRing[N]): Map[A, N] = {
+        val mapCR = new spire.std.MapCRng[A, N]()
         xs.aggregate(Map.empty[A, N].withDefaultValue(ring.zero))(
           (m, x) => m + (x -> ring.plus(m(x), ring.one)),
-          _ + _)
+          mapCR.plus)
       }
     }
 
   implicit val tallyList =
     new Talliable[List] {
 
-      def tally[A, N](xs: List[A])(implicit ring: Ring[N]): Map[A, N] = {
+      def tally[A, N](xs: List[A])(implicit ring: CRing[N]): Map[A, N] = {
+        val mapCR = new spire.std.MapCRng[A, N]()
         xs.aggregate(Map.empty[A, N].withDefaultValue(ring.zero))(
           (m, x) => m + (x -> ring.plus(m(x), ring.one)),
-          _ + _)
+          mapCR.plus)
       }
     }
 
