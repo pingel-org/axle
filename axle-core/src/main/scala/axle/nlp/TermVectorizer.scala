5595569ca2ae37bec97de06ae11cc4bd8ad76381
Adam Pingel <pingel@gmail.com>
2020-08-30 00:04:51 -0400

    yet another MapCRng

--- axle-core/src/main/scala/axle/nlp/TermVectorizer.scala
+++ axle-core/src/main/scala/axle/nlp/TermVectorizer.scala
@@ -1,23 +1,30 @@
 package axle.nlp
 
-import spire.algebra.Ring
+//import spire.algebra.Ring
+import spire.algebra.CRing
 import spire.implicits.MapCRng
 import spire.implicits.additiveSemigroupOps
 
-case class TermVectorizer[V: Ring](stopwords: Set[String])
+case class TermVectorizer[V: CRing](stopwords: Set[String])
   extends Function1[String, Map[String, V]] {
 
-  val one = Ring[V].one
-  val zero = Ring[V].zero
+  val one = CRing[V].one
+  val zero = CRing[V].zero
 
   val whitespace = """\s+""".r
 
   val emptyCount = Map.empty[String, V].withDefaultValue(zero)
 
-  def countWordsInLine(line: String): Map[String, V] =
-    whitespace.split(line.toLowerCase)
+  def countWordsInLine(line: String): Map[String, V] = {
+    val mapCR = new spire.std.MapCRng[String, V]()
+    whitespace
+      .split(line.toLowerCase)
       .filterNot(stopwords.contains)
-      .aggregate(emptyCount)((m, w) => m + (w -> (m(w) + one)), _ + _)
+      .aggregate(emptyCount)(
+        (m, w) => m + (w -> (m(w) + one)),
+        mapCR.plus
+      )
+  }
 
   def uniqueWordsInLine(line: String): Map[String, V] =
     whitespace.split(line.toLowerCase)
