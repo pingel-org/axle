ccfc901ea5bbcc440ba43e1a82cc9ecf8da4a94e
Adam Pingel <pingel@gmail.com>
2020-09-07 14:21:15 -0400

    check in Gemfile; rework update-docs.sh

--- update-docs.sh
+++ update-docs.sh
@@ -1,14 +1,23 @@
-#!/bin/bash
+#!/bin/bash +x
+
+#BUILDDIR=axle-docs/target/foo
+#BUILDDIR=../axle-site-build
+BUILDDIR=../axle-bar
+STAGEDIR=~/s3/axle-lang.org/
+
 
 sbt -J-Xmx6G "project axle-docs" mdoc
-
 sbt -J-Xmx6G "project axle-docs" makeSite
 
-mkdir -p axle-docs/target/site/tutorial/images/
-mv *.svg *.png axle-docs/target/site/tutorial/images/
+mkdir -p $BUILDDIR
+cp axle-docs/src/site/Gemfile $BUILDDIR
+cp axle-docs/src/site/_config.yml $BUILDDIR
+cp axle-docs/src/site/favicon.ico $BUILDDIR
+cp -R axle-docs/src/site/tutorial $BUILDDIR
+cp -R axle-docs/target/site/* $BUILDDIR
+mv *.svg *.png $BUILDDIR/tutorial/images/
+cp -R axle-docs/src/site/css $BUILDDIR
 
-cp -R axle-docs/src/site/css axle-docs/target/site/
+jekyll build --source $BUILDDIR --destination $STAGEDIR
 
-jekyll build --source axle-docs/target/site/ --destination ~/s3/axle-lang.org/
-
-(cd ~/s3/axle-lang.org/; python -m SimpleHTTPServer 8000)
+(cd $STAGEDIR; python -m SimpleHTTPServer 8000)
