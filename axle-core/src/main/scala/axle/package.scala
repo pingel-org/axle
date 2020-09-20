5e069844c8d1ad582b345d0d5aed8122e223ab38
Adam Pingel <pingel@gmail.com>
2020-09-19 21:06:25 -0400

    mv much of remaining axle._ to axle.algebra._

--- axle-core/src/main/scala/axle/package.scala
+++ axle-core/src/main/scala/axle/package.scala
@@ -4,89 +4,12 @@ import scala.reflect.ClassTag
 import scala.collection.immutable.TreeMap
 import scala.language.implicitConversions
 
-import cats.Show
-import cats.implicits._
-
-import spire.algebra._
-import spire.implicits.additiveSemigroupOps
-import spire.implicits.additiveGroupOps
-
 /**
+ * Axle enrichments of scala collections
  *
  */
 
 package object axle {
-
-  // basic functions
-
-  /**
-   * dummy is not to be used widely, but is used for for scanLeft, where
-   * it's often desirable to provide a throw-away value as the first argument
-   * without using an Option type for an already complicated method signature.
-   * A better work-around would be an alternate version of scanLeft that had
-   * this behavior built in.
-   *
-   * Something like this:
-   *
-   * def scanLeftDropFirst[A, Repr, B, C, That](
-   *   tl: scala.collection.TraversableLike[A, Repr])(z: C)(op: ((B, C), A) ⇒ (B, C))(
-   *   implicit bf: scala.collection.generic.CanBuildFrom[Repr, (B, C), That]) =
-   *   tl.scanLeft((axle.dummy[B], z))(op) // Caller should .drop(1). TODO do that here
-   */
-
-  def dummy[T]: T = null.asInstanceOf[T]
-
-  def ignore[T]: T => Unit = (t: T) => {}
-
-  def id[A](x: A): A = x
-
-  // Seq operations
-
-  /**
-   * gaps
-   *
-   * assumes that the input xs are already sorted
-   */
-
-  def gaps[T](xs: Seq[T])(implicit ringT: Ring[T]): Seq[(T, T)] = {
-    import ringT.one
-    xs.zip(xs.drop(1))
-      .map({ case (x, y) => (x, y - x) })
-      .filterNot(_._2 == one)
-      .map({ case (x, g) => (x + one, x + g - one) })
-  }
-
-  def runs[T](xs: Seq[T], breaks: Set[T]): Seq[(T, T)] =
-    if (xs.isEmpty) {
-      List.empty
-    } else {
-      val tail = xs.drop(1)
-      val run = tail.takeWhile(x => !breaks.contains(x))
-      (xs.head, (xs.head +: run).last) +: runs(tail.drop(run.length), breaks)
-    }
-
-  /**
-   * runs
-   *
-   * assumes xs is already sorted
-   */
-
-  def runs[T](xs: Seq[T])(implicit ringT: Ring[T], orderT: Order[T]): Seq[(T, T)] = {
-    import ringT.one
-    val breaks = xs.zip(xs.drop(1)).filter({ case (x, y) => orderT.compare(y - x, one) == 1 }).map(_._2).toSet
-    runs(xs, breaks)
-  }
-
-  // List methods
-
-  def replicate[T](n: Int)(v: T): List[T] = (0 until n).map(i => v).toList
-
-  def reverse[T](l: List[T]): List[T] = l.reverse
-
-  def intersperse[T](d: T)(l: List[T]): List[T] =
-    (0 until (2 * l.size - 1)).map(i => i % 2 match { case 0 => l(i / 2) case 1 => d }).toList
-
-  // Axle enrichments of scala collections
 
   implicit def enrichIterable[T](ita: Iterable[T]): EnrichedIterable[T] = EnrichedIterable(ita)
 
@@ -101,55 +24,5 @@ package object axle {
   implicit def enrichArray[T: ClassTag](arr: Array[T]): EnrichedArray[T] = EnrichedArray(arr)
 
   implicit def enrichInt(n: Int): EnrichedInt = EnrichedInt(n)
-
-  // Function application patterns
-
-  def applyK[N](f: N => N, x0: N, k: Int): N =
-    (1 to k).foldLeft(x0)({ case (x, _) => f(x) })
-
-  // Iterator methods
-
-  def applyForever[N](f: N => N, x0: N): Iterator[N] =
-    Iterator
-      .continually(())
-      .scanLeft(x0)({ case (x, _) => f(x) })
-
-  def trace[N](f: N => N, x0: N): Iterator[(N, Set[N])] =
-    Iterator
-      .continually(())
-      .scanLeft((x0, Set.empty[N]))({
-        case ((x, points), _) =>
-          (f(x), points + x)
-      })
-
-  /**
-   * mergeStreams takes streams that are ordered w.r.t. Order[T]
-   *
-   */
-
-  def mergeStreams[T](streams: Seq[LazyList[T]])(
-    implicit
-    orderT: Order[T]): LazyList[T] = {
-
-    val frontier = streams.flatMap(_.headOption)
-
-    if (frontier.size === 0) {
-      LazyList.empty
-    } else {
-      val head = frontier.min
-      LazyList.cons(head, mergeStreams(streams.map(_.dropWhile(_ === head))))
-    }
-  }
-
-  def filterOut[T](stream: LazyList[T], toRemove: LazyList[T])(implicit orderT: Order[T]): LazyList[T] =
-    if (stream.isEmpty || toRemove.isEmpty) {
-      stream
-    } else {
-      val remove = toRemove.head
-      stream.takeWhile(_ < remove) ++ filterOut(stream.dropWhile(_ <= remove), toRemove.drop(1))
-    }
-
-  def lazyListsFrom[N](n: N)(implicit orderN: Order[N], ringN: Ring[N]): LazyList[N] =
-    LazyList.cons(n, lazyListsFrom(ringN.plus(n, ringN.one)))
 
 }
