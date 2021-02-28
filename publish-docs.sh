9dd77be205dc97a3c3288d5c202b3b6dd80c8384
Adam Pingel <pingel@gmail.com>
2021-02-28 14:39:16 -0500

    parameterize publish docs

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-s3cmd sync ~/s3/axle-lang.org/ s3://axle-lang.org/
-s3cmd setacl s3://axle-lang.org/ --acl-public --recursive
+SITESTAGEDIR=~/s3/axle-lang.org/
+SITEURL=s3://axle-lang.org/
+
+s3cmd sync $SITESTAGEDIR $SITEURL
+
+s3cmd setacl $SITEURL --acl-public --recursive
