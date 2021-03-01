205b64b6ddb178829f99c7faac4e5b33a683dc61
Adam Pingel <pingel@gmail.com>
2021-02-28 20:54:46 -0500

    create ~/.s3cfg

--- site-setup.sh
+++ site-setup.sh
@@ -6,6 +6,12 @@ sudo apt-get install ruby-full build-essential zlib1g-dev
 
 sudo apt-get install -y s3cmd
 
+cat << EOF >> ~/.s3cfg
+[default]
+access_key = ${SITEAWSACCESSKEY}
+secret_key = ${SITEAWSSECRETKEY}
+EOF
+
 sudo gem install jekyll bundler
 
 
