9325ae8ecd9398466976a23a6ded1a83390403de
Adam Pingel <pingel@gmail.com>
2022-04-22 00:24:11 -0400

    simplify site publish

--- site-update.sh
+++ site-update.sh
@@ -2,19 +2,21 @@
 
 set -e
 
-sbt -J-Xmx6G "project axle-docs" clean mdoc makeSite
+sbt 'project axle-docs' clean
+sbt -J-Xmx6G 'project axle-docs' mdoc
+sbt 'project axle-docs' makeSite
 
 export SITEBUILDDIR=axle-docs/target/site
 
 mkdir -p $SITEBUILDDIR
-cp axle-docs/src/site/_config.yml $SITEBUILDDIR
 cp axle-docs/src/site/favicon.ico $SITEBUILDDIR
 cp -R axle-docs/src/site/tutorial $SITEBUILDDIR
-cp -R axle-docs/target/site/* $SITEBUILDDIR
 mkdir -p $SITEBUILDDIR/tutorial/images/
-mv *.svg *.png $SITEBUILDDIR/tutorial/images/
-# Where does bowl.html belong?
+mv *.svg *.png $SITEBUILDDIR/tutorial/images/ # Where does bowl.html belong?
 cp -R axle-docs/src/site/css $SITEBUILDDIR
+
 # Where is $SITEBUILDDIR/target coming from?
 
-sbt 'project axle-docs' ghpagesCleanSite ghpagesPushSite
+# sbt 'project axle-docs' ghpagesCleanSite
+
+sbt 'project axle-docs' ghpagesPushSite
