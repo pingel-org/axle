b8761d2def6b673d02355c59d784741155985d7a
Adam Pingel <pingel@gmail.com>
2022-04-23 21:40:22 -0400

    simplify to be only sbt commands

--- site-update.sh
+++ site-update.sh
@@ -3,11 +3,7 @@
 set -e
 
 sbt 'project axle-docs' clean
-
-sbt -J-Xmx6G 'project axle-docs' mdoc # axle-docs/src/main/mdoc -> axle-docs/target/mdoc/; write new stuff to docwork
-
-sbt 'project axle-docs' laikaSite # {axle-docs/target/mdoc, axle-docs/src/site} -> axle-docs/target/site
-
+sbt -J-Xmx6G 'project axle-docs' mdoc
+sbt 'project axle-docs' laikaSite
 # sbt 'project axle-docs' ghpagesCleanSite
-
 sbt 'project axle-docs' ghpagesPushSite
