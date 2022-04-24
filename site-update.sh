197b7ad9ebd59a3b139dc74bff30424e55a08652
Adam Pingel <pingel@gmail.com>
2022-04-23 21:32:57 -0400

    docwork is checked in

--- site-update.sh
+++ site-update.sh
@@ -4,7 +4,6 @@ set -e
 
 sbt 'project axle-docs' clean
 
-mkdir -p docwork
 sbt -J-Xmx6G 'project axle-docs' mdoc # axle-docs/src/main/mdoc -> axle-docs/target/mdoc/; write new stuff to docwork
 
 sbt 'project axle-docs' laikaSite # {axle-docs/target/mdoc, axle-docs/src/site} -> axle-docs/target/site
