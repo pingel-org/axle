5027d83eaf5741df69df83feee926d72b9235aed
Adam Pingel <pingel@gmail.com>
2022-04-25 15:34:46 -0400

    date before and after

--- site-update.sh
+++ site-update.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+date
 sbt 'project axle-docs' clean
 sbt -J-Xmx6G 'project axle-docs' mdoc
 sbt 'project axle-docs' laikaSite
 # sbt 'project axle-docs' ghpagesCleanSite
 sbt 'project axle-docs' ghpagesPushSite
+date
