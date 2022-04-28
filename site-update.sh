7ab070f12e0f6d1831ef04c12060c9760d0bc3fd
Adam Pingel <pingel@gmail.com>
2022-04-28 11:34:33 -0400

    revert to specify axle-docs

--- site-update.sh
+++ site-update.sh
@@ -3,7 +3,8 @@
 set -e
 
 date
-sbt -J-Xmx8G clean mdoc
-sbt laikaSite ghpagesPushSite
-# ghpagesCleanSite
+sbt -J-Xmx8G 'project axle-docs' clean mdoc
+sbt 'project axle-docs' laikaSite
+#sbt ghpagesCleanSite
+sbt 'project axle-docs' ghpagesPushSite
 date
