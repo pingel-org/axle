ca8828c2278980fe7b3eddbfa5f580ce890f1935
Adam Pingel <pingel@gmail.com>
2022-04-28 11:03:39 -0400

    remove 'project axle-docs'

--- site-update.sh
+++ site-update.sh
@@ -3,7 +3,7 @@
 set -e
 
 date
-sbt -J-Xmx8G 'project axle-docs' clean mdoc
-sbt 'project axle-docs' laikaSite ghpagesPushSite
+sbt -J-Xmx8G clean mdoc
+sbt laikaSite ghpagesPushSite
 # ghpagesCleanSite
 date
