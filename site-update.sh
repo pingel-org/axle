4ef7f0e4dd78a90478b77bc43d986e797c4ca804
Adam Pingel <pingel@gmail.com>
2022-05-06 22:09:46 -0400

    -J-Xmx8G seemingly no longer needed

--- site-update.sh
+++ site-update.sh
@@ -3,8 +3,8 @@
 set -e
 
 date
-sbt -J-Xmx8G 'project axle-docs' clean
-sbt -J-Xmx8G 'project axle-docs' mdoc
+sbt 'project axle-docs' clean
+sbt 'project axle-docs' mdoc
 sbt 'project axle-docs' laikaSite
 #sbt ghpagesCleanSite
 sbt 'project axle-docs' ghpagesPushSite
